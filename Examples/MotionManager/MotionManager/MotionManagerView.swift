de4058af497350e864a06fb6ad230f6e20ce4f88
Brandon Williams <mbw234@gmail.com>
2020-05-08 09:48:24 -0700

    Allow motion manager to be demo'd in SwiftUI preview (#46)
    * Allow motion manager to be demo'd in SwiftUI preview.
    
    * improvements
    
    * readme
    
    * clean up

--- Examples/MotionManager/MotionManager/MotionManagerView.swift
+++ Examples/MotionManager/MotionManager/MotionManagerView.swift
@@ -2,6 +2,18 @@ import ComposableArchitecture
 import CoreMotion
 import SwiftUI
 
+private let readMe = """
+This demonstrates how to work with the MotionManager API from Apple's Motion framework.
+
+Unfortunately the Motion APIs are not available in SwiftUI previews or simulators. However, \
+thanks to how the Composable Architecture models its dependencies and effects, it is trivial \
+to substitute a mock MotionClient into the SwiftUI preview so that we can still play around with \
+its basic functionality.
+
+Here we are creating a mock MotionClient that simulates motion data by running a timer that emits \
+sinusoidal values.
+"""
+
 struct AppState: Equatable {
   var alertTitle: String?
   var isRecording = false
@@ -63,11 +75,13 @@ struct AppView: View {
   var body: some View {
     WithViewStore(self.store) { viewStore in
       VStack {
+        Text(readMe)
+          .multilineTextAlignment(.leading)
+          .layoutPriority(1)
+
         Spacer(minLength: 100)
 
-        ZStack {
-          plot(buffer: viewStore.z, scale: 40)
-        }
+        plot(buffer: viewStore.z, scale: 40)
 
         Button(action: { viewStore.send(.recordingButtonTapped) }) {
           HStack {
@@ -117,15 +131,37 @@ func plot(buffer: [Double], scale: Double) -> Path {
 
 struct AppView_Previews: PreviewProvider {
   static var previews: some View {
-    AppView(
+    // Since MotionManager isn't usable in SwiftUI previews or simulators we create one that just
+    // sends a bunch of data on some sine curves.
+    var isStarted = false
+    let mockMotionClient = MotionClient(
+      create: { id in
+        Effect.timer(id: id, every: 0.01, on: DispatchQueue.main)
+          .filter { _ in isStarted }
+          .map { time in
+            let t = Double(time.dispatchTime.uptimeNanoseconds) / 500_000_000.0
+            return .motionUpdate(
+              .init(
+                gravity: .init(x: sin(2 * t), y: -cos(-t), z: sin(3 * t)),
+                userAcceleration: .init(x: -cos(-3 * t), y: sin(2 * t), z: -cos(t))
+              )
+            )
+        }
+        .eraseToEffect()
+    },
+      startDeviceMotionUpdates: { _ in .fireAndForget { isStarted = true } },
+      stopDeviceMotionUpdates: { _ in .fireAndForget { isStarted = false } }
+    )
+
+    return AppView(
       store: Store(
         initialState: AppState(
-          isRecording: false,
-          z: (1...350)
-            .map { sin(Double($0) / 10) }
+          z: (1...350).map {
+            2 * sin(Double($0) / 20) + cos(Double($0) / 8)
+          }
         ),
         reducer: appReducer,
-        environment: .init(motionClient: .live)
+        environment: .init(motionClient: mockMotionClient)
       )
     )
   }
