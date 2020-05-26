c6d75a726ae925c9c3d9a537c0281f9bb0bbe7f1
Stephen Celis <stephen@stephencelis.com>
2020-05-26 12:56:54 -0400

    cleanup

--- Sources/ComposableArchitecture/Debugging/ReducerInstrumentation.swift
+++ Sources/ComposableArchitecture/Debugging/ReducerInstrumentation.swift
@@ -2,26 +2,27 @@ import Combine
 import os.signpost
 
 extension Reducer {
-  /// Instruments the reducer with [signpost](https://developer.apple.com/documentation/os/logging/recording_performance_data).
+  /// Instruments the reducer with
+  /// [signposts](https://developer.apple.com/documentation/os/logging/recording_performance_data).
   /// Each invocation of the reducer will be measured by an interval, and the lifecycle of its
   /// effects will be measured with interval and event signposts.
   ///
-  /// To use, build your app for instruments (⌘I), create a blank instrument, and then use the +
-  /// icon in the top right to add the signpost instrument. Start recording your app (red button
-  /// in the top left) and then you should see timing information for every action sent to the
-  /// store and every effect executed.
+  /// To use, build your app for Instruments (⌘I), create a blank instrument, and then use the "+"
+  /// icon at top right to add the signpost instrument. Start recording your app (red button at top
+  /// left) and then you should see timing information for every action sent to the store and every
+  /// effect executed.
   ///
-  /// The effect instrumentation can be particularly useful for inspecting the lifecycle of long
-  /// living effects. For example, if you start an effect (e.g. a location manager) in `onAppear`
-  /// and forget to tear down the effect in `onDisappear`, it will clearly show in instruments
-  /// that the effect was never completed.
+  /// Effect instrumentation can be particularly useful for inspecting the lifecycle of long-living
+  /// effects. For example, if you start an effect (e.g. a location manager) in `onAppear` and
+  /// forget to tear down the effect in `onDisappear`, it will clearly show in Instruments that the
+  /// effect never completed.
   ///
   /// - Parameters:
   ///   - prefix: A string to print at the beginning of the formatted message for the signpost.
-  ///   - log: An optional `OSLog` to use for signposts.
+  ///   - log: An `OSLog` to use for signposts.
   /// - Returns: A reducer that has been enhanced with instrumentation.
   public func signpost(
-    _ prefix: String = _zeroWidthSpace,
+    _ prefix: String = "",
     log: OSLog = OSLog(
       subsystem: "co.pointfree.composable-architecture",
       category: "Reducer Instrumentation"
@@ -29,7 +30,10 @@ extension Reducer {
   ) -> Self {
     guard log.signpostsEnabled else { return self }
 
-    let prefix = prefix == _zeroWidthSpace ? _zeroWidthSpace : "[\(prefix)] "
+    // NB: Prevent rendering as "N/A" in Instruments
+    let zeroWidthSpace = "\u{200B}"
+
+    let prefix = prefix.isEmpty ? zeroWidthSpace : "[\(prefix)] "
 
     return Self { state, action, environment in
       if log.signpostsEnabled {
@@ -84,8 +88,6 @@ extension Publisher where Failure == Never {
         })
   }
 }
-
-public let _zeroWidthSpace = "​"
 
 private func debugCaseOutput(_ value: Any) -> String {
   let mirror = Mirror(reflecting: value)
