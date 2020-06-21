6e3be5a2aa5645d775ec65d1c65a42a902367c3a
Brandon Williams <mbw234@gmail.com>
2020-06-21 10:02:47 -0500

    wip

--- Tests/ComposableCoreMotionTests/ComposableCoreMotionTests.swift
+++ Tests/ComposableCoreMotionTests/ComposableCoreMotionTests.swift
@@ -3,15 +3,4 @@ import XCTest
 @testable import ComposableCoreMotion
 
 class ComposableCoreMotionTests: XCTestCase {
-  func testQ() {
-
-    let a = Attitude(
-      quaternion: CMQuaternion(x: 0.46291, y: -0.3086067, z: 0.7715167, w: 0.3086067))
-
-    let tmp = a.rotationMatrix
-    print(a.roll)
-    print(a.pitch)
-    print(a.yaw)
-    print("!")
-  }
 }
