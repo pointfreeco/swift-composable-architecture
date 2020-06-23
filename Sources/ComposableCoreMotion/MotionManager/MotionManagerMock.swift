895de18fcffa9badc01d14e963307b75658e5ea1
Stephen Celis <stephen@stephencelis.com>
2020-06-23 19:00:01 -0400

    mock

--- Sources/ComposableCoreMotion/MotionManager/MotionManagerMock.swift
+++ Sources/ComposableCoreMotion/MotionManager/MotionManagerMock.swift
@@ -100,16 +100,3 @@ extension MotionManager {
     )
   }
 }
-
-public func _unimplemented(
-  _ function: StaticString, file: StaticString = #file, line: UInt = #line
-) -> Never {
-  fatalError(
-    """
-    `\(function)` was called but is not implemented. Be sure to provide an implementation for
-    this endpoint when creating the mock.
-    """,
-    file: file,
-    line: line
-  )
-}
