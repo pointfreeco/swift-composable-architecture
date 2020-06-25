20434cbd13fd16ba0b9549f223c69d353e57c873
Brandon Williams <mbw234@gmail.com>
2020-06-25 11:16:51 -0500

    Use new main/app stuff for case studies

--- Examples/CaseStudies/SwiftUICaseStudies/SceneDelegate.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/SceneDelegate.swift
@@ -1,26 +1,10 @@
 import SwiftUI
-import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-
-  func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    self.window = (scene as? UIWindowScene).map(UIWindow.init(windowScene:))
-    self.window?.rootViewController = UIHostingController(rootView: RootView())
-    self.window?.makeKeyAndVisible()
-  }
-}
-
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    true
+@main
+struct CaseStudies: App {
+  var body: some Scene {
+    WindowGroup {
+      RootView()
+    }
   }
 }
