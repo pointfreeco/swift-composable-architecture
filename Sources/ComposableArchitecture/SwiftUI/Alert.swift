6b06e5a3a03fb591f0ae25a559c87c953d566a6f
Brandon Williams <mbw234@gmail.com>
2020-06-27 18:18:23 -0500

    wip

--- Sources/ComposableArchitecture/SwiftUI/Alert.swift
+++ Sources/ComposableArchitecture/SwiftUI/Alert.swift
@@ -1,6 +1,114 @@
 import SwiftUI
 
-/// A data type that describes the state of an alert that can be shown to the user.
+/// A data type that describes the state of an alert that can be shown to the user. The `Action`
+/// generic is the type of actions that can be sent from tapping on a button in the alert.
+///
+/// This type can be used in your application's state in order to control the presentation or
+/// dismissal of alerts. It is preferrable to use this API instead of the default SwiftUI API
+/// for alerts because SwiftUI uses 2-way bindings in order to control the showing and dismissal
+/// of alerts, and that does not play nicely with the Composable Architecture. The library requires
+/// that all state mutations happen by sending an action so that a reducer can handle that logic,
+/// which greatly simplifies how data flows through your application, and gives you instant
+/// testability on all parts of your application.
+///
+/// To use this API, you model all the alert actions in your domain's action enum:
+///
+///     enum Action {
+///       case cancelTapped
+///       case confirmTapped
+///       case deleteTapped
+///
+///       // Your other actions
+///     }
+///
+/// And you model the state for showing the alert in your domain's state, and it can start off in
+/// the `.dismissed` state:
+///
+///     struct AppState {
+///       var alert = AlertState<AppAction>.dismissed
+///       // Your other state
+///     }
+///
+/// Then, in the reducer you can construct an `AlertState` value to represent the alert you want
+/// to show to the user:
+///
+///     let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, env in
+///       switch action
+///         case .cancelTapped:
+///           state.alert = .dismissed
+///           return .none
+///
+///         case .confirmTapped:
+///             state.alert = .dismissed
+///             // Do deletion logic...
+///
+///         case .deleteTapped:
+///           state.alert = .show(
+///             .init(
+///               message: "Are you sure you want to delete this? It cannot be undone.",
+///               primaryButton: .init(
+///                 action: .confirmTapped,
+///                 label: "Confirm",
+///                 type: .default
+///               ),
+///               secondaryButton: .init(
+///                 action: .cancelTapped,
+///                 label: "Cancel",
+///                 type: .cancel
+///               ),
+///               title: "Delete"
+///             )
+///           )
+///       }
+///     }
+///
+/// And then, in your view you can use the new `.sheet(_:send:dismiss:)` method on `View` in order
+/// to present the alert in a way that works best with the Composable Architecture:
+///
+///     Button("Delete") { viewStore.send(.deleteTapped) }
+///       .alert(
+///         viewStore.alert,
+///         send: viewStore.send,
+///         dismiss: .cancelTapped
+///       )
+///
+/// This makes your reducer in complete control of when the alert is shown or dismissed, and makes
+/// it so that any choice made in the alert is automatically fed back into the reducer so that you
+/// can handle its logic.
+///
+/// Even better, you can instantly write tests that your alert-behavior works as expected:
+///
+///     let store = TestStore(
+///       initialState: AppState(),
+///       reducer: appReducer,
+///       environment: .mock
+///     )
+///
+///     store.assert(
+///       .send(.deleteTapped) {
+///         $0.alert = .show(
+///           .init(
+///             message: "Are you sure you want to delete this? It cannot be undone.",
+///             primaryButton: .init(
+///               action: .confirmTapped,
+///               label: "Confirm",
+///               type: .default
+///             ),
+///             secondaryButton: .init(
+///               action: .cancelTapped,
+///               label: "Cancel",
+///               type: .cancel
+///             ),
+///             title: "Delete"
+///           )
+///         )
+///       },
+///       .send(.deleteTapped) {
+///         $0.alert = .dismissed
+///         // Also verify that delete logic executed correctly
+///       }
+///     )
+///
 public enum AlertState<Action> {
   case dismissed
   case show(Alert)
@@ -47,17 +155,6 @@ public enum AlertState<Action> {
   }
 }
 
-extension AlertState: Equatable where Action: Equatable {}
-extension AlertState: Hashable where Action: Hashable {}
-extension AlertState.Alert: Equatable where Action: Equatable {}
-extension AlertState.Alert: Hashable where Action: Hashable {}
-extension AlertState.Alert.Button: Equatable where Action: Equatable {}
-extension AlertState.Alert.Button: Hashable where Action: Hashable {}
-
-extension AlertState.Alert: Identifiable where Action: Hashable {
-  public var id: Self { self }
-}
-
 extension View {
   /// Displays an alert when `state` is in the `.show` state.
   ///
@@ -90,6 +187,17 @@ extension View {
       content: { $0.toSwiftUI(send: send) }
     )
   }
+}
+
+extension AlertState: Equatable where Action: Equatable {}
+extension AlertState: Hashable where Action: Hashable {}
+extension AlertState.Alert: Equatable where Action: Equatable {}
+extension AlertState.Alert: Hashable where Action: Hashable {}
+extension AlertState.Alert.Button: Equatable where Action: Equatable {}
+extension AlertState.Alert.Button: Hashable where Action: Hashable {}
+
+extension AlertState.Alert: Identifiable where Action: Hashable {
+  public var id: Self { self }
 }
 
 extension AlertState.Alert.Button {
