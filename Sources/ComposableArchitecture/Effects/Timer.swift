c2f64aa87488cd77c7b41653ba10940c824eeaf2
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2020-07-09 10:25:06 -0400

    Update docs for Effect.timer and TestStore. (#220)
    * Update docs for Effect.timer and TestStore.
    
    * format
    
    * update
    
    * Update Sources/ComposableArchitecture/Effects/Timer.swift
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>
    
    * Update Sources/ComposableArchitecture/TestSupport/TestStore.swift
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>
    
    * wip
    
    * Update TestStore.swift
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>

--- Sources/ComposableArchitecture/Effects/Timer.swift
+++ Sources/ComposableArchitecture/Effects/Timer.swift
@@ -2,23 +2,89 @@ import Combine
 import CombineSchedulers
 
 extension Effect where Failure == Never {
-  /// Returns an effect that repeatedly emits the current time of the given
-  /// scheduler on the given interval.
+  /// Returns an effect that repeatedly emits the current time of the given scheduler on the given
+  /// interval.
   ///
-  /// This effect serves as a testable alternative to `Timer.publish`, which
-  /// performs its work on a run loop, _not_ a scheduler.
+  /// While it is possible to use Foundation's `Timer.publish(every:tolerance:on:in:options:)` API
+  /// to create a timer in the Composable Architecture, it is not advisable. This API only allows
+  /// creating a timer on a run loop, which means when writing tests you will need to explicitly
+  /// wait for time to pass in order to see how the effect evolves in your feature.
   ///
-  ///     struct TimerId: Hashable {}
+  /// In the Composable Architecture we test time-based effects like this by using the
+  /// `TestScheduler`, which allows us to explicitly and immediately advance time forward so that
+  /// we can see how effects emit. However, because `Timer.publish` takes a concrete `RunLoop` as
+  /// its scheduler, we can't substitute in a `TestScheduler` during tests`.
   ///
-  ///     switch action {
-  ///     case .startTimer:
-  ///       return Effect.timer(id: TimerId(), every: 1, on: environment.scheduler)
-  ///         .map { .timerUpdated($0) }
-  ///     case let .timerUpdated(date):
-  ///       state.date = date
-  ///       return .none
-  ///     case .stopTimer:
-  ///       return .cancel(id: TimerId())
+  /// That is why we provide the `Effect.timer` effect. It allows you to create a timer that works
+  /// with any scheduler, not just a run loop, which means you can use a `DispatchQueue` or
+  /// `RunLoop` when running your live app, but use a `TestScheduler` in tests.
+  ///
+  /// To start and stop a timer in your feature you can create the timer effect from an action
+  /// and then use the `.cancel(id:)` effect to stop the timer:
+  ///
+  ///     struct AppState {
+  ///       var count = 0
+  ///     }
+  ///
+  ///     enum AppAction {
+  ///       case startButtonTapped, stopButtonTapped, timerTicked
+  ///     }
+  ///
+  ///     struct AppEnvironment {
+  ///       var mainQueue: AnySchedulerOf<DispatchQueue>
+  ///     }
+  ///
+  ///     let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, env in
+  ///       struct TimerId: Hashable {}
+  ///
+  ///       switch action {
+  ///       case .startButtonTapped:
+  ///         return Effect.timer(id: TimerId(), every: 1, on: env.mainQueue)
+  ///           .map { _ in .timerTicked }
+  ///
+  ///       case .stopButtonTapped:
+  ///         return .cancel(id: TimerId())
+  ///
+  ///       case let .timerTicked:
+  ///         state.count += 1
+  ///         return .none
+  ///     }
+  ///
+  /// Then to test the timer in this feature you can use a test scheduler to advance time:
+  ///
+  ///   func testTimer() {
+  ///     let scheduler = DispatchQueue.testScheduler
+  ///
+  ///     let store = TestStore(
+  ///       initialState: .init(),
+  ///       reducer: appReducer,
+  ///       envirnoment: .init(
+  ///         mainQueue: scheduler.eraseToAnyScheduler()
+  ///       )
+  ///     )
+  ///
+  ///     store.assert(
+  ///       .send(.startButtonTapped),
+  ///
+  ///       .do { scheduler.advance(by: .seconds(1)) },
+  ///       .receive(.timerTicked) { $0.count = 1 },
+  ///
+  ///       .do { scheduler.advance(by: .seconds(5)) },
+  ///       .receive(.timerTicked) { $0.count = 2 },
+  ///       .receive(.timerTicked) { $0.count = 3 },
+  ///       .receive(.timerTicked) { $0.count = 4 },
+  ///       .receive(.timerTicked) { $0.count = 5 },
+  ///       .receive(.timerTicked) { $0.count = 6 },
+  ///
+  ///       .send(.stopButtonTapped)
+  ///     )
+  ///   }
+  ///
+  /// - Note: This effect is only meant to be used with features built in the Composable
+  ///   Architecture, and returned from a reducer. If you want a testable alternative to
+  ///   Foundation's `Timer.publish` you can use the publisher `Publishers.Timer` that is included
+  ///   in this library via the
+  ///   [`CombineSchedulers`](https://github.com/pointfreeco/combine-schedulers) module.
   ///
   /// - Parameters:
   ///   - interval: The time interval on which to publish events. For example, a value of `0.5`
