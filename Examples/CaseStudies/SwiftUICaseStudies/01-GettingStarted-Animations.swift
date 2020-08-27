b3ddf16165b99997c5ecf490ccc9cfc8d30c5504
Brandon Williams <mbw234@gmail.com>
2020-08-27 11:51:48 -0400

    clean up

--- Examples/CaseStudies/SwiftUICaseStudies/01-GettingStarted-Animations.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/01-GettingStarted-Animations.swift
@@ -55,30 +55,6 @@ struct AnimationsEnvironment {
   var mainQueue: AnySchedulerOf<DispatchQueue>
 }
 
-extension Scheduler {
-  func animation(_ animation: Animation) -> AnyScheduler<SchedulerTimeType, SchedulerOptions> {
-    AnyScheduler(
-      minimumTolerance: { self.minimumTolerance },
-      now: { self.now },
-      scheduleImmediately: { options, action in
-        self.schedule(options: options) {
-          withAnimation(animation, action)
-        }
-      },
-      delayed: { date, tolerance, options, action in
-        self.schedule(after: date, tolerance: tolerance, options: options) {
-          withAnimation(animation, action)
-        }
-      },
-      interval: { date, interval, tolerance, options, action in
-        self.schedule(after: date, interval: interval, tolerance: tolerance, options: options) {
-          withAnimation(animation, action)
-        }
-      }
-    )
-  }
-}
-
 let animationsReducer = Reducer<AnimationsState, AnimationsAction, AnimationsEnvironment> {
   state, action, environment in
 
