13cb3c3165eb436fd8da1276f5d549fa2617548e
Stephen Celis <stephen@stephencelis.com>
2020-08-27 12:23:12 -0400

    Update CI to include Xcode 12 (#266)
    * Update CI to include Xcode 12
    
    * Use CurrentValueSubject

--- Sources/ComposableArchitecture/Store.swift
+++ Sources/ComposableArchitecture/Store.swift
@@ -7,7 +7,7 @@ import Foundation
 /// You will typically construct a single one of these at the root of your application, and then use
 /// the `scope` method to derive more focused stores that can be passed to subviews.
 public final class Store<State, Action> {
-  @Published private(set) var state: State
+  var state: CurrentValueSubject<State, Never>
   var effectCancellables: [UUID: AnyCancellable] = [:]
   private var isSending = false
   private var parentCancellable: AnyCancellable?
@@ -60,15 +60,15 @@ public final class Store<State, Action> {
     action fromLocalAction: @escaping (LocalAction) -> Action
   ) -> Store<LocalState, LocalAction> {
     let localStore = Store<LocalState, LocalAction>(
-      initialState: toLocalState(self.state),
+      initialState: toLocalState(self.state.value),
       reducer: { localState, localAction in
         self.send(fromLocalAction(localAction))
-        localState = toLocalState(self.state)
+        localState = toLocalState(self.state.value)
         return .none
       }
     )
-    localStore.parentCancellable = self.$state
-      .sink { [weak localStore] newValue in localStore?.state = toLocalState(newValue) }
+    localStore.parentCancellable = self.state
+      .sink { [weak localStore] newValue in localStore?.state.value = toLocalState(newValue) }
     return localStore
   }
 
@@ -102,20 +102,20 @@ public final class Store<State, Action> {
       return localState
     }
 
-    return toLocalState(self.$state.eraseToAnyPublisher())
+    return toLocalState(self.state.eraseToAnyPublisher())
       .map { localState in
         let localStore = Store<LocalState, LocalAction>(
           initialState: localState,
           reducer: { localState, localAction in
             self.send(fromLocalAction(localAction))
-            localState = extractLocalState(self.state) ?? localState
+            localState = extractLocalState(self.state.value) ?? localState
             return .none
           })
 
-        localStore.parentCancellable = self.$state
+        localStore.parentCancellable = self.state
           .sink { [weak localStore] state in
             guard let localStore = localStore else { return }
-            localStore.state = extractLocalState(state) ?? localStore.state
+            localStore.state.value = extractLocalState(state) ?? localStore.state.value
           }
         return localStore
       }
@@ -163,7 +163,7 @@ public final class Store<State, Action> {
         )
       }
       self.isSending = true
-      let effect = self.reducer(&self.state, action)
+      let effect = self.reducer(&self.state.value, action)
       self.isSending = false
 
       var didComplete = false
@@ -207,7 +207,7 @@ public final class Store<State, Action> {
     reducer: @escaping (inout State, Action) -> Effect<Action, Never>
   ) {
     self.reducer = reducer
-    self.state = initialState
+    self.state = CurrentValueSubject(initialState)
   }
 }
 
