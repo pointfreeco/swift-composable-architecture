1c5fad5451e9cf8381b9f66fadad7417bd4b5d81
Brandon Williams <mbw234@gmail.com>
2020-09-14 15:20:19 -0400

    test

--- Tests/ComposableArchitectureTests/QueuingTests.swift
+++ Tests/ComposableArchitectureTests/QueuingTests.swift
@@ -5,95 +5,44 @@ import XCTest
 final class QueuingTests: XCTestCase {
 
   func testQueuing() {
-    let service = Service()
+    let subject = PassthroughSubject<Void, Never>()
 
-    let store = Store(
-      initialState: State(toggle: .off),
-      reducer: reducer,
-      environment: Environment(
-        service: service
-      )
+    enum Action: Equatable {
+      case incrementTapped
+      case `init`
+      case doIncrement
+    }
+
+    let store = TestStore(
+      initialState: 0,
+      reducer: Reducer<Int, Action, Void> { state, action, _ in
+        switch action {
+        case .incrementTapped:
+          subject.send()
+          return .none
+
+        case .`init`:
+          return subject.map { .doIncrement }.eraseToEffect()
+
+        case .doIncrement:
+          state += 1
+          return .none
+        }
+      },
+      environment: ()
     )
-    let viewStore = ViewStore(store)
 
-    viewStore.send(.`init`)
-    viewStore.send(.sendUUIDTapped)
-    XCTAssertEqual(.init(toggle: .on), viewStore.state)
-
-    viewStore.send(.sendNilTapped)
-    XCTAssertEqual(.init(toggle: .off), viewStore.state)
-
-    let testStore = TestStore(
-      initialState: State(toggle: .off),
-      reducer: reducer,
-      environment: Environment(
-        service: service
-      )
-    )
-    testStore.assert(
+    store.assert(
       .send(.`init`),
-      .send(.sendUUIDTapped),
-      .receive(.receiveServiceResult("A")) {
-        $0.toggle = .on
+      .send(.incrementTapped),
+      .receive(.doIncrement) {
+        $0 = 1
       },
-      .send(.sendNilTapped),
-      .receive(.receiveServiceResult(nil)) {
-        $0.toggle = .off
+      .send(.incrementTapped),
+      .receive(.doIncrement) {
+        $0 = 2
       },
-      .do { service.subject.send(completion: .finished)}
+      .do { subject.send(completion: .finished) }
     )
-  }
-}
-
-
-final class Service {
-  let subject = PassthroughSubject<String?, Never>()
-  
-  func publisher(for id: String) -> Effect<String?, Never> {
-    subject.eraseToEffect()
-  }
-  
-  func trigger(value: String?) {
-    subject.send(value)
-  }
-}
-
-enum Actions: Equatable {
-  case `init`
-  case sendNilTapped
-  case sendUUIDTapped
-  case receiveServiceResult(String?)
-}
-
-enum Toggle: String, Equatable {
-  case on
-  case off
-}
-
-struct State: Equatable {
-  var toggle: Toggle
-}
-
-struct Environment {
-  var service: Service
-}
-
-let reducer = Reducer<State, Actions, Environment> { state, action, environment in
-  switch action {
-  case .`init`:
-    return environment.service.publisher(for: "my id")
-      .map(Actions.receiveServiceResult)
-    
-  case .sendNilTapped:
-    environment.service.trigger(value: nil)
-    return .none
-    
-  case .sendUUIDTapped:
-    environment.service.trigger(value: "A")
-    return .none
-    
-  case let .receiveServiceResult(result):
-    state.toggle = result != nil ? .on : .off
-    return .none
   }
 }
