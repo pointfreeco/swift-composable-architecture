dc4b49ddbe99587a4c81e6f910ad490ccd9e672e
Stephen Celis <stephen@stephencelis.com>
2020-09-16 12:43:01 -0400

    add presents

--- Sources/ComposableArchitecture/Reducer.swift
+++ Sources/ComposableArchitecture/Reducer.swift
@@ -407,6 +407,31 @@ public struct Reducer<State, Action, Environment> {
     }
   }
 
+  public func presents<LocalState, LocalAction, LocalEnvironment>(
+    _ localReducer: Reducer<LocalState, LocalAction, LocalEnvironment>,
+    cancelEffectsOnDismiss: Bool,
+    state toLocalState: WritableKeyPath<State, LocalState?>,
+    action toLocalAction: CasePath<Action, LocalAction>,
+    environment toLocalEnvironment: @escaping (Environment) -> LocalEnvironment
+  ) -> Self {
+    let id = UUID()
+    return Self { state, action, environment in
+      let hadLocalState = state[keyPath: toLocalState] != nil
+      let localEffects = localReducer
+        .optional()
+        .pullback(state: toLocalState, action: toLocalAction, environment: toLocalEnvironment)
+        .run(&state, action, environment)
+        .cancellable(id: id)
+      let globalEffects = self.run(&state, action, environment)
+      let hasLocalState = state[keyPath: toLocalState] != nil
+      return .merge(
+        localEffects,
+        globalEffects,
+        cancelEffectsOnDismiss && hadLocalState && !hasLocalState ? .cancel(id: id) : .none
+      )
+    }
+  }
+
   /// A version of `pullback` that transforms a reducer that works on an element into one that works
   /// on an identified array of elements.
   ///
