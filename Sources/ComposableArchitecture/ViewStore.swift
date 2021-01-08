93fbcb5d9ad60261847d9c14f587657bd010bde7
Stephen Celis <stephen@stephencelis.com>
2021-01-07 19:47:21 -0500

    Fix Binding animations

--- Sources/ComposableArchitecture/ViewStore.swift
+++ Sources/ComposableArchitecture/ViewStore.swift
@@ -125,10 +125,15 @@ public final class ViewStore<State, Action>: ObservableObject {
     Binding(
       get: { get(self.state) },
       set: { newLocalState, transaction in
-        withAnimation(transaction.disablesAnimations ? nil : transaction.animation) {
+        if transaction.animation != nil {
+          withTransaction(transaction) {
+            self.send(localStateToViewAction(newLocalState))
+          }
+        } else {
           self.send(localStateToViewAction(newLocalState))
         }
-      })
+      }
+    )
   }
 
   /// Derives a binding from the store that prevents direct writes to state and instead sends
