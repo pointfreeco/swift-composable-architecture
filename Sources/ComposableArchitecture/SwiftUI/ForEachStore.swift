8205fd3c3f7ee66a8cab4612544f93c91b37652b
Stephen Celis <stephen@stephencelis.com>
2021-02-15 13:27:52 -0500

    Basic docs

--- Sources/ComposableArchitecture/SwiftUI/ForEachStore.swift
+++ Sources/ComposableArchitecture/SwiftUI/ForEachStore.swift
@@ -1,6 +1,61 @@
 import SwiftUI
 
-/// A structure that computes views on demand from a store on a collection of data.
+/// A Composable Architecture-friendly wrapper around `ForEach` that simplifies working with
+/// collections of state.
+///
+/// `ForEachStore` loops over a store's collection with a store scoped to the domain of each
+/// element. This allows you to extract and modularize an element's view and avoid concerns around
+/// collection index math and parent-child store communication.
+///
+/// For example, a todos app may define the domain and logic associated with an individual todo:
+///
+///     struct TodoState: Equatable, Identifiable {
+///       let id: UUID
+///       var description = ""
+///       var isComplete = false
+///     }
+///     enum TodoAction {
+///       case isCompleteToggled(Bool)
+///       case descriptionChanged(String)
+///     }
+///     struct TodoEnvironment {}
+///     let todoReducer = Reducer<TodoState, TodoAction, TodoEnvironment { ... }
+///
+/// As well as a view with a domain-specific store:
+///
+///     struct TodoView: View {
+///       let store: Store<TodoState, TodoAction>
+///       var body: some View { ... }
+///     }
+///
+/// For a parent domain to work with a collection of todos, it can hold onto this collection in
+/// state:
+///
+///     struct AppState: Equatable {
+///       var todos: IdentifiedArrayOf<TodoState> = []
+///     }
+///
+/// Define a case to handle actions sent to the child domain:
+///
+///     enum AppAction {
+///       case todo(id: TodoState.ID, action: TodoAction)
+///     }
+///
+/// Enhance its reducer using `forEach`:
+///
+///     let appReducer = todoReducer.forEach(
+///       state: \.todos,
+///       action: /AppAction.todo(id:action:),
+///       environment: { _ in TodoEnvironment() }
+///     )
+///
+/// And finally render a list of `TodoView` using `ForEachStore`:
+///
+///     ForEachStore(
+///       self.store.scope(state: \.todos, AppAction.todo(id:action:))
+///     ) { todoStore in
+///       TodoView(store: todoStore)
+///     }
 public struct ForEachStore<EachState, EachAction, Data, ID, Content>: DynamicViewContent
 where Data: Collection, ID: Hashable, Content: View {
   public let data: Data
