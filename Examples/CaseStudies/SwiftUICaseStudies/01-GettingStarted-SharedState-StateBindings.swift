b466e0d8541799a37249772848143e41baeffb40
Thomas Grapperon <tgrapperon@gmail.com>
2021-03-20 05:12:30 -0300

    Revert "Remove modern trailing closure syntax for backward compatibility"
    This reverts commit 427fb456ac7e13469a3825da328e5cb4d46025eb.

--- Examples/CaseStudies/SwiftUICaseStudies/01-GettingStarted-SharedState-StateBindings.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/01-GettingStarted-SharedState-StateBindings.swift
@@ -159,11 +159,12 @@ struct SharedStateWithBinding: Equatable {
             PropertyBinding(\.count, \.count),
 
             // Explicit declaration, equivalent to
-            // PropertyBinding(\.content, \.text)
-            PropertyBinding<Self, FeatureState>(
-              get: { src, dest in dest.text = src.content },
-              set: { src, dest in src.content = dest.text }
-            )
+            // PropertyBinding(\.content, \.text) or (\.content, \.text)
+            PropertyBinding<Self, FeatureState> { src, dest in
+                dest.text = src.content
+            } set: { src, dest in
+                src.content = dest.text
+            }
         ]
         #endif
     }
