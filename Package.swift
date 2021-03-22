84b9a004384eb8dcd2fd817a5bc61d0152a47422
Stephen Celis <stephen@stephencelis.com>
2021-03-22 13:54:52 -0400

    Failing Effects (#453)

--- Package.swift
+++ Package.swift
@@ -17,8 +17,9 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.3.1"),
+    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.4.0"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.1.3"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),
   ],
   targets: [
     .target(
@@ -26,12 +27,12 @@ let package = Package(
       dependencies: [
         "CasePaths",
         "CombineSchedulers",
+        "XCTestDynamicOverlay"
       ]
     ),
     .testTarget(
       name: "ComposableArchitectureTests",
       dependencies: [
-        "CombineSchedulers",
         "ComposableArchitecture",
       ]
     ),
---
+++ Sources/ComposableArchitecture/TestSupport/FailingEffect.swift
@@ -0,0 +1,83 @@
+#if DEBUG
+  import XCTestDynamicOverlay
+
+  extension Effect {
+    /// An effect that causes a test to fail if it runs.
+    ///
+    /// This effect can provide an additional layer of certainty that a tested code path does not
+    /// execute a particular effect.
+    ///
+    /// For example, let's say we have a very simple counter application, where a user can increment
+    /// and decrement a number. The state and actions are simple enough:
+    ///
+    ///     struct CounterState: Equatable {
+    ///       var count = 0
+    ///     }
+    ///
+    ///     enum CounterAction: Equatable {
+    ///       case decrementButtonTapped
+    ///       case incrementButtonTapped
+    ///     }
+    ///
+    /// Let's throw in a side effect. If the user attempts to decrement the counter below zero, the
+    /// application should refuse and play an alert sound instead.
+    ///
+    /// We can model playing a sound in the environment with an effect:
+    ///
+    ///     struct CounterEnvironment {
+    ///       let playAlertSound: () -> Effect<Never, Never>
+    ///     }
+    ///
+    /// Now that we've defined the domain, we can describe the logic in a reducer:
+    ///
+    ///     let counterReducer = Reducer<
+    ///       CounterState, CounterAction, CounterEnvironment
+    ///     > { state, action, environment in
+    ///       switch action {
+    ///       case .decrementButtonTapped:
+    ///         if state > 0 {
+    ///           state.count -= 0
+    ///           return .none
+    ///         } else {
+    ///           return environment.playAlertSound()
+    ///             .fireAndForget()
+    ///         }
+    ///
+    ///       case .incrementButtonTapped:
+    ///         state.count += 1
+    ///         return .non
+    ///       }
+    ///     }
+    ///
+    /// Let's say we want to write a test for the increment path. We can see in the reducer that it
+    /// should never play an alert, so we can configure the environment with an effect that will
+    /// fail if it ever executes:
+    ///
+    ///     func testIncrement() {
+    ///       let store = TestStore(
+    ///         initialState: CounterState(count: 0)
+    ///         reducer: counterReducer,
+    ///         environment: CounterEnvironment(
+    ///           playSound: .failing("playSound")
+    ///         )
+    ///       )
+    ///
+    ///       store.send(.increment) {
+    ///         $0.count = 1
+    ///       }
+    ///     }
+    ///
+    /// By using a `.failing` effect in our environment we have strengthened the assertion and made
+    /// the test easier to understand at the same time. We can see, without consulting the reducer
+    /// itself, that this particular action should not access this effect.
+    ///
+    /// - Parameter prefix: A string that identifies this scheduler and will prefix all failure
+    ///   messages.
+    /// - Returns: An effect that causes a test to fail if it runs.
+    public static func failing(_ prefix: String) -> Self {
+      .fireAndForget {
+        XCTFail("\(prefix.isEmpty ? "" : "\(prefix) - ")A failing effect ran.")
+      }
+    }
+  }
+#endif
