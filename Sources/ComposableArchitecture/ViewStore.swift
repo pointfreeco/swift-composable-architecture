4c1898d24aa5eea69b02f7d4432b2e5777115e11
Stephen Celis <stephen@stephencelis.com>
2021-04-23 00:03:30 -0400

    wip

--- Sources/ComposableArchitecture/ViewStore.swift
+++ Sources/ComposableArchitecture/ViewStore.swift
@@ -44,8 +44,7 @@ import SwiftUI
 @dynamicMemberLookup
 public final class ViewStore<State, Action>: ObservableObject {
   /// A publisher of state.
-//  public let publisher: StorePublisher<State>
-  public let publisher: AnyPublisher<State, Never>
+  public let publisher: StorePublisher<State>
 
   private var viewCancellable: AnyCancellable?
 
@@ -65,10 +64,12 @@ public final class ViewStore<State, Action>: ObservableObject {
   ) {
     let publisher = store.state
       .removeDuplicates(by: isDuplicate)
+      .shareReplay(1)
       .prepend(store.state.value)
-      .share()
-    self.publisher = publisher
-      .eraseToAnyPublisher()
+    self.publisher = .init(
+      _output: { store.state.value },
+      upstream: publisher.eraseToAnyPublisher()
+    )
     self.state = store.state.value
     self._send = store.send
     self.viewCancellable = publisher.sink { [weak self] in self?.state = $0 }
@@ -239,5 +240,97 @@ extension ViewStore where State: Equatable {
 extension ViewStore where State == Void {
   public convenience init(_ store: Store<Void, Action>) {
     self.init(store, removeDuplicates: ==)
+  }
+}
+
+// Adapted from https://www.onswiftwings.com/posts/share-replay-operator/
+public final class ReplaySubject<Output, Failure: Error>: Subject {
+  private var buffer = [Output]()
+  private let bufferSize: Int
+  private var subscriptions = [Subscription<Output, Failure>]()
+  private var completion: Subscribers.Completion<Failure>?
+  private let lock = NSRecursiveLock()
+
+  public init(_ bufferSize: Int = 0) {
+    self.bufferSize = bufferSize
+  }
+
+  public func send(subscription: Combine.Subscription) {
+    self.lock.self.lock()
+    defer { self.lock.unlock() }
+    subscription.request(.unlimited)
+  }
+
+  public func send(_ value: Output) {
+    self.lock.lock()
+    defer { self.lock.unlock() }
+    self.buffer.append(value)
+    self.buffer = self.buffer.suffix(bufferSize)
+    self.subscriptions.forEach { $0.receive(value) }
+  }
+
+  public func send(completion: Subscribers.Completion<Failure>) {
+    self.lock.lock()
+    defer { self.lock.unlock() }
+    self.completion = completion
+    self.subscriptions.forEach { $0.receive(completion: completion) }
+  }
+
+  public func receive<Downstream: Subscriber>(subscriber: Downstream)
+  where Downstream.Failure == Failure, Downstream.Input == Output {
+    self.lock.lock()
+    defer { self.lock.unlock() }
+    let subscription = Subscription(downstream: AnySubscriber(subscriber))
+    subscriber.receive(subscription: subscription)
+    self.subscriptions.append(subscription)
+    subscription.replay(self.buffer, completion: self.completion)
+  }
+
+  private final class Subscription<Output, Failure: Error>: Combine.Subscription {
+    private let downstream: AnySubscriber<Output, Failure>
+    private var isCompleted = false
+    private var demand: Subscribers.Demand = .none
+
+    init(downstream: AnySubscriber<Output, Failure>) {
+      self.downstream = downstream
+    }
+
+    func request(_ newDemand: Subscribers.Demand) {
+      self.demand += newDemand
+    }
+
+    func cancel() {
+      self.isCompleted = true
+    }
+
+    public func receive(_ value: Output) {
+      guard !self.isCompleted, self.demand > 0 else { return }
+      self.demand += self.downstream.receive(value)
+      self.demand -= 1
+    }
+
+    public func receive(completion: Subscribers.Completion<Failure>) {
+      guard !self.isCompleted else { return }
+      self.isCompleted = true
+      self.downstream.receive(completion: completion)
+    }
+
+    public func replay(_ values: [Output], completion: Subscribers.Completion<Failure>?) {
+      guard !isCompleted else { return }
+      values.forEach { value in self.receive(value) }
+      if let completion = completion { self.receive(completion: completion) }
+    }
+  }
+}
+
+extension Publisher {
+  public func shareReplay(
+    _ bufferSize: Int
+  )
+    -> Publishers.Autoconnect<Publishers.Multicast<Self, ReplaySubject<Output, Failure>>>
+  {
+    self
+      .multicast(subject: ReplaySubject(bufferSize))
+      .autoconnect()
   }
 }
