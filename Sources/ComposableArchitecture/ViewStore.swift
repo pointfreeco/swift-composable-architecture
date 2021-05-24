b2073b06b9e2c4f7ae996aab38368ac5bdb2cad1
Stephen Celis <stephen@stephencelis.com>
2021-05-24 12:00:06 -0700

    fix

--- Sources/ComposableArchitecture/ViewStore.swift
+++ Sources/ComposableArchitecture/ViewStore.swift
@@ -58,26 +58,14 @@ public final class ViewStore<State, Action>: ObservableObject {
   ///   - store: A store.
   ///   - isDuplicate: A function to determine when two `State` values are equal. When values are
   ///     equal, repeat view computations are removed.
-  public convenience init(
+  public init(
     _ store: Store<State, Action>,
     removeDuplicates isDuplicate: @escaping (State, State) -> Bool
   ) {
     let publisher = store.state.removeDuplicates(by: isDuplicate)
-    self.init(
-      publisher: publisher,
-      state: store.state.value,
-      send: store.send
-    )
-  }
-
-  private init<P: Publisher>(
-    publisher: P,
-    state: State,
-    send: @escaping (Action) -> Void
-  ) where P.Output == State, P.Failure == Never {
     self.publisher = StorePublisher(publisher)
-    self.state = state
-    self._send = send
+    self.state = store.state.value
+    self._send = store.send
     self.viewCancellable = publisher.sink { [weak self] in self?.state = $0 }
   }
 
