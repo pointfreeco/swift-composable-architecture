ef45d038fcd824040b1b99a634d10df7d7fa7c93
Stephen Celis <stephen@stephencelis.com>
2021-07-07 15:35:38 -0400

    Revert code that snuck in #267 (#635)
    Had some debug code that puzzled me while testing some of the Case Path
    changes today.

--- Examples/CaseStudies/SwiftUICaseStudies/04-HigherOrderReducers-Recursion.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/04-HigherOrderReducers-Recursion.swift
@@ -34,7 +34,6 @@ struct NestedState: Equatable, Identifiable {
 
 indirect enum NestedAction: Equatable {
   case append
-  case exclaim
   case node(index: Int, action: NestedAction)
   case remove(IndexSet)
   case rename(String)
@@ -52,10 +51,6 @@ let nestedReducer = Reducer<
     state.children.append(NestedState(id: environment.uuid()))
     return .none
 
-  case .exclaim:
-    state.description += "!"
-    return .none
-
   case let .node(index, action):
     return self.run(&state.children[index], action, environment)
       .map { .node(index: index, action: $0) }
@@ -65,11 +60,8 @@ let nestedReducer = Reducer<
     return .none
 
   case let .rename(name):
-    struct ExclaimId: Hashable {}
-
     state.description = name
-    return Effect(value: .exclaim)
-      .debounce(id: ExclaimId(), for: 1, scheduler: DispatchQueue.main)
+    return .none
   }
 }
 
