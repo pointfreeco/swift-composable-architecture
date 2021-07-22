a04ec640cc3d28c1cc5ad36d37160d735a6a6e37
Stephen Celis <stephen@stephencelis.com>
2021-07-22 09:55:59 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
   dependencies: [
     .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.5.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.4.0"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", .branch("main")),
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),
   ],
