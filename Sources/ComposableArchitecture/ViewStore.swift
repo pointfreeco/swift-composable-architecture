0f73f4a76bc85bb1a667d98c733f8ef711b1c2ca
Stephen Celis <stephen@stephencelis.com>
2021-08-02 10:55:15 -0400

    Update ViewStore.swift

--- Sources/ComposableArchitecture/ViewStore.swift
+++ Sources/ComposableArchitecture/ViewStore.swift
@@ -47,9 +47,9 @@ import SwiftUI
 ///
 /// ### Thread safety
 ///
-/// The ``ViewStore`` class is not thread-safe, and all interactions with it must happen on the main
-/// thread. See the documentation of the ``Store`` class for more information why this decision was
-/// made.
+/// The ``ViewStore`` class is not thread-safe, and all interactions with it must happen on the same
+/// thread its store runs on, which is typically the main thread for UI-based applications. See the
+/// documentation of the ``Store`` class for more information why this decision was made.
 @dynamicMemberLookup
 public final class ViewStore<State, Action>: ObservableObject {
   // N.B. `ViewStore` does not use a `@Published` property, so `objectWillChange`
