dd149979a90d1275590d919de7865debf6819095
Brandon Williams <mbrandonw@hey.com>
2021-08-12 16:03:29 -0400

    docs

--- Sources/ComposableArchitecture/Beta/Concurrency.swift
+++ Sources/ComposableArchitecture/Beta/Concurrency.swift
@@ -4,9 +4,6 @@ import SwiftUI
 #if compiler(>=5.5)
   @available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
   extension Effect {
-    // TODO: Document that this should only be used in live dependencies, since async work cannot
-    // currently be made synchronous
-    
     /// Wraps an asynchronous unit of work in an effect.
     ///
     /// This function is useful for executing work in an asynchronous context and capture the result
@@ -14,9 +11,24 @@ import SwiftUI
     ///
     /// ```swift
     /// Effect.task {
-    ///   await
+    ///   guard case let .some((data, _)) = try? await URLSession.shared
+    ///     .data(from: .init(string: "http://numbersapi.com/42")!)
+    ///   else {
+    ///     return "Could not load"
+    ///   }
+    ///
+    ///   return String(decoding: data, as: UTF8.self)
     /// }
     /// ```
+    ///
+    /// Note that due to the lack of tools to control the execution of asynchronous work in Swift,
+    /// it is not recommended to use this function in reducers directly. Doing so will introduce
+    /// thread hops into your effects that will make testing difficult. You will be responsible for
+    /// adding explicit expectations to wait for small amounts of time so that effects can deliver
+    /// their output.
+    ///
+    /// Instead, this function is most helpful for calling `async`/`await` functions from the live
+    /// implementation of dependencies, such as `URLSession.data`, `MKLocalSearch.start` and more.
     ///
     /// - Parameters:
     ///   - priority: Priority of the underlying task. If `nil`, the priority will come from
@@ -44,6 +56,24 @@ import SwiftUI
     ///
     /// This function is useful for executing work in an asynchronous context and capture the result
     /// in an ``Effect`` so that the reducer, a non-asynchronous context, can process it.
+    ///
+    /// ```swift
+    /// Effect.task {
+    ///   let (data, _) = try await URLSession.shared
+    ///     .data(from: .init(string: "http://numbersapi.com/42")!)
+    ///
+    ///   return String(decoding: data, as: UTF8.self)
+    /// }
+    /// ```
+    ///
+    /// Note that due to the lack of tools to control the execution of asynchronous work in Swift,
+    /// it is not recommended to use this function in reducers directly. Doing so will introduce
+    /// thread hops into your effects that will make testing difficult. You will be responsible for
+    /// adding explicit expectations to wait for small amounts of time so that effects can deliver
+    /// their output.
+    ///
+    /// Instead, this function is most helpful for calling `async`/`await` functions from the live
+    /// implementation of dependencies, such as `URLSession.data`, `MKLocalSearch.start` and more.
     ///
     /// - Parameters:
     ///   - priority: Priority of the underlying task. If `nil`, the priority will come from
