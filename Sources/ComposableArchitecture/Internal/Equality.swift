6aaf4024d70c6a48e0d5302f16fc189afb04ba83
Stephen Celis <stephen@stephencelis.com>
2021-08-18 10:21:30 -0400

    wip

--- Sources/ComposableArchitecture/Internal/Equality.swift
+++ Sources/ComposableArchitecture/Internal/Equality.swift
@@ -1,18 +1,14 @@
-@inlinable
 func isEqual<T>(_ lhs: T, _ rhs: T) -> Bool {
   var lhs = lhs
   var rhs = rhs
   return memcmp(&lhs, &rhs, MemoryLayout<T>.size) == 0
     || (Box<T>.self as? AnyEquatable.Type)?.isEqual(lhs, rhs) == true
 }
-@usableFromInline
-protocol AnyEquatable {
+private protocol AnyEquatable {
   static func isEqual<T>(_ lhs: T, _ rhs: T) -> Bool
 }
-@usableFromInline
-enum Box<PossiblyEquatable> {}
+private enum Box<PossiblyEquatable> {}
 extension Box: AnyEquatable where PossiblyEquatable: Equatable {
-  @inlinable
   static func isEqual<T>(_ lhs: T, _ rhs: T) -> Bool {
     lhs as? PossiblyEquatable == rhs as? PossiblyEquatable
   }
