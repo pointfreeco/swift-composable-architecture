6278cb65ea16badbf19743e882e301d16762df58
Pat Brown <mrpatbrown@gmail.com>
2021-09-03 21:28:39 +1000

    formatting

--- Sources/ComposableArchitecture/SwiftUI/ObservedViewStore.swift
+++ Sources/ComposableArchitecture/SwiftUI/ObservedViewStore.swift
@@ -3,33 +3,37 @@ import SwiftUI
 
 @propertyWrapper
 public struct ObservedViewStore<State, Action>: DynamicProperty {
+  @ObservedObject public var wrappedValue: ViewStore<State, Action>
+
   public init(wrappedValue viewStore: ViewStore<State, Action>) {
     self._wrappedValue = .init(wrappedValue: viewStore)
     viewStore.observedViewStore = self
   }
 
-  @ObservedObject public var wrappedValue: ViewStore<State, Action>
-
   public var projectedValue: ObservedObject<ViewStore<State, Action>>.Wrapper {
-    $wrappedValue
+    self.$wrappedValue
   }
 
-  func projectedBinding<Value>(for keyPath: WritableKeyPath<State, BindableState<Value>>) -> Binding<Value>
+  fileprivate func projectedBinding<Value>(
+    for keyPath: WritableKeyPath<State, BindableState<Value>>
+  ) -> Binding<Value>
     where Action: BindableAction, Action.State == State, Value: Equatable
   {
-    return $wrappedValue[binding: keyPath]
+    self.$wrappedValue[bindable: keyPath]
   }
 }
 
 extension ViewStore {
-  func projectedBinding<Value>(for keyPath: WritableKeyPath<State, BindableState<Value>>) -> Binding<Value>?
+  func projectedBinding<Value>(
+    for keyPath: WritableKeyPath<State, BindableState<Value>>
+  ) -> Binding<Value>?
     where Action: BindableAction, Action.State == State, Value: Equatable
   {
     self.observedViewStore?.projectedBinding(for: keyPath)
   }
 
   subscript<Value>(
-    binding keyPath: WritableKeyPath<State, BindableState<Value>>
+    bindable keyPath: WritableKeyPath<State, BindableState<Value>>
   ) -> Value
     where Action: BindableAction, Action.State == State, Value: Equatable
   {
