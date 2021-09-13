637706c304937ff72c74de18a87c966115f2f9bf
Brandon Williams <mbrandonw@hey.com>
2021-09-13 17:16:20 -0400

    clean up

--- Sources/ComposableArchitecture/Reducers/_Reducer.swift
+++ Sources/ComposableArchitecture/Reducers/_Reducer.swift
@@ -5,13 +5,4 @@ public protocol _Reducer {
   func reduce(into state: inout State, action: Action) -> Effect<Action, Never>
 }
 
-// store.environment(\.apiClient.endpoint) { _ in Effect(value: .success(1)) } <-
-// reducer.environment(\.apiClient.endpoint) { _ in Effect(value: .success(1)) }
-
-// @Dependency
-// @EnvironmentDependecy
-
-// dependencies as actors
-// @MainActor class Store
-
 // TODO: ReducerModifier??
