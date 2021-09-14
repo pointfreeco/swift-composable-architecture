5d0355cd4abe22cd672e88269eb34e8914b5bd40
Stephen Celis <stephen@stephencelis.com>
2021-09-14 19:40:01 -0400

    wip

--- Tests/ComposableArchitectureTests/EffectTests.swift
+++ Tests/ComposableArchitectureTests/EffectTests.swift
@@ -210,7 +210,7 @@ final class EffectTests: XCTestCase {
     }
   #endif
 
-  #if canImport(_Concurrency)
+  #if compiler(>=5.5) && canImport(_Concurrency)
     func testTask() {
       guard #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) else { return }
 
