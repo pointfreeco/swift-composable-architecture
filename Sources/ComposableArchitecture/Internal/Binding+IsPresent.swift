04eb8259185ae0075d47271da2b71fdee8e727e1
Stephen Celis <stephen@stephencelis.com>
2021-09-14 18:36:28 -0400

    Feedback

--- Sources/ComposableArchitecture/Internal/Binding+IsPresent.swift
+++ Sources/ComposableArchitecture/Internal/Binding+IsPresent.swift
@@ -4,12 +4,9 @@ extension Binding {
   func isPresent<Wrapped>() -> Binding<Bool> where Value == Wrapped? {
     .init(
       get: { self.wrappedValue != nil },
-      set: { isPresent in
-        if !isPresent {
-          withTransaction(self.transaction) {
-            self.wrappedValue = nil
-          }
-        }
+      set: { isPresent, transaction in
+        guard !isPresent else { return }
+        self.transaction(transaction).wrappedValue = nil
       }
     )
   }
