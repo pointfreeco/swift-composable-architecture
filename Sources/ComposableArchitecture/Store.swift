d715300cd4156dbc002df96fddaf5e6110b85bcc
Stephen Celis <stephen@stephencelis.com>
2021-09-16 11:11:13 -0400

    Thread messaging (#803)
    * Use thread dictionary for check instead
    
    * Improve thread check messaging for Store.scope
    
    * typo fix
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>

--- Sources/ComposableArchitecture/Store.swift
+++ Sources/ComposableArchitecture/Store.swift
@@ -282,6 +282,7 @@ public final class Store<State, Action> {
     state toLocalState: @escaping (State) -> LocalState,
     action fromLocalAction: @escaping (LocalAction) -> Action
   ) -> Store<LocalState, LocalAction> {
+    self.threadCheck(status: .scope)
     var isSending = false
     let localStore = Store<LocalState, LocalAction>(
       initialState: toLocalState(self.state.value),
@@ -416,6 +417,7 @@ public final class Store<State, Action> {
 
   private enum ThreadCheckStatus {
     case effectCompletion(Action)
+    case scope
     case send(Action, isFromViewStore: Bool)
   }
 
@@ -429,22 +431,28 @@ public final class Store<State, Action> {
       switch status {
       case let .effectCompletion(action):
         message = """
-          An effect returned from the action "\(debugCaseOutput(action))" completed on the \
-          wrong thread. Make sure to use ".receive(on:)" on any effects that execute on background \
+          An effect returned from the action "\(debugCaseOutput(action))" completed on the wrong \
+          thread. Make sure to use ".receive(on:)" on any effects that execute on background \
           threads to receive their output on the same thread the store was created on.
+          """
+
+      case .scope:
+        message = """
+          "Store.scope" was called on the wrong thread. Make sure that "Store.scope" is always \
+          called on the same thread the store was created on.
           """
 
       case let .send(action, isFromViewStore: true):
         message = """
-          "ViewStore.send(\(debugCaseOutput(action)))" was called on the wrong thread. Make \
-          sure that "ViewStore.send" is always called on the same thread the store was created on.
+          "ViewStore.send(\(debugCaseOutput(action)))" was called on the wrong thread. Make sure \
+          that "ViewStore.send" is always called on the same thread the store was created on.
           """
 
       case let .send(action, isFromViewStore: false):
         message = """
-          An effect emitted the action "\(debugCaseOutput(action))" from the wrong thread. Make sure \
-          to use ".receive(on:)" on any effects that execute on background threads to receive their \
-          output on the same thread the store was created on.
+          An effect emitted the action "\(debugCaseOutput(action))" from the wrong thread. Make \
+          sure to use ".receive(on:)" on any effects that execute on background threads to receive \
+          their output on the same thread the store was created on.
           """
       }
 
