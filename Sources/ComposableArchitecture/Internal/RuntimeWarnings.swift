c4089ca1138ca13f32334aacd8b05872073ec594
Stephen Celis <stephen@stephencelis.com>
2021-09-17 16:31:03 -0400

    note

--- Sources/ComposableArchitecture/Internal/RuntimeWarnings.swift
+++ Sources/ComposableArchitecture/Internal/RuntimeWarnings.swift
@@ -1,6 +1,11 @@
 import OSLog
 
 #if DEBUG
+  // NB: Xcode runtime warnings offer a much better experience than traditional assertions and
+  //     breakpoints, but Apple provides no means of creating custom runtime warnings ourselves.
+  //     To work around this, we hook into SwiftUI's runtime issue delivery mechanism, instead.
+  //
+  // Feedback filed: https://gist.github.com/stephencelis/a8d06383ed6ccde3e5ef5d1b3ad52bbc
   let rw = (
     dso: { () -> UnsafeMutableRawPointer in
       var info = Dl_info()
