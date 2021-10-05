e3190721233ea29bb042d66bec595128ffe2db01
Brandon Williams <mbrandonw@hey.com>
2021-10-05 13:13:31 -0400

    wip

--- Sources/ComposableArchitecture/Internal/RuntimeWarnings.swift
+++ Sources/ComposableArchitecture/Internal/RuntimeWarnings.swift
@@ -9,7 +9,13 @@
   let rw = (
     dso: { () -> UnsafeMutableRawPointer in
       var info = Dl_info()
-      dladdr(dlsym(dlopen(nil, RTLD_LAZY), "$s7SwiftUI4TextV8verbatimACSS_tcfC"), &info)
+      dladdr(
+        dlsym(
+          dlopen(nil, RTLD_LAZY),
+          "$s10Foundation15AttributeScopesO7SwiftUIE05swiftE0AcDE0D12UIAttributesVmvg"
+        ),
+        &info
+      )
       return info.dli_fbase
     }(),
     log: OSLog(subsystem: "com.apple.runtime-issues", category: "ComposableArchitecture")
