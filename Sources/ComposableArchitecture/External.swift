ba7f1c30b524d6388a319cc4ca294e92403d481a
Stephen Celis <stephen@stephencelis.com>
2021-12-14 11:42:58 -0500

    round out

--- Sources/ComposableArchitecture/External.swift
+++ Sources/ComposableArchitecture/External.swift
@@ -21,7 +21,47 @@ extension TestScheduler {
 // swift-concurrency-extensions
 
 import XCTestDynamicOverlay
-import Combine
+
+extension AsyncStream {
+  public init(_ build: @escaping (Continuation) async -> Void) {
+    self.init { continuation in
+      Task {
+        await build(continuation)
+        continuation.finish()
+      }
+    }
+  }
+
+  // TODO: Is this name right for vanilla?
+  public static func passthrough() -> (continuation: Continuation, stream: Self) {
+    var continuation: Continuation!
+    let stream = Self { continuation = $0 }
+    return (continuation, stream)
+  }
+
+  public static func yielding(_ element: Element) -> Self {
+    Self {
+      $0.yield(element)
+      $0.finish()
+    }
+  }
+
+  // TODO: Is this name right for vanilla?
+  public static func empty(finishImmediately: Bool = true) -> Self {
+    self.init {
+      if finishImmediately {
+        $0.finish()
+      }
+    }
+  }
+
+  public static func failing(_ message: String) -> Self {
+    .init {
+      XCTFail("Unimplemented: \(message)")
+      return nil
+    }
+  }
+}
 
 extension AsyncThrowingStream where Failure == Error {
   public init(_ build: @escaping (Continuation) async throws -> Void) {
@@ -37,6 +77,13 @@ extension AsyncThrowingStream where Failure == Error {
     }
   }
 
+  // TODO: Is this name right for vanilla?
+  public static func passthrough() -> (continuation: Continuation, stream: Self) {
+    var continuation: Continuation!
+    let stream = Self { continuation = $0 }
+    return (continuation, stream)
+  }
+
   public static func yielding(_ element: Element) -> Self {
     Self {
       $0.yield(element)
@@ -48,9 +95,10 @@ extension AsyncThrowingStream where Failure == Error {
     self.init { $0.finish(throwing: error) }
   }
 
-  public static func empty(completeImmediately: Bool = true) -> Self {
+  // TODO: Is this name right for vanilla?
+  public static func empty(finishImmediately: Bool = true) -> Self {
     self.init {
-      if completeImmediately {
+      if finishImmediately {
         $0.finish()
       }
     }
@@ -62,56 +110,54 @@ extension AsyncThrowingStream where Failure == Error {
       return nil
     }
   }
-
-  public static func passthrough() -> (continuation: Self.Continuation, stream: Self) {
-    var c: Continuation!
-    let s = Self { c = $0 }
-    return (c, s)
-  }
 }
 
-extension AsyncStream {
-  public var publisher: AnyPublisher<Element, Never> {
-    let subject = PassthroughSubject<Element, Never>()
+#if canImport(Combine)
+  import Combine
 
-    let task = Task {
-      await withTaskCancellationHandler {
-        subject.send(completion: .finished)
-      } operation: {
-        for await element in self {
-          subject.send(element)
-        }
-        subject.send(completion: .finished)
-      }
-    }
+  extension AsyncStream {
+    public var publisher: AnyPublisher<Element, Never> {
+      let subject = PassthroughSubject<Element, Never>()
 
-    return subject
-      .handleEvents(receiveCancel: task.cancel)
-      .eraseToAnyPublisher()
-  }
-}
-
-extension AsyncSequence {
-  public var publisher: AnyPublisher<Element, Error> {
-    let subject = PassthroughSubject<Element, Error>()
-
-    let task = Task {
-      do {
-        try await withTaskCancellationHandler {
+      let task = Task {
+        await withTaskCancellationHandler {
           subject.send(completion: .finished)
         } operation: {
-          for try await element in self {
+          for await element in self {
             subject.send(element)
           }
           subject.send(completion: .finished)
         }
-      } catch {
-        subject.send(completion: .failure(error))
       }
-    }
 
-    return subject
-      .handleEvents(receiveCancel: task.cancel)
-      .eraseToAnyPublisher()
+      return subject
+        .handleEvents(receiveCancel: task.cancel)
+        .eraseToAnyPublisher()
+    }
   }
-}
+
+  extension AsyncSequence {
+    public var publisher: AnyPublisher<Element, Error> {
+      let subject = PassthroughSubject<Element, Error>()
+
+      let task = Task {
+        do {
+          try await withTaskCancellationHandler {
+            subject.send(completion: .finished)
+          } operation: {
+            for try await element in self {
+              subject.send(element)
+            }
+            subject.send(completion: .finished)
+          }
+        } catch {
+          subject.send(completion: .failure(error))
+        }
+      }
+
+      return subject
+        .handleEvents(receiveCancel: task.cancel)
+        .eraseToAnyPublisher()
+    }
+  }
+#endif
