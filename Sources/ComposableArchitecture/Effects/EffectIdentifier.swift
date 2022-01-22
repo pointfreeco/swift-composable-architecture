d692a37924fe10864044497920111f8b1186a0e7
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-01-22 16:54:27 -0300

    Add tests and runtime warnings

--- Sources/ComposableArchitecture/Effects/EffectIdentifier.swift
+++ Sources/ComposableArchitecture/Effects/EffectIdentifier.swift
@@ -1,17 +1,19 @@
 import Foundation
+
 #if DEBUG
-import os
+  import os
 #endif
+
 @propertyWrapper
 public struct EffectID: Hashable {
   static var currentContextID: AnyHashable? {
     Thread.current.threadDictionary.value(forKey: currentContextKey) as? AnyHashable
   }
-  
+
   @usableFromInline
   static let currentContextKey = "swift-composable-architecture:currentContext"
   private let effectIdentifier: EffectIdentifier
-  
+
   public var wrappedValue: EffectIdentifier {
     effectIdentifier.with(contextID: Self.currentContextID)
   }
@@ -49,7 +51,7 @@ public struct EffectIdentifier: Hashable {
   let file: String
   let line: UInt
   let column: UInt
-  
+
   internal init(
     contextID: AnyHashable? = nil,
     userData: AnyHashable? = nil,
@@ -62,26 +64,64 @@ public struct EffectIdentifier: Hashable {
     self.file = "\(file)"
     self.line = line
     self.column = column
-    #if DEBUG
-      if contextID == nil {
-        os_log(
-          .fault, dso: rw.dso, log: rw.log,
-          """
-          An `@EffectID` declared at "%@:%d" was accessed outside of a reducer's context.
-          
-          `@EffectID` identifiers should only be accessed by `Reducer`'s while they're receiving \
-          an action.
-          """,
-          "\(file)",
-          line
-        )
-      }
-    #endif
   }
-  
+
   func with(contextID: AnyHashable?) -> Self {
     var identifier = self
     identifier.contextID = contextID
     return identifier
   }
+
+  public static func == (lhs: EffectIdentifier, rhs: EffectIdentifier) -> Bool {
+    #if DEBUG
+      if lhs.contextID == nil || rhs.contextID == nil {
+        func issueWarningIfNeeded(id: EffectIdentifier) {
+          guard id.contextID == nil else { return }
+          let warningID = WarningID(file: id.file, line: id.line, column: id.column)
+          guard
+            Self.issuedWarningsLock.sync(work: {
+              guard !issuedWarnings.contains(warningID) else { return false }
+              issuedWarnings.insert(warningID)
+              return true
+            })
+          else {
+            return
+          }
+          os_log(
+            .fault, dso: rw.dso, log: rw.log,
+            """
+            An `@EffectID` declared at "%@:%d" was accessed outside of a reducer's context.
+
+            `@EffectID` identifiers should only be accessed by `Reducer`'s while they're receiving \
+            an action.
+            """,
+            "\(id.file)",
+            id.line
+          )
+        }
+        issueWarningIfNeeded(id: lhs)
+        issueWarningIfNeeded(id: rhs)
+      }
+    #endif
+    guard
+      lhs.file == rhs.file,
+      lhs.line == rhs.line,
+      lhs.column == rhs.column,
+      lhs.contextID == rhs.contextID,
+      lhs.userData == rhs.userData
+    else {
+      return false
+    }
+    return true
+  }
+
+  #if DEBUG
+    static var issuedWarningsLock = NSRecursiveLock()
+    static var issuedWarnings = Set<WarningID>()
+    struct WarningID: Hashable {
+      let file: String
+      let line: UInt
+      let column: UInt
+    }
+  #endif
 }
---
+++ Tests/ComposableArchitectureTests/EffectIdentifierTests.swift
@@ -0,0 +1,48 @@
+import XCTest
+
+@testable import ComposableArchitecture
+
+final class EffectIdentifierTests: XCTestCase {
+  @EffectID var id1
+  @EffectID var id2_1 = 1
+  @EffectID var id2_2 = 1
+
+  override func setUp() {
+    super.setUp()
+    // Set a context to avoid runtime warnings
+    Thread.current.threadDictionary.setValue(0, forKey: EffectID.currentContextKey)
+  }
+
+  override func tearDown() {
+    Thread.current.threadDictionary.setValue(nil, forKey: EffectID.currentContextKey)
+    super.tearDown()
+  }
+
+  func testEffectIdentifierEquality() {
+    XCTAssertEqual(id1, id1)
+    XCTAssertEqual(id2_1, id2_1)
+    XCTAssertEqual(id2_2, id2_2)
+
+    XCTAssertNotEqual(id1, id2_1)
+    XCTAssertNotEqual(id1, id2_2)
+    XCTAssertNotEqual(id2_1, id2_2)
+  }
+
+  #if compiler(>=5.4)
+    func testLocalEffectIdentifierEquality() {
+      @EffectID var id1
+      @EffectID var id2_1 = 1
+      @EffectID var id2_2 = 1
+
+      XCTAssertEqual(id1, id1)
+      XCTAssertEqual(id2_1, id2_1)
+      XCTAssertEqual(id2_2, id2_2)
+
+      XCTAssertNotEqual(id1, id2_1)
+      XCTAssertNotEqual(id1, id2_2)
+      XCTAssertNotEqual(id2_1, id2_2)
+
+      XCTAssertNotEqual(id1, self.id1)
+    }
+  #endif
+}
