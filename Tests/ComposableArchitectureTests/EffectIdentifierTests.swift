539e74cd2d70b3cc850717786a6a0c9ea0ca0e69
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-01-22 18:02:56 -0300

    Fix tests on Xcode 12.4
    Older versions of Xcode seem to have difficulties with property wrappers and conditional compilation when building tests.
    All tests for `EffectIdentifier` are moved to Swift > 5.4

--- Tests/ComposableArchitectureTests/EffectIdentifierTests.swift
+++ Tests/ComposableArchitectureTests/EffectIdentifierTests.swift
@@ -1,34 +1,33 @@
-import XCTest
+#if compiler(>=5.4)
+  import XCTest
 
-@testable import ComposableArchitecture
+  @testable import ComposableArchitecture
+  final class EffectIdentifierTests: XCTestCase {
+    @EffectID var id1
+    @EffectID var id2_1 = 1
+    @EffectID var id2_2 = 1
 
-final class EffectIdentifierTests: XCTestCase {
-  @EffectID var id1
-  @EffectID var id2_1 = 1
-  @EffectID var id2_2 = 1
+    override func setUp() {
+      super.setUp()
+      // Set a context to avoid runtime warnings
+      Thread.current.threadDictionary.setValue(0, forKey: EffectID.currentContextKey)
+    }
 
-  override func setUp() {
-    super.setUp()
-    // Set a context to avoid runtime warnings
-    Thread.current.threadDictionary.setValue(0, forKey: EffectID.currentContextKey)
-  }
+    override func tearDown() {
+      Thread.current.threadDictionary.setValue(nil, forKey: EffectID.currentContextKey)
+      super.tearDown()
+    }
 
-  override func tearDown() {
-    Thread.current.threadDictionary.setValue(nil, forKey: EffectID.currentContextKey)
-    super.tearDown()
-  }
+    func testEffectIdentifierEquality() {
+      XCTAssertEqual(id1, id1)
+      XCTAssertEqual(id2_1, id2_1)
+      XCTAssertEqual(id2_2, id2_2)
 
-  func testEffectIdentifierEquality() {
-    XCTAssertEqual(id1, id1)
-    XCTAssertEqual(id2_1, id2_1)
-    XCTAssertEqual(id2_2, id2_2)
+      XCTAssertNotEqual(id1, id2_1)
+      XCTAssertNotEqual(id1, id2_2)
+      XCTAssertNotEqual(id2_1, id2_2)
+    }
 
-    XCTAssertNotEqual(id1, id2_1)
-    XCTAssertNotEqual(id1, id2_2)
-    XCTAssertNotEqual(id2_1, id2_2)
-  }
-
-  #if compiler(>=5.4)
     func testLocalEffectIdentifierEquality() {
       @EffectID var id1
       @EffectID var id2_1 = 1
@@ -44,5 +43,5 @@ final class EffectIdentifierTests: XCTestCase {
 
       XCTAssertNotEqual(id1, self.id1)
     }
-  #endif
-}
+  }
+#endif
