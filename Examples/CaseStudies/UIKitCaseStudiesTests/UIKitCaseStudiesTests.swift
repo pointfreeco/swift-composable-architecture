ca21ef9d6b25e19b2d6de4b91f9ca4947b6b53b2
bjford <me.bjdev@outlook.com>
2022-02-15 21:50:57 +0100

    Add unit tests to UIKitCaseStudies

--- Examples/CaseStudies/UIKitCaseStudiesTests/UIKitCaseStudiesTests.swift
+++ Examples/CaseStudies/UIKitCaseStudiesTests/UIKitCaseStudiesTests.swift
@@ -1,6 +1,56 @@
+import ComposableArchitecture
 import XCTest
 
 @testable import UIKitCaseStudies
 
-class UIKitCaseStudiesTests: XCTestCase {
+final class UIKitCaseStudiesTests: XCTestCase {
+  func testCountDown() {
+    let store = TestStore(
+      initialState: CounterState(),
+      reducer: counterReducer,
+      environment: CounterEnvironment()
+    )
+
+    store.send(.incrementButtonTapped) {
+      $0.count = 1
+    }
+    store.send(.decrementButtonTapped) {
+      $0.count = 0
+    }
+  }
+
+  func testCountDownList() {
+    let firstState = CounterState()
+    let secondState = CounterState()
+    let thirdState = CounterState()
+
+    let store = TestStore(
+      initialState: CounterListState(
+        counters: [firstState, secondState, thirdState]
+      ),
+      reducer: counterListReducer,
+      environment: CounterListEnvironment()
+    )
+
+    store.send(.counter(id: firstState.id, action: .incrementButtonTapped)) {
+      $0.counters[id: firstState.id]?.count = 1
+    }
+    store.send(.counter(id: firstState.id, action: .decrementButtonTapped)) {
+      $0.counters[id: firstState.id]?.count = 0
+    }
+
+    store.send(.counter(id: secondState.id, action: .incrementButtonTapped)) {
+      $0.counters[id: secondState.id]?.count = 1
+    }
+    store.send(.counter(id: secondState.id, action: .decrementButtonTapped)) {
+      $0.counters[id: secondState.id]?.count = 0
+    }
+
+    store.send(.counter(id: thirdState.id, action: .incrementButtonTapped)) {
+      $0.counters[id: thirdState.id]?.count = 1
+    }
+    store.send(.counter(id: thirdState.id, action: .decrementButtonTapped)) {
+      $0.counters[id: thirdState.id]?.count = 0
+    }
+  }
 }
