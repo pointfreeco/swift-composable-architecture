e828aeb49608858dcbe29435b005a4c9b8d33bd6
Pat Brown <mrpatbrown@gmail.com>
2022-02-25 05:23:26 +1100

    `Effect.cancel(ids:)` prefer sequence of ids over variadic list. (#1009)
    * Disfavor variadic cancellation
    
    * Changed ids paramter from array to sequence
    
    * Small typo

--- Sources/ComposableArchitecture/Effects/Cancellation.swift
+++ Sources/ComposableArchitecture/Effects/Cancellation.swift
@@ -89,16 +89,17 @@ extension Effect {
   /// - Parameter ids: A variadic list of effect identifiers.
   /// - Returns: A new effect that will cancel any currently in-flight effects with the given
   ///   identifiers.
+  @_disfavoredOverload
   public static func cancel(ids: AnyHashable...) -> Effect {
     .cancel(ids: ids)
   }
 
   /// An effect that will cancel multiple currently in-flight effects with the given identifiers.
   ///
-  /// - Parameter ids: An array of effect identifiers.
+  /// - Parameter ids: A sequence of effect identifiers.
   /// - Returns: A new effect that will cancel any currently in-flight effects with the given
   ///   identifiers.
-  public static func cancel(ids: [AnyHashable]) -> Effect {
+  public static func cancel<S: Sequence>(ids: S) -> Effect where S.Element == AnyHashable {
     .merge(ids.map(Effect.cancel(id:)))
   }
 }
