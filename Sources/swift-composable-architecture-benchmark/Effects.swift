393ca68cf3478e79c817a46d11b15f392d638f03
Pat Brown <mrpatbrown@gmail.com>
2022-03-13 09:14:07 +1100

    wip

--- Sources/swift-composable-architecture-benchmark/Effects.swift
+++ Sources/swift-composable-architecture-benchmark/Effects.swift
@@ -3,19 +3,19 @@ import ComposableArchitecture
 
 let effectsSuite = BenchmarkSuite(name: "Effects") { suite in
 
-  suite.benchmark(".merge([a, b])") {
+  suite.benchmark("merge([a, b])") {
     _ = Effect<Void, Never>.merge([.none, .none]).sink {}
   }
 
-  suite.benchmark(".merge(a, b)") {
+  suite.benchmark("merge(a, b)") {
     _ = Effect<Void, Never>.merge(.none, .none).sink {}
   }
 
-  suite.benchmark(".merge([a, b, c, d, e, f, g, h])") {
+  suite.benchmark("merge([a, b, c, d, e, f, g, h])") {
     _ = Effect<Void, Never>.merge([.none, .none, .none, .none, .none, .none, .none, .none]).sink {}
   }
 
-  suite.benchmark(".merge(a, b, c, d, e, f, g, h)") {
+  suite.benchmark("merge(a, b, c, d, e, f, g, h)") {
     _ = Effect<Void, Never>.merge(.none, .none, .none, .none, .none, .none, .none, .none).sink {}
   }
 }
