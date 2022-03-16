02ac00d4ed04017dfb399489fb7f000fc4538e37
Pat Brown <mrpatbrown@gmail.com>
2022-03-16 11:18:17 +1100

    Add transparent attribute to runtimeWarning (#1024)

--- Sources/ComposableArchitecture/Internal/RuntimeWarnings.swift
+++ Sources/ComposableArchitecture/Internal/RuntimeWarnings.swift
@@ -25,6 +25,7 @@
     log: OSLog(subsystem: "com.apple.runtime-issues", category: "ComposableArchitecture")
   )
 
+  @_transparent
   func runtimeWarning(
     _ message: StaticString,
     _ args: CVarArg...
