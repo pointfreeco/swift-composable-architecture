ea07f63f3d92b71c3288e7195ae18866bff1b5f2
stephencelis <stephencelis@users.noreply.github.com>
2022-03-29 21:17:14 +0000

    Run swift-format

--- Sources/ComposableArchitecture/Internal/Deprecations.swift
+++ Sources/ComposableArchitecture/Internal/Deprecations.swift
@@ -9,7 +9,8 @@ extension Effect {
   @available(
     *,
     deprecated,
-    message: "Using a variadic list is no longer supported. Use an array of identifiers instead. For more on this change, see: https://github.com/pointfreeco/swift-composable-architecture/pull/1041"
+    message:
+      "Using a variadic list is no longer supported. Use an array of identifiers instead. For more on this change, see: https://github.com/pointfreeco/swift-composable-architecture/pull/1041"
   )
   @_disfavoredOverload
   public static func cancel(ids: AnyHashable...) -> Effect {
