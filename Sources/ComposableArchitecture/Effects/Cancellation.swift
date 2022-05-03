bf521e9e5da3ea277df0548698b7dcf165c67b6e
Stephen Celis <stephen@stephencelis.com>
2022-05-03 17:35:09 -0400

    Improve performance of type-safe effect cancel token lookup (#1077)
    * Add type discriminator to effect cancel id hashing
    
    * wip

--- Sources/ComposableArchitecture/Effects/Cancellation.swift
+++ Sources/ComposableArchitecture/Effects/Cancellation.swift
@@ -37,6 +37,7 @@ extension Effect {
       cancellablesLock.lock()
       defer { cancellablesLock.unlock() }
 
+      let id = CancelToken(id: id)
       if cancelInFlight {
         cancellationCancellables[id]?.forEach { $0.cancel() }
       }
@@ -79,7 +80,7 @@ extension Effect {
   public static func cancel(id: AnyHashable) -> Effect {
     return .fireAndForget {
       cancellablesLock.sync {
-        cancellationCancellables[id]?.forEach { $0.cancel() }
+        cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() }
       }
     }
   }
@@ -94,5 +95,15 @@ extension Effect {
   }
 }
 
-var cancellationCancellables: [AnyHashable: Set<AnyCancellable>] = [:]
+struct CancelToken: Hashable {
+  let id: AnyHashable
+  let discriminator: ObjectIdentifier
+
+  init(id: AnyHashable) {
+    self.id = id
+    self.discriminator = ObjectIdentifier(type(of: id.base))
+  }
+}
+
+var cancellationCancellables: [CancelToken: Set<AnyCancellable>] = [:]
 let cancellablesLock = NSRecursiveLock()
