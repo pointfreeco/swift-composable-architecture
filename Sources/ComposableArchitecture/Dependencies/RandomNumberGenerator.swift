c0bd0e790f5e6fb9f97eebfa5c1e7957abd0ff4a
Stephen Celis <stephen@stephencelis.com>
2022-05-11 15:54:32 -0400

    wip

--- Sources/ComposableArchitecture/Dependencies/RandomNumberGenerator.swift
+++ Sources/ComposableArchitecture/Dependencies/RandomNumberGenerator.swift
@@ -1,12 +1,26 @@
+// TODO: Should this be `withRandomNumberGenerator: ((inout any RandomNumberGenerator) -> R) -> R`?
+
 extension DependencyValues {
   public var randomNumberGenerator: any RandomNumberGenerator {
     get { self[RandomNumberGeneratorKey.self] }
-    set { self[RandomNumberGeneratorKey.self] = newValue }
+    set { self[RandomNumberGeneratorKey.self] = BoxedRandomNumberGenerator(rng: newValue) }
   }
-}
 
-private enum RandomNumberGeneratorKey: LiveDependencyKey {
-  static let liveValue: any RandomNumberGenerator = SystemRandomNumberGenerator()
-  // TODO: Testable RNG
-  static let testValue: any RandomNumberGenerator = SystemRandomNumberGenerator()
+  private enum RandomNumberGeneratorKey: LiveDependencyKey {
+    static let liveValue: any RandomNumberGenerator = SystemRandomNumberGenerator()
+    // TODO: Testable or failing RNG
+    static let testValue: any RandomNumberGenerator = SystemRandomNumberGenerator()
+  }
+
+  private final class BoxedRandomNumberGenerator: RandomNumberGenerator {
+    var rng: any RandomNumberGenerator
+
+    init(rng: any RandomNumberGenerator) {
+      self.rng = rng
+    }
+
+    func next() -> UInt64 {
+      self.rng.next()
+    }
+  }
 }
