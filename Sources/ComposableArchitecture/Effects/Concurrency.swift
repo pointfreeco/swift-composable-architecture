636b2a2be6ec8f4208dff62d3cf7e065988e407d
Brandon Williams <mbrandonw@hey.com>
2022-05-12 16:24:24 -0700

    wip

--- Sources/ComposableArchitecture/Effects/Concurrency.swift
+++ Sources/ComposableArchitecture/Effects/Concurrency.swift
@@ -10,54 +10,6 @@ import SwiftUI
     ///
     /// ```swift
     /// Effect.task {
-    ///   guard case let .some((data, _)) = try? await URLSession.shared
-    ///     .data(from: .init(string: "http://numbersapi.com/42")!)
-    ///   else {
-    ///     return "Could not load"
-    ///   }
-    ///
-    ///   return String(decoding: data, as: UTF8.self)
-    /// }
-    /// ```
-    ///
-    /// Note that due to the lack of tools to control the execution of asynchronous work in Swift,
-    /// it is not recommended to use this function in reducers directly. Doing so will introduce
-    /// thread hops into your effects that will make testing difficult. You will be responsible
-    /// for adding explicit expectations to wait for small amounts of time so that effects can
-    /// deliver their output.
-    ///
-    /// Instead, this function is most helpful for calling `async`/`await` functions from the live
-    /// implementation of dependencies, such as `URLSession.data`, `MKLocalSearch.start` and more.
-    ///
-    /// - Parameters:
-    ///   - priority: Priority of the underlying task. If `nil`, the priority will come from
-    ///     `Task.currentPriority`.
-    ///   - operation: The operation to execute.
-    /// - Returns: An effect wrapping the given asynchronous work.
-    public static func task(
-      priority: TaskPriority? = nil,
-      operation: @escaping @Sendable () async -> Output
-    ) -> Self where Failure == Never {
-      var task: Task<Void, Never>?
-      return .future { callback in
-        task = Task(priority: priority) {
-          guard !Task.isCancelled else { return }
-          let output = await operation()
-          guard !Task.isCancelled else { return }
-          callback(.success(output))
-        }
-      }
-      .handleEvents(receiveCancel: { task?.cancel() })
-      .eraseToEffect()
-    }
-
-    /// Wraps an asynchronous unit of work in an effect.
-    ///
-    /// This function is useful for executing work in an asynchronous context and capture the
-    /// result in an ``Effect`` so that the reducer, a non-asynchronous context, can process it.
-    ///
-    /// ```swift
-    /// Effect.task {
     ///   let (data, _) = try await URLSession.shared
     ///     .data(from: .init(string: "http://numbersapi.com/42")!)
     ///
@@ -119,8 +71,65 @@ import SwiftUI
         .fireAndForget()
     }
   }
-#endif
 
+extension Effect where Failure == Never {
+  /// Wraps an asynchronous unit of work in an effect.
+  ///
+  /// This function is useful for executing work in an asynchronous context and capture the
+  /// result in an ``Effect`` so that the reducer, a non-asynchronous context, can process it.
+  ///
+  /// ```swift
+  /// Effect.task {
+  ///   guard case let .some((data, _)) = try? await URLSession.shared
+  ///     .data(from: .init(string: "http://numbersapi.com/42")!)
+  ///   else {
+  ///     return "Could not load"
+  ///   }
+  ///
+  ///   return String(decoding: data, as: UTF8.self)
+  /// }
+  /// ```
+  ///
+  /// Note that due to the lack of tools to control the execution of asynchronous work in Swift,
+  /// it is not recommended to use this function in reducers directly. Doing so will introduce
+  /// thread hops into your effects that will make testing difficult. You will be responsible
+  /// for adding explicit expectations to wait for small amounts of time so that effects can
+  /// deliver their output.
+  ///
+  /// Instead, this function is most helpful for calling `async`/`await` functions from the live
+  /// implementation of dependencies, such as `URLSession.data`, `MKLocalSearch.start` and more.
+  ///
+  /// - Parameters:
+  ///   - priority: Priority of the underlying task. If `nil`, the priority will come from
+  ///     `Task.currentPriority`.
+  ///   - operation: The operation to execute.
+  /// - Returns: An effect wrapping the given asynchronous work.
+  public static func task(
+    priority: TaskPriority? = nil,
+    operation: @escaping @Sendable () async -> Output
+  ) -> Self where Failure == Never {
+    Effect<Output, Error>.task(priority: priority, operation: operation)
+      .catch { _ in Empty() }
+      .eraseToEffect()
+  }
+
+  public static func task(
+    priority: TaskPriority? = nil,
+    operation: @escaping @Sendable () async throws -> Output,
+    `catch`: @escaping (Error) -> Output? = { _ in nil }
+  ) -> Self {
+    Effect<Output, Error>.task(priority: priority, operation: operation)
+      .catch { (error: Error) -> AnyPublisher<Output, Never> in
+        if let output = `catch`(error) {
+          return Just(output).eraseToAnyPublisher()
+        } else {
+          return Empty().eraseToAnyPublisher()
+        }
+      }
+      .eraseToEffect()
+  }
+}
+#endif
 
 import SwiftUI
 public struct Send<Action> {
@@ -135,6 +144,27 @@ public struct Send<Action> {
   }
   func pullback<NewAction>(_ f: @escaping (NewAction) -> Action) -> Send<NewAction> {
     .init(send: { newAction in self.send(f(newAction)) })
+  }
+}
+
+extension Effect where Failure == Error {
+  public static func run(
+    priority: TaskPriority? = nil,
+    _ operation: @escaping (_ send: Send<Output>) async throws -> Void
+  ) -> Self {
+    .run { subscriber in
+      let task = Task(priority: priority) {
+        do {
+          try await operation(Send(send: subscriber.send(_:)))
+        } catch {
+          subscriber.send(completion: .failure(error))
+        }
+        subscriber.send(completion: .finished)
+      }
+      return AnyCancellable {
+        task.cancel()
+      }
+    }
   }
 }
 
@@ -157,7 +187,7 @@ extension Effect where Failure == Never {
   public static func run(
     priority: TaskPriority? = nil,
     _ operation: @escaping (_ send: Send<Output>) async throws -> Void,
-    `catch`: @escaping (TaskFailure) -> Output? = { _ in nil }
+    `catch`: @escaping (Error) -> Output? = { _ in nil }
   ) -> Self {
     .run { subscriber in
       let task = Task(priority: priority) {
@@ -165,7 +195,7 @@ extension Effect where Failure == Never {
           let send = Send(send: subscriber.send(_:))
           try await operation(send)
         } catch {
-          if let output = `catch`(.failure(error)) {
+          if let output = `catch`(error) {
             subscriber.send(output)
           }
         }
@@ -177,5 +207,3 @@ extension Effect where Failure == Never {
     }
   }
 }
-
-
