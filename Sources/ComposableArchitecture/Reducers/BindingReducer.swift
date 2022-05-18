3516b0efd5e569a62f8a98b5bd8462be87fa77d2
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-05-18 12:50:18 -0300

    wip

--- Sources/ComposableArchitecture/Reducers/BindingReducer.swift
+++ Sources/ComposableArchitecture/Reducers/BindingReducer.swift
@@ -7,6 +7,11 @@ import SwiftUI
     public func binding() -> BindingReducer<Self> {
       .init(upstream: self)
     }
+    
+    @inlinable
+    public func bindingWithBody() -> BindingReducerWithBody<Self> {
+      .init(upstream: self)
+    }
   }
 
 public struct BindingReducer<Upstream: ReducerProtocol>: ReducerProtocol
@@ -30,6 +35,36 @@ where Upstream.Action: BindableAction, Upstream.State == Upstream.Action.State {
 
     bindingAction.set(&state)
     return self.upstream.reduce(into: &state, action: action)
+  }
+}
+
+@usableFromInline
+struct BindingActionReducer<State, Action>: ReducerProtocol
+where Action: BindableAction, Action.State == State {
+  @usableFromInline
+  init() {}
+  
+  @inlinable
+  func reduce(into state: inout State, action: Action) -> Effect<Action, Never> {
+    (/Action.binding).extract(from: action)?.set(&state)
+    return .none
+  }
+}
+
+public struct BindingReducerWithBody<Upstream: ReducerProtocol>: ReducerProtocol
+where Upstream.Action: BindableAction, Upstream.State == Upstream.Action.State {
+  @usableFromInline
+  let upstream: Upstream
+
+  @usableFromInline
+  init(upstream: Upstream) {
+    self.upstream = upstream
+  }
+  
+  @inlinable
+  public var body: some ReducerProtocol<Upstream.State, Upstream.Action> {
+    BindingActionReducer()
+    upstream
   }
 }
 #endif
--- Sources/swift-composable-architecture-benchmark/main.swift
+++ Sources/swift-composable-architecture-benchmark/main.swift
@@ -1,42 +1,70 @@
 import Benchmark
 import ComposableArchitecture
 
-let counterReducer = Reducer<Int, Bool, Void> { state, action, _ in
-  if action {
-    state += 1
-  } else {
-    state = 0
+
+//let counterReducer = Reducer<Int, Bool, Void> { state, action, _ in
+//  if action {
+//    state += 1
+//  } else {
+//    state = 0
+//  }
+//  return .none
+//}
+//
+//let store1 = Store(initialState: 0, reducer: counterReducer, environment: ())
+//let store2 = store1.scope { $0 }
+//let store3 = store2.scope { $0 }
+//let store4 = store3.scope { $0 }
+//
+//let viewStore1 = ViewStore(store1)
+//let viewStore2 = ViewStore(store2)
+//let viewStore3 = ViewStore(store3)
+//let viewStore4 = ViewStore(store4)
+//
+//benchmark("Scoping (1)") {
+//  viewStore1.send(true)
+//}
+//viewStore1.send(false)
+//
+//benchmark("Scoping (2)") {
+//  viewStore2.send(true)
+//}
+//viewStore1.send(false)
+//
+//benchmark("Scoping (3)") {
+//  viewStore3.send(true)
+//}
+//viewStore1.send(false)
+//
+//benchmark("Scoping (4)") {
+//  viewStore4.send(true)
+//}
+do {
+  struct State: Equatable {
+    @BindableState var value: Int = 0
   }
-  return .none
-}
 
-let store1 = Store(initialState: 0, reducer: counterReducer, environment: ())
-let store2 = store1.scope { $0 }
-let store3 = store2.scope { $0 }
-let store4 = store3.scope { $0 }
+  enum Action: BindableAction {
+    case binding(BindingAction<State>)
+  }
 
-let viewStore1 = ViewStore(store1)
-let viewStore2 = ViewStore(store2)
-let viewStore3 = ViewStore(store3)
-let viewStore4 = ViewStore(store4)
-
-benchmark("Scoping (1)") {
-  viewStore1.send(true)
-}
-viewStore1.send(false)
-
-benchmark("Scoping (2)") {
-  viewStore2.send(true)
-}
-viewStore1.send(false)
-
-benchmark("Scoping (3)") {
-  viewStore3.send(true)
-}
-viewStore1.send(false)
-
-benchmark("Scoping (4)") {
-  viewStore4.send(true)
+  do {
+    let store = Store(initialState: .init(),
+                      reducer: EmptyReducer<State, Action>().binding())
+    let viewStore = ViewStore(store)
+    benchmark("reduce") {
+      viewStore.send(.set(\.$value, 1))
+    }
+  }
+  
+  do {
+    let store = Store(initialState: .init(),
+                      reducer: EmptyReducer<State, Action>().bindingWithBody())
+    let viewStore = ViewStore(store)
+    benchmark("body") {
+      viewStore.send(.set(\.$value, 1))
+    }
+  }
 }
 
 Benchmark.main()
