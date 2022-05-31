4324761f331b9749874639ff8ac6bd82e1635ec0
Stephen Celis <stephen@stephencelis.com>
2022-05-31 11:14:46 -0400

    Remove "Strict Reducers" Example (#1116)

--- Examples/CaseStudies/SwiftUICaseStudies/00-RootView.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/00-RootView.swift
@@ -272,16 +272,6 @@ struct RootView: View {
             )
 
             NavigationLink(
-              "Strict reducers",
-              destination: DieRollView(
-                store: self.store.scope(
-                  state: \.dieRoll,
-                  action: RootAction.dieRoll
-                )
-              )
-            )
-
-            NavigationLink(
               "Elm-like subscriptions",
               destination: ClockView(
                 store: self.store.scope(
