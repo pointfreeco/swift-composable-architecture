f2c556252eb87cc2a1c5cb71c0b207f8bf47f34d
Stephen Celis <stephen@stephencelis.com>
2022-05-31 20:56:50 -0400

    wip

--- Sources/ComposableArchitecture/Effects/Concurrency.swift
+++ Sources/ComposableArchitecture/Effects/Concurrency.swift
@@ -97,9 +97,24 @@ import SwiftUI
       priority: TaskPriority? = nil,
       operation: @escaping @Sendable () async -> Output
     ) -> Self where Failure == Never {
-      Effect<Output, Error>.task(priority: priority, operation: operation)
-        .catch { _ in Empty() }
-        .eraseToEffect()
+      Deferred<Publishers.HandleEvents<PassthroughSubject<Output, Failure>>> {
+        let subject = PassthroughSubject<Output, Failure>()
+        let task = Task(priority: priority) { @MainActor in
+          do {
+            try Task.checkCancellation()
+            let output = await operation()
+            try Task.checkCancellation()
+            subject.send(output)
+            subject.send(completion: .finished)
+          } catch is CancellationError {
+            subject.send(completion: .finished)
+          } catch {
+            subject.send(completion: .finished)
+          }
+        }
+        return subject.handleEvents(receiveCancel: task.cancel)
+      }
+      .eraseToEffect()
     }
 
     public static func run(
