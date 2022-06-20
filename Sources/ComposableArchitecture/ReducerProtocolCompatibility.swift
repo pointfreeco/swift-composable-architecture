e2e806862c1a25998621049fc3bf947c6a371e3e
Stephen Celis <stephen@stephencelis.com>
2022-06-20 17:47:16 -0400

    wip

--- Sources/ComposableArchitecture/ReducerProtocolCompatibility.swift
+++ Sources/ComposableArchitecture/ReducerProtocolCompatibility.swift
@@ -52,3 +52,40 @@ extension Store {
     )
   }
 }
+
+extension TestStore {
+  /// Initializes a test store from an initial state, a reducer, and an initial environment.
+  ///
+  /// - Parameters:
+  ///   - initialState: The state to start the test from.
+  ///   - reducer: A reducer.
+  ///   - environment: The environment to start the test from.
+  public convenience init(
+    initialState: LocalState,
+    reducer: ComposableArchitecture.Reducer<LocalState, LocalAction, Environment>,
+    environment: Environment,
+    file: StaticString = #file,
+    line: UInt = #line
+  )
+  where
+    Reducer == Reduce<LocalState, LocalAction>
+  {
+    let environment = Box(wrappedValue: environment)
+    let reducer = TestReducer(
+      Reduce(
+        reducer.pullback(state: \.self, action: .self, environment: { $0.wrappedValue }),
+        environment: environment
+      ),
+      initialState: initialState
+    )
+    self.init(
+      _environment: environment,
+      file: file,
+      fromLocalAction: { $0 },
+      line: line,
+      reducer: reducer,
+      store: Store(initialState: initialState, reducer: reducer),
+      toLocalState: { $0 }
+    )
+  }
+}
