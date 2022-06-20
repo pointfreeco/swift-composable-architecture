74f8bea21c9420337b4271eb6f5ceba937958535
Stephen Celis <stephen@stephencelis.com>
2022-06-20 16:00:18 -0400

    wip

--- Sources/ComposableArchitecture/Internal/ExistentialWitness.swift
+++ Sources/ComposableArchitecture/Internal/ExistentialWitness.swift
@@ -1,42 +1,61 @@
-// NB: A swift(<5.7) mechanism for conditionally opening existential protocol conformances.
-private enum _Witness<T> {}
+#if swift(>=5.7)
+  // MARK: - Equatable
 
-// MARK: - Equatable
-
-func _isEqual(_ lhs: Any, _ rhs: Any) -> Bool? {
-  func open<T>(_: T.Type) -> Bool? {
-    (_Witness<T>.self as? _AnyEquatable.Type)?.isEqual(lhs, rhs)
+  func _isEqual(_ lhs: Any, _ rhs: Any) -> Bool? {
+    (lhs as? any Equatable)?.isEqual(other: rhs)
   }
-  return _openExistential(type(of: lhs), do: open)
-}
 
-private protocol _AnyEquatable {
-  static func isEqual(_ lhs: Any, _ rhs: Any) -> Bool
-}
-
-extension _Witness: _AnyEquatable where T: Equatable {
-  fileprivate static func isEqual(_ lhs: Any, _ rhs: Any) -> Bool {
-    guard
-      let lhs = lhs as? T,
-      let rhs = rhs as? T
-    else { return false }
-    return lhs == rhs
+  private extension Equatable {
+    func isEqual(other: Any) -> Bool {
+      self == other as? Self
+    }
   }
-}
 
-// MARK: - LiveDependencyKey
+  // MARK: - LiveDependencyKey
 
-func _liveValue(_ key: Any.Type) -> Any? {
-  func open<T>(_: T.Type) -> Any? {
-    (_Witness<T>.self as? _AnyLiveDependencyKey.Type)?.liveValue
+  func _liveValue(_ key: Any.Type) -> Any? {
+    (key as? any LiveDependencyKey.Type)?.liveValue
   }
-  return _openExistential(key, do: open)
-}
+#else
+  private enum _Witness<T> {}
 
-protocol _AnyLiveDependencyKey {
-  static var liveValue: Any { get }
-}
+  // MARK: - Equatable
 
-extension _Witness: _AnyLiveDependencyKey where T: LiveDependencyKey {
-  static var liveValue: Any { T.liveValue }
-}
+  func _isEqual(_ lhs: Any, _ rhs: Any) -> Bool? {
+    func open<T>(_: T.Type) -> Bool? {
+      (_Witness<T>.self as? _AnyEquatable.Type)?.isEqual(lhs, rhs)
+    }
+    return _openExistential(type(of: lhs), do: open)
+  }
+
+  private protocol _AnyEquatable {
+    static func isEqual(_ lhs: Any, _ rhs: Any) -> Bool
+  }
+
+  extension _Witness: _AnyEquatable where T: Equatable {
+    fileprivate static func isEqual(_ lhs: Any, _ rhs: Any) -> Bool {
+      guard
+        let lhs = lhs as? T,
+        let rhs = rhs as? T
+      else { return false }
+      return lhs == rhs
+    }
+  }
+
+  // MARK: - LiveDependencyKey
+
+  func _liveValue(_ key: Any.Type) -> Any? {
+    func open<T>(_: T.Type) -> Any? {
+      (_Witness<T>.self as? _AnyLiveDependencyKey.Type)?.liveValue
+    }
+    return _openExistential(key, do: open)
+  }
+
+  protocol _AnyLiveDependencyKey {
+    static var liveValue: Any { get }
+  }
+
+  extension _Witness: _AnyLiveDependencyKey where T: LiveDependencyKey {
+    static var liveValue: Any { T.liveValue }
+  }
+#endif
