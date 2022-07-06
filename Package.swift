8cf3f72f8f4d00953d00320d5be9e98063c96ad4
Stephen Celis <stephen@stephencelis.com>
2022-07-06 14:31:37 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.8.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.3.2"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.3.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.3.2"),
   ],
   targets: [
     .target(
