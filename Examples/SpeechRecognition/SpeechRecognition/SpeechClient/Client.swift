488fdc2d59ac55f5db9e01645910820021e67fc7
mbrandonw <mbrandonw@users.noreply.github.com>
2022-07-15 21:05:09 +0000

    Run swift-format

--- Examples/SpeechRecognition/SpeechRecognition/SpeechClient/Client.swift
+++ Examples/SpeechRecognition/SpeechRecognition/SpeechClient/Client.swift
@@ -5,8 +5,7 @@ import Speech
 struct SpeechClient {
   var finishTask: () -> Effect<Never, Never>
   var requestAuthorization: () -> Effect<SFSpeechRecognizerAuthorizationStatus, Never>
-  var startTask:
-    (SFSpeechAudioBufferRecognitionRequest) -> Effect<SpeechRecognitionResult, Error>
+  var startTask: (SFSpeechAudioBufferRecognitionRequest) -> Effect<SpeechRecognitionResult, Error>
 
   enum Error: Swift.Error, Equatable {
     case taskError
