dd1e5710045c461cde70474a3ed5c06216e31a25
Brandon Williams <mbrandonw@hey.com>
2022-07-17 07:48:14 -0500

    temporarily make box public

--- Sources/ComposableArchitecture/Internal/Box.swift
+++ Sources/ComposableArchitecture/Internal/Box.swift
@@ -1,16 +1,12 @@
-final class Box<Wrapped> {
-  var wrappedValue: Wrapped
+public class Box<Wrapped> {
+  public var wrappedValue: Wrapped
 
-  init(wrappedValue: Wrapped) {
+  public init(wrappedValue: Wrapped) {
     self.wrappedValue = wrappedValue
   }
 
-  var boxedValue: Wrapped {
+  public var boxedValue: Wrapped {
     _read { yield self.wrappedValue }
     _modify { yield &self.wrappedValue }
-  }
-
-  var projectedValue: Box<Wrapped> {
-    self
   }
 }
