15264ba0d42d654838e472b58341ff3bb67a3492
Brandon Williams <mbrandonw@hey.com>
2022-08-15 16:52:56 -0400

    more tests

--- Tests/ComposableArchitectureTests/DependencyKeyWritingReducerTests.swift
+++ Tests/ComposableArchitectureTests/DependencyKeyWritingReducerTests.swift
@@ -4,26 +4,9 @@ import XCTest
 @MainActor
 final class DependencyKeyWritingReducerTests: XCTestCase {
   func testWritingFusionOrder() async {
-    struct Feature: ReducerProtocol {
-      @Dependency(\.date) var date
-      struct State: Equatable {
-        var currentDate = Date()
-      }
-      enum Action {
-        case tap
-      }
-      func reduce(into state: inout State, action: Action) -> Effect<Action, Never> {
-        switch action {
-        case .tap:
-          state.currentDate = self.date()
-          return .none
-        }
-      }
-    }
-
     let reducer = Feature()
-      .dependency(\.date, ConstantDateGenerator(Date(timeIntervalSince1970: 42)))
-      .dependency(\.date, ConstantDateGenerator(Date(timeIntervalSince1970: 1729)))
+      .dependency(\.myValue, 42)
+      .dependency(\.myValue, 1729)
 
     let store = TestStore(
       initialState: Feature.State(),
@@ -31,33 +14,16 @@ final class DependencyKeyWritingReducerTests: XCTestCase {
     )
 
     await store.send(.tap) {
-      $0.currentDate = Date(timeIntervalSince1970: 42)
+      $0.value = 42
     }
   }
 
   func testWritingOrder() async {
-    struct Feature: ReducerProtocol {
-      @Dependency(\.date) var date
-      struct State: Equatable {
-        var currentDate = Date()
-      }
-      enum Action {
-        case tap
-      }
-      func reduce(into state: inout State, action: Action) -> Effect<Action, Never> {
-        switch action {
-        case .tap:
-          state.currentDate = self.date()
-          return .none
-        }
-      }
-    }
-
     let reducer = CombineReducers {
       Feature()
-        .dependency(\.date, ConstantDateGenerator(Date(timeIntervalSince1970: 42)))
+        .dependency(\.myValue, 42)
     }
-    .dependency(\.date, ConstantDateGenerator(Date(timeIntervalSince1970: 1729)))
+    .dependency(\.myValue, 1729)
 
     let store = TestStore(
       initialState: Feature.State(),
@@ -65,7 +31,69 @@ final class DependencyKeyWritingReducerTests: XCTestCase {
     )
 
     await store.send(.tap) {
-      $0.currentDate = Date(timeIntervalSince1970: 42)
+      $0.value = 42
     }
+  }
+
+  func testDependency_EffectOfEffect() async {
+    struct Feature: ReducerProtocol {
+      struct State: Equatable { var count = 0 }
+      enum Action: Equatable { case tap, response(Int), otherResponse(Int) }
+      @Dependency(\.myValue) var myValue
+
+      func reduce(into state: inout State, action: Action) -> Effect<Action, Never> {
+        switch action {
+        case .tap:
+          state.count += 1
+          return .task { .response(self.myValue) }
+
+        case let .response(value):
+          state.count = value
+          return .task { .otherResponse(self.myValue) }
+
+        case let .otherResponse(value):
+          state.count = value
+          return .none
+        }
+      }
+    }
+
+    let store = TestStore(
+      initialState: Feature.State(),
+      reducer: Feature()
+        .dependency(\.myValue, 42)
+    )
+
+    await store.send(.tap) {
+      $0.count = 1
+    }
+    await store.receive(.response(42)) {
+      $0.count = 42
+    }
+    await store.receive(.otherResponse(42))
+  }
+}
+
+private struct Feature: ReducerProtocol {
+  @Dependency(\.myValue) var myValue
+  struct State: Equatable { var value = 0 }
+  enum Action { case tap }
+  func reduce(into state: inout State, action: Action) -> Effect<Action, Never> {
+    switch action {
+    case .tap:
+      state.value = self.myValue
+      return .none
+    }
+  }
+}
+
+private enum MyValue: LiveDependencyKey {
+  static let liveValue = 0
+  static let testValue = 0
+}
+extension DependencyValues {
+  var myValue: Int {
+    get { self[MyValue.self] }
+    set { self[MyValue.self] = newValue }
   }
 }
