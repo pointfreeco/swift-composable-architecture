4cf0b30ce95aeca67d28214f1a729b36d124d427
Stephen Celis <stephen@stephencelis.com>
2022-08-22 21:21:15 -0400

    wip

--- Sources/Dependencies/DependencyKey.swift
+++ Sources/Dependencies/DependencyKey.swift
@@ -1,9 +1,39 @@
+/// A key for accessing dependencies.
+///
+/// Similar to SwiftUI's `EnvironmentKey` protocol, which is used to extend `EnvironmentValues` with
+/// custom dependencies, `DependencyKey` can be used to extend ``DependencyValues``.
+///
+/// `DependencyKey` has one main requirement, ``testValue``, which must return a default value for
+/// the purposes of testing, and one optional requirement, ``previewValue``, which can return a
+/// default value suitable for Xcode previews.
+///
+/// See ``LiveDependencyKey`` to define a static, default value for the live application.
 public protocol DependencyKey {
+  /// The associated type representing the type of the dependency key's value.
   associatedtype Value
   // NB: This associated type should be constrained to `Sendable` when this bug is fixed:
   //     https://github.com/apple/swift/issues/60649
 
+  /// The preview value for the dependency key.
+  ///
+  /// This value is automatically installed into stores running in Xcode previews, as well as
+  /// reducer hierarchies that set their dependency environment to `preview`:
+  ///
+  /// ```swift
+  /// MyFeature()
+  ///   .dependency(\.environment, .preview)
+  /// ```
   static var previewValue: Value { get }
+
+  /// The test value for the dependency key.
+  ///
+  /// This value is automatically installed into test stores and reducer hierarchies that set their
+  /// dependency environment to `test`:
+  ///
+  /// ```swift
+  /// MyFeature()
+  ///   .dependency(\.environment, .test)
+  /// ```
   static var testValue: Value { get }
 }
 
@@ -11,6 +41,7 @@ extension DependencyKey {
   public static var previewValue: Value { Self.testValue }
 }
 
+/// A key for accessing live dependencies.
 public protocol LiveDependencyKey: DependencyKey {
   static var liveValue: Value { get }
 }
--- Sources/Dependencies/DependencyValues.swift
+++ Sources/Dependencies/DependencyValues.swift
@@ -16,15 +16,45 @@ import Foundation
 /// ```
 public struct DependencyValues: Sendable {
   public enum Environment: Sendable {
-    case live, preview, test
+    case live
+    case preview
+    case test
   }
 
   @TaskLocal public static var current = Self()
 
   private var storage: [ObjectIdentifier: AnySendable] = [:]
 
+  /// Creates a dependency values instance.
+  ///
+  /// You don't typically create an instance of ``DependencyValues`` directly. Doing so would
+  /// provide access only to default values. Instead, you rely on an dependency values' instance
+  /// that the Composable Architecture manages for you when you use the ``Dependency`` property
+  /// wrapper and the `ReducerProtocol/dependency(_:_:)`` modifier.
   public init() {}
 
+  /// Accesses the dependency value associated with a custom key.
+  ///
+  /// Create custom dependency values by defining a key that conforms to the ``DependencyKey``
+  /// protocol, and then using that key with the subscript operator of the ``DependencyValues``
+  /// structure to get and set a value for that key:
+  ///
+  /// ```swift
+  /// private struct MyDependencyKey: DependencyKey {
+  ///   static let testValue = "Default value"
+  /// }
+  ///
+  /// extension DependencyValues {
+  ///   var myCustomValue: String {
+  ///     get { self[MyDependencyKey.self] }
+  ///     set { self[MyDependencyKey.self] = newValue }
+  ///   }
+  /// }
+  /// ```
+  ///
+  /// You use custom dependency values the same way you use system-provided values, setting a value
+  /// with the `ReducerProtocol/dependency(_:_:)` modifier, and reading values with the
+  /// ``Dependency`` property wrapper.
   public subscript<Key: DependencyKey>(
     key: Key.Type,
     file: StaticString = #file,
@@ -112,7 +142,12 @@ private enum EnvironmentKey: LiveDependencyKey {
   static var previewValue = DependencyValues.Environment.preview
   static var testValue = DependencyValues.Environment.test
 }
+
 extension DependencyValues {
+  // TODO: Should we reconsider this name?
+  // Using and mentioning our own "dependency environment" and also mentioning SwiftUI's environment
+  // in the same documentation is confusing, so maybe we can come up with something more unique. We
+  // discussed "mode," but do we like `.dependency(\.mode, .test)`? Can we come up with better?
   public var environment: DependencyValues.Environment {
     get { self[EnvironmentKey.self] }
     set { self[EnvironmentKey.self] = newValue }
