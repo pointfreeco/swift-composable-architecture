9456f481cba46d6f3480d6123921c75ba597a5a9
Stephen Celis <stephen@stephencelis.com>
2022-08-25 18:45:41 -0400

    wip

--- Examples/SpeechRecognition/SpeechRecognition/SpeechClient/Lorem.swift
+++ Examples/SpeechRecognition/SpeechRecognition/SpeechClient/Lorem.swift
@@ -9,10 +9,10 @@ extension SpeechClient {
       finishTask: { await isRecording.setValue(false) },
       requestAuthorization: { .authorized },
       startTask: { _ in
-          .init { c in
-            Task {
-              await isRecording.setValue(true)
-              var finalText = """
+        AsyncThrowingStream { continuation in
+          Task {
+            await isRecording.setValue(true)
+            var finalText = """
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
               exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute \
@@ -20,31 +20,31 @@ extension SpeechClient {
               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui \
               officia deserunt mollit anim id est laborum.
               """
-              var text = ""
-              while await isRecording.value {
-                let word = finalText.prefix { $0 != " " }
-                try await Task.sleep(
-                  nanoseconds: UInt64(word.count) * NSEC_PER_MSEC * 50
+            var text = ""
+            while await isRecording.value {
+              let word = finalText.prefix { $0 != " " }
+              try await Task.sleep(
+                nanoseconds: UInt64(word.count) * NSEC_PER_MSEC * 50
                   + .random(in: 0...(NSEC_PER_MSEC * 200))
-                )
-                finalText.removeFirst(word.count)
-                if finalText.first == " " {
-                  finalText.removeFirst()
-                }
-                text += word + " "
-                c.yield(
-                  .init(
-                    bestTranscription: .init(
-                      formattedString: text,
-                      segments: []
-                    ),
-                    isFinal: false,
-                    transcriptions: []
-                  )
-                )
+              )
+              finalText.removeFirst(word.count)
+              if finalText.first == " " {
+                finalText.removeFirst()
               }
+              text += word + " "
+              continuation.yield(
+                SpeechRecognitionResult(
+                  bestTranscription: Transcription(
+                    formattedString: text,
+                    segments: []
+                  ),
+                  isFinal: false,
+                  transcriptions: []
+                )
+              )
             }
           }
+        }
       }
     )
   }
