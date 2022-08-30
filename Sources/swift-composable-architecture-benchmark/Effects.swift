d90bd9e051abaf49664c072624dbd42053b981dd
Stephen Celis <stephen@stephencelis.com>
2022-08-30 11:17:23 -0400

    wip

--- Sources/swift-composable-architecture-benchmark/Effects.swift
+++ Sources/swift-composable-architecture-benchmark/Effects.swift
@@ -4,11 +4,22 @@ import ComposableArchitecture
 import Foundation
 
 let effectSuite = BenchmarkSuite(name: "Effects") {
-  $0.benchmark("Merging Effect.none") {
+  $0.benchmark("Merged Effect.none (create, flat)") {
+    let effect = Effect<Int, Never>.merge((1...100).map { _ in .none })
+  }
+
+  $0.benchmark("Merged Effect.none (create, nested)") {
     var effect = Effect<Int, Never>.none
-    for _ in 1...1_000 {
+    for _ in 1...100 {
       effect = .merge(effect, .none)
     }
+  }
+
+  var effect = Effect<Int, Never>.none
+  for _ in 1...100 {
+    effect = .merge(effect, .none)
+  }
+  $0.benchmark("Merged Effect.none (sink)") {
     _ = effect.sink(receiveCompletion: { _ in }, receiveValue: { _ in })
   }
 }
