f5f2a7c8e61a809ae04f49df0a7acf63f3fa09ee
Stephen Celis <stephen@stephencelis.com>
2022-09-08 18:54:38 -0400

    remove

--- Sources/ComposableArchitecture/SwiftUI/WithViewStore.swift
+++ Sources/ComposableArchitecture/SwiftUI/WithViewStore.swift
@@ -120,7 +120,7 @@ public struct WithViewStore<ViewState, ViewAction, Content> {
   @ObservedObject private var viewStore: ViewStore<ViewState, ViewAction>
 
   init(
-    store: @autoclosure @escaping () -> Store<ViewState, ViewAction>,
+    store: Store<ViewState, ViewAction>,
     removeDuplicates isDuplicate: @escaping (ViewState, ViewState) -> Bool,
     content: @escaping (ViewStore<ViewState, ViewAction>) -> Content,
     file: StaticString = #fileID,
@@ -136,7 +136,7 @@ public struct WithViewStore<ViewState, ViewAction, Content> {
         return previousState
       }
     #endif
-    self._viewStore = .init(wrappedValue: ViewStore(store(), removeDuplicates: isDuplicate))
+    self.viewStore = ViewStore(store, removeDuplicates: isDuplicate)
   }
 
   /// Prints debug information to the console whenever the view is computed.
