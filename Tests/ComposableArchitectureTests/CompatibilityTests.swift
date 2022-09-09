78e41dcd4aab402fa824eae269e77a7f1211c791
Brandon Williams <mbrandonw@hey.com>
2022-09-09 14:27:35 -0400

    wip

--- Tests/ComposableArchitectureTests/CompatibilityTests.swift
+++ Tests/ComposableArchitectureTests/CompatibilityTests.swift
@@ -75,55 +75,40 @@ final class CompatibilityTests: XCTestCase {
     )
   }
 
-  func testCaseStudy_ReentrantActionsFromPublisher() {
-    struct State: Equatable {
-      var city: String
-      var country: String
-    }
-
-    enum Action: Equatable {
-      case updateCity(String)
-      case updateCountry(String)
-    }
-
-    let reducer = Reducer<State, Action, Void> { state, action, _ in
-      switch action {
-      case let .updateCity(city):
-        state.city = city
+  // Actions can be re-entrantly sent into the store while observing changes to the store's state.
+  // In such cases we need to take special care that those re-entrant actions are handled _after_
+  // the original action.
+  //
+  // In particular, this means that in the implementation of `Store.send` we need to flip
+  // `isSending` to false _after_ the store's state mutation is made so that re-entrant actions
+  // are buffered rather than immediately handled.
+  func testActionReentranceFromStateObservation() {
+    let store = Store<Int, Int>(
+      initialState: 0,
+      reducer: .init { state, action, _ in
+        state = action
         return .none
-      case let .updateCountry(country):
-        state.country = country
-        return .none
-      }
-    }
-
-    let store = Store(
-      initialState: State(city: "New York", country: "USA"),
-      reducer: reducer,
+      },
       environment: ()
     )
+
     let viewStore = ViewStore(store)
 
     var cancellables: Set<AnyCancellable> = []
-
-    viewStore.publisher.city
-      .sink { city in
-        if city == "London" {
-          viewStore.send(.updateCountry("UK"))
-        }
+    viewStore.publisher
+      .sink { value in
+        if value == 1 { viewStore.send(0) }
       }
       .store(in: &cancellables)
 
-    var countryUpdates = [String]()
-    viewStore.publisher.country
-      .sink { country in
-        countryUpdates.append(country)
-      }
+    var stateChanges: [Int] = []
+    viewStore.publisher
+      .sink { stateChanges.append($0) }
       .store(in: &cancellables)
 
-    viewStore.send(.updateCity("London"))
-
-    XCTAssertEqual(countryUpdates, ["USA", "UK"])
+    XCTAssertEqual(stateChanges, [0])
+    viewStore.send(1)
+    XCTAssertEqual(stateChanges, [0, 1, 0])
   }
 }
 
