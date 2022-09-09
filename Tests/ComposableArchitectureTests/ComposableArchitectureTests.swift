d4834b56e92f347ac6c8b9908d415fd51c3d18f4
Stephen Celis <stephen@stephencelis.com>
2022-09-09 15:11:05 -0400

    Fix flakey test (#1364)
    * Fix flakey test
    
    * Try something more general

--- Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift
+++ Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift
@@ -123,11 +123,7 @@ final class ComposableArchitectureTests: XCTestCase {
       case response(Int)
     }
 
-    struct Environment {
-      let fetch: (Int) async -> Int
-    }
-
-    let reducer = Reducer<Int, Action, Environment> { state, action, environment in
+    let reducer = Reducer<Int, Action, Void> { state, action, _ in
       enum CancelID {}
 
       switch action {
@@ -138,7 +134,7 @@ final class ComposableArchitectureTests: XCTestCase {
         state += 1
         return .task { [state] in
           try await mainQueue.sleep(for: .seconds(1))
-          return .response(await environment.fetch(state))
+          return .response(state * state)
         }
         .cancellable(id: CancelID.self)
 
@@ -151,9 +147,7 @@ final class ComposableArchitectureTests: XCTestCase {
     let store = TestStore(
       initialState: 0,
       reducer: reducer,
-      environment: Environment(
-        fetch: { value in value * value }
-      )
+      environment: ()
     )
 
     await store.send(.incr) { $0 = 1 }
