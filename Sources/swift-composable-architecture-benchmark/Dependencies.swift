5996171c1098836c4912fa0800299a3001fffbbe
Brandon Williams <mbrandonw@hey.com>
2022-09-27 12:20:00 -0400

    make benchmark 5.7 compatible

--- Sources/swift-composable-architecture-benchmark/Dependencies.swift
+++ Sources/swift-composable-architecture-benchmark/Dependencies.swift
@@ -5,18 +5,20 @@ import Dependencies
 import Foundation
 
 let dependenciesSuite = BenchmarkSuite(name: "Dependencies") {
-  let reducer: some ReducerProtocol<Int, Void> = EmptyReducer()
-    .dependency(\.calendar, .autoupdatingCurrent)
-    .dependency(\.date, .live)
-    .dependency(\.locale, .autoupdatingCurrent)
-    .dependency(\.mainQueue, .immediate)
-    .dependency(\.mainRunLoop, .immediate)
-    .dependency(\.timeZone, .autoupdatingCurrent)
-    .dependency(\.uuid, .live)
+  #if swift(>=5.7)
+    let reducer: some ReducerProtocol<Int, Void> = EmptyReducer()
+      .dependency(\.calendar, .autoupdatingCurrent)
+      .dependency(\.date, .live)
+      .dependency(\.locale, .autoupdatingCurrent)
+      .dependency(\.mainQueue, .immediate)
+      .dependency(\.mainRunLoop, .immediate)
+      .dependency(\.timeZone, .autoupdatingCurrent)
+      .dependency(\.uuid, .live)
 
-  $0.benchmark("Dependency key writing") {
-    var state = 0
-    _ = reducer.reduce(into: &state, action: ())
-    precondition(state == 0)
-  }
+    $0.benchmark("Dependency key writing") {
+      var state = 0
+      _ = reducer.reduce(into: &state, action: ())
+      precondition(state == 0)
+    }
+  #endif
 }
