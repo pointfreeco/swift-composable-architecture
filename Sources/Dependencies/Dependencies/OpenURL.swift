1fbf6ad64514974a7e7e627b96917b96eeea5a0d
Brandon Williams <mbrandonw@hey.com>
2022-09-29 09:55:43 -0400

    format

--- Sources/Dependencies/Dependencies/OpenURL.swift
+++ Sources/Dependencies/Dependencies/OpenURL.swift
@@ -1,60 +1,60 @@
 #if !os(watchOS)
-import XCTestDynamicOverlay
+  import XCTestDynamicOverlay
 
-#if canImport(AppKit)
-  import AppKit
-#endif
-#if canImport(UIKit)
-  import UIKit
-#endif
+  #if canImport(AppKit)
+    import AppKit
+  #endif
+  #if canImport(UIKit)
+    import UIKit
+  #endif
 
-extension DependencyValues {
-  var openURL: OpenURLEffect {
-    get { self[OpenURLKey.self] }
-    set { self[OpenURLKey.self] = newValue }
-  }
+  extension DependencyValues {
+    var openURL: OpenURLEffect {
+      get { self[OpenURLKey.self] }
+      set { self[OpenURLKey.self] = newValue }
+    }
 
-  private enum OpenURLKey: DependencyKey {
-    static let liveValue = OpenURLEffect { url in
-      let stream = AsyncStream<Bool> { continuation in
-        let task = Task { @MainActor in
-          #if canImport(AppKit)
-            NSWorkspace.shared.open(url, configuration: .init()) { _, error in
-              continuation.yield(error == nil)
-              continuation.finish()
-            }
-          #endif
-          #if canImport(UIKit)
-            UIApplication.shared.open(url) { canOpen in
-              continuation.yield(canOpen)
-              continuation.finish()
-            }
-          #endif
-          // TODO: Make sure `tvOS` and `watchOS` behave like `EnvironmentValues.openURL`?
+    private enum OpenURLKey: DependencyKey {
+      static let liveValue = OpenURLEffect { url in
+        let stream = AsyncStream<Bool> { continuation in
+          let task = Task { @MainActor in
+            #if canImport(AppKit)
+              NSWorkspace.shared.open(url, configuration: .init()) { _, error in
+                continuation.yield(error == nil)
+                continuation.finish()
+              }
+            #endif
+            #if canImport(UIKit)
+              UIApplication.shared.open(url) { canOpen in
+                continuation.yield(canOpen)
+                continuation.finish()
+              }
+            #endif
+            // TODO: Make sure `tvOS` and `watchOS` behave like `EnvironmentValues.openURL`?
+          }
+          continuation.onTermination = { _ in
+            task.cancel()
+          }
         }
-        continuation.onTermination = { _ in
-          task.cancel()
-        }
+        return await stream.first(where: { _ in true }) ?? false
       }
-      return await stream.first(where: { _ in true }) ?? false
+      static let testValue = OpenURLEffect { _ in
+        XCTFail(#"Unimplemented: @Dependency(\.openURL)"#)
+        return false
+      }
     }
-    static let testValue = OpenURLEffect { _ in
-      XCTFail(#"Unimplemented: @Dependency(\.openURL)"#)
-      return false
+  }
+
+  public struct OpenURLEffect: Sendable {
+    private let handler: @Sendable (URL) async -> Bool
+
+    init(handler: @escaping @Sendable (URL) async -> Bool) {
+      self.handler = handler
+    }
+
+    @discardableResult
+    public func callAsFunction(_ url: URL) async -> Bool {
+      await self.handler(url)
     }
   }
-}
-
-public struct OpenURLEffect: Sendable {
-  private let handler: @Sendable (URL) async -> Bool
-
-  init(handler: @escaping @Sendable (URL) async -> Bool) {
-    self.handler = handler
-  }
-
-  @discardableResult
-  public func callAsFunction(_ url: URL) async -> Bool {
-    await self.handler(url)
-  }
-}
 #endif
