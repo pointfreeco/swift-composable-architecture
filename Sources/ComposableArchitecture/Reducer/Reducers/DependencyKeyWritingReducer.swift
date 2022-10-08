75a2e2750ba8ee2d9d0122bfbec4cbfc79818d8a
Brandon Williams <mbrandonw@hey.com>
2022-10-08 13:37:12 -0400

    more docs for DependencyKeyWritingReducer

--- Sources/ComposableArchitecture/Reducer/Reducers/DependencyKeyWritingReducer.swift
+++ Sources/ComposableArchitecture/Reducer/Reducers/DependencyKeyWritingReducer.swift
@@ -1,6 +1,62 @@
 extension ReducerProtocol {
   /// Sets the dependency value of the specified key path to the given value.
   ///
+  /// This overrides the dependency specified by `keyPath` for the execution of the receiving
+  /// reducer _and_ all of its effects. It can be useful for altering the dependencies for just
+  /// one portion of your application, while letting the rest of the application continue using the
+  /// default live dependencies.
+  ///
+  /// For example, suppose you are creating an onboarding experience to teach people how to use one
+  /// of your features. This can be done by constructing a new reducer that embeds the core
+  /// feature's domain and layers on additional logic:
+  ///
+  /// ```swift
+  /// struct Onboarding: ReducerProtocol {
+  ///   struct State {
+  ///     var feature: Feature.State
+  ///     // Additional onboarding state
+  ///   }
+  ///   enum Action {
+  ///     case feature(Feature.Action)
+  ///     // Additional onboarding actions
+  ///   }
+  ///
+  ///   var body: some ReducerProtocol<State, Action> {
+  ///     Scope(state: \.feature, action: /Action.feature) {
+  ///       Feature()
+  ///     }
+  ///
+  ///     Reduce { state, action in
+  ///       // Additional onboarding logic
+  ///     }
+  ///   }
+  /// }
+  /// ```
+  ///
+  /// This can work just fine, but the `Feature` reducer will have access to all of the live
+  /// dependencies by default, and that might not be ideal. For example, the `Feature` reducer
+  /// may need to make API requests and read/write from user defaults. It may be preferable
+  /// to run the `Feature` reducer in an alternative environment for onboarding purposes, such
+  /// as an API client that returns some mock data or an in-memory user defaults so that the
+  /// onboarding experience doesn't accidentally trample on shared data.
+  ///
+  /// This can be by using the ``dependency(_:_:)`` method to override those dependencies
+  /// just for the `Feature` reducer and its effects:
+  ///
+  /// ```swift
+  /// var body: some ReducerProtocol<State, Action> {
+  ///   Scope(state: \.feature, action: /Action.feature) {
+  ///     Feature()
+  ///       .dependency(\.apiClient, .mock)
+  ///       .dependency(\.userDefaults, .mock)
+  ///   }
+  ///
+  ///   Reduce { state, action in
+  ///     // Additional onboarding logic
+  ///   }
+  /// }
+  /// ```
+  ///
   /// - Parameters:
   ///   - keyPath: A key path that indicates the property of the `DependencyValues` structure to
   ///     update.
@@ -19,9 +75,29 @@ extension ReducerProtocol {
 
   /// Modifies a reducer's dependencies with a closure.
   ///
+  /// This is similar to ``dependency(_:_:)``, except it allows you to mutate all depependencies
+  /// at once. This can be handy when you want to alter a dependency but still use its current
+  /// value.
+  ///
+  /// For example, suppose you want to see when a particular endpoint of a dependency gets called
+  /// in your application. You can override that endpoint to insert a breakpoint or print state,
+  /// but still call out to the original endpoint:
+  ///
+  /// ```swift
+  ///   Feature()
+  ///     .dependencies { values in
+  ///       let current = speechClient.requestAuthorization
+  ///       values.speechClient.requestAuthorization = {
+  ///         print("requestAuthorization")
+  /// 🔵      try await current()
+  ///       }
+  ///     }
+  /// ```
+  ///
   /// - Parameter update: A closure that is handed a mutable instance of dependency values.
   @inlinable
   public func dependencies(_ update: @escaping (inout DependencyValues) -> Void)
+  // TODO: this isn't actually true, but we also can't use `some` until dropping support for swift 5.6
   // NB: We should not return `some ReducerProtocol<State, Action>` here. That would prevent the
   //     specialization defined below from being called, which fuses chained calls to `dependency`.
   -> _DependencyKeyWritingReducer<Self> {
