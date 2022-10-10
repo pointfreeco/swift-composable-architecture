4228ff40e0df1cb849f6a158e018b1c13d6bd9c9
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2022-10-10 12:05:46 -0400

    Remove swiftSettings.

--- Package.swift
+++ Package.swift
@@ -38,13 +38,6 @@ let package = Package(
         .product(name: "CustomDump", package: "swift-custom-dump"),
         .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
         .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
-      ],
-      swiftSettings: [
-        .unsafeFlags([
-          // "-enable-library-evolution"
-          // "-Xfrontend", "-warn-concurrency",
-          // "-Xfrontend", "-enable-actor-data-race-checks",
-        ])
       ]
     ),
     .testTarget(
