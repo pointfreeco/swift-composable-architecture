2b7d3f0b55b49a6ae094b6808938680a027e8544
Stephen Celis <stephen@stephencelis.com>
2022-10-11 09:22:57 -0700

    More agnostic

--- Sources/Dependencies/Dependencies/UUID.swift
+++ Sources/Dependencies/Dependencies/UUID.swift
@@ -126,24 +126,14 @@ public struct UUIDGenerator: Sendable {
 }
 
 private final class IncrementingUUIDGenerator: @unchecked Sendable {
-  private let lock: os_unfair_lock_t
+  private let lock = NSLock()
   private var sequence = 0
 
-  init() {
-    self.lock = os_unfair_lock_t.allocate(capacity: 1)
-    self.lock.initialize(to: os_unfair_lock())
-  }
-
-  deinit {
-    self.lock.deinitialize(count: 1)
-    self.lock.deallocate()
-  }
-
   func callAsFunction() -> UUID {
-    os_unfair_lock_lock(self.lock)
+    self.lock.lock()
     defer {
       self.sequence += 1
-      os_unfair_lock_unlock(self.lock)
+      self.lock.unlock()
     }
     return UUID(uuidString: "00000000-0000-0000-0000-\(String(format: "%012x", self.sequence))")!
   }
