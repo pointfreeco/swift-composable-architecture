2131caf9abc589a6a800d2ce22c1863bec7aea2d
Inwoo Hwang <dlsdn1207@gmail.com>
2022-10-20 20:41:28 +0900

    refactor : remove unnecessary conformance to Sendable Protocol

--- Examples/Search/Search/WeatherClient.swift
+++ Examples/Search/Search/WeatherClient.swift
@@ -38,7 +38,7 @@ struct Forecast: Decodable, Equatable, Sendable {
 // Typically this interface would live in its own module, separate from the live implementation.
 // This allows the search feature to compile faster since it only depends on the interface.
 
-struct WeatherClient: Sendable {
+struct WeatherClient {
   var forecast: @Sendable (GeocodingSearch.Result) async throws -> Forecast
   var search: @Sendable (String) async throws -> GeocodingSearch
 }
