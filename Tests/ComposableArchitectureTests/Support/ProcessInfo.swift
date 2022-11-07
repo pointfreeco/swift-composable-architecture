233563b6237ae3507c9dd07285eb7b313410253c
Stephen Celis <stephen@stephencelis.com>
2022-11-07 10:41:08 -0800

    debug

--- Tests/ComposableArchitectureTests/Support/ProcessInfo.swift
+++ Tests/ComposableArchitectureTests/Support/ProcessInfo.swift
@@ -2,6 +2,9 @@ import Foundation
 
 extension ProcessInfo {
   var isCI: Bool {
-    self.environment["CI"].map { Bool($0.lowercased()) ?? ($0 == "1") } ?? false
+    print("!!!")
+    dump(self.environment)
+    print("!!!")
+    return self.environment["CI"].map { Bool($0.lowercased()) ?? ($0 == "1") } ?? false
   }
 }
