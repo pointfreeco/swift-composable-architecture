11e5facd96197013eaff7e5d7685ef7d165857ed
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-09 22:01:47 +0000

    wip

--- Sources/Dependencies/Dependencies/Data.swift
+++ Sources/Dependencies/Dependencies/Data.swift
@@ -2,6 +2,33 @@ import Foundation
 import XCTestDynamicOverlay
 
 extension DependencyValues {
+  /// A dependency that reads data from a specified URL.
+  ///
+  /// By default, a "live" reader is supplied, which returns the real disk/remote data reading
+  /// mechanism called by invoking `Data(contentsOf:options:)` under the hood. When used from
+  /// a `TestStore`, an "unimplemented" reader that returns empty `Data` and additionally
+  /// reports test failures is supplied, unless explicitly overridden.
+  ///
+  /// To override the current data reading logic in tests, you can override the reader using
+  /// ``withValue(_:_:operation:)-705n``:
+  ///
+  /// ```swift
+  /// DependencyValues.withValue(\.data, .init(contentsOfURL: { _, _ in Data("deadbeef".utf8) })) {
+  ///   // Assertions...
+  /// }
+  /// ```
+  ///
+  /// Or, if you are using the Composable Architecture, you can override dependencies directly
+  /// on the `TestStore`:
+  ///
+  /// ```swift
+  /// let store = TestStore(
+  ///   initialState: MyFeature.State()
+  ///   reducer: MyFeature()
+  /// )
+  ///
+  /// store.dependencies.data = .init(contentsOfURL: { _, _ in Data("deadbeef".utf8) })
+  /// ```
   public var data: DataReader {
     get { self[DataReaderKey.self] }
     set { self[DataReaderKey.self] = newValue }
@@ -20,9 +47,15 @@ extension DependencyValues {
   }
 }
 
+/// A dependency that reads data from a specified URL.
+///
+/// See ``DependencyValues/data`` for more information.
 public struct DataReader: Sendable {
   private var contentsOfURL: @Sendable (URL, Data.ReadingOptions) throws -> Data
 
+  /// Initializes a data reader that reads data via a closure.
+  ///
+  /// - Parameter contentsOfURL: A closure that returns data from a URL when called.
   public init(contentsOfURL: @escaping @Sendable (URL, Data.ReadingOptions) throws -> Data) {
     self.contentsOfURL = contentsOfURL
   }
