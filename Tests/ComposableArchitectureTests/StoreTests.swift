23ffae73585e4f9066aef24238422871b2ae8c51
Brandon Williams <mbrandonw@hey.com>
2022-12-05 09:20:35 -0600

    wip

--- Tests/ComposableArchitectureTests/StoreTests.swift
+++ Tests/ComposableArchitectureTests/StoreTests.swift
@@ -539,4 +539,91 @@ final class StoreTests: XCTestCase {
 
     ViewStore(store).send(true)
   }
+
+  @available(iOS 16, *)
+  func testParentEffectUnaffectedByCancellationOfChildEffect() async throws {
+    let store = Store(
+      initialState: Parent.State(child: Child.State()),
+      reducer: Parent()
+    )
+
+    let parentViewStore = ViewStore(store)
+    let childViewStore = ViewStore(store.scope(state: \.child, action: Parent.Action.child))
+
+    var counts: [Int] = []
+    parentViewStore.publisher.sink { parentState in
+      counts.append(parentState.count)
+    }
+    .store(in: &self.cancellables)
+
+    XCTAssertEqual(counts, [0])
+    let task = childViewStore.send(.task)
+    XCTAssertEqual(parentViewStore.child, nil)
+    await task.cancel()
+
+    try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+    XCTAssertEqual(counts, [0, 1])
+  }
+}
+
+
+
+struct Parent: ReducerProtocol {
+  struct State: Equatable {
+    var count = 0
+    var child: Child.State?
+  }
+  enum Action: Equatable {
+    case child(Child.Action)
+    case showSheetTapped
+    case childShowingChanged(Bool)
+    case delayedWork
+  }
+
+  var body: some ReducerProtocol<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .child(.task):
+        state.child = nil // Cancels child task by tearing down child view in real app
+        return .task {
+          // A small delay on the child's hook before we take further action in the parent.
+          // This task ends up cancelled and delayedWork action never runs
+          do {
+            try await Task.sleep(nanoseconds: NSEC_PER_SEC / 2)
+            return .delayedWork
+          } catch {
+            print(error)
+            print("---------")
+            return .delayedWork
+          }
+        }
+      case .child:
+        return .none
+      case .showSheetTapped:
+        state.child = .init()
+        return .none
+      case .childShowingChanged:
+        return .none
+      case .delayedWork:
+        state.count += 1
+        return .none
+      }
+    }
+    .ifLet(\.child, action: /Action.child) {
+      Child()
+    }
+  }
+}
+struct Child: ReducerProtocol {
+  struct State: Equatable {}
+  enum Action: Equatable {
+    case task
+  }
+
+  func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+    switch action {
+    case .task:
+      return .none
+    }
+  }
 }
