ba59ad1e1f1a3f47df709795107954d78f23c6df
Stephen Celis <stephen@stephencelis.com>
2023-01-06 10:48:06 -0500

    Revert "resolve binding key path crash (#1784)"
    This reverts commit fbe68471a3cf4eef6f0501d07642ee70b5bc3e84.

--- Sources/ComposableArchitecture/ViewStore.swift
+++ Sources/ComposableArchitecture/ViewStore.swift
@@ -466,11 +466,8 @@ public final class ViewStore<ViewState, ViewAction>: ObservableObject {
     get: @escaping (ViewState) -> Value,
     send valueToAction: @escaping (Value) -> ViewAction
   ) -> Binding<Value> {
-    @ObservedState var val = get(self.state)
-    return .init(
-      get: { [$val] in $val.wrappedValue },
-      set: { [weak self] in self?.send(valueToAction($0)) }
-    )
+    ObservedObject(wrappedValue: self)
+      .projectedValue[get: .init(rawValue: get), send: .init(rawValue: valueToAction)]
   }
 
   /// Derives a binding from the store that prevents direct writes to state and instead sends
@@ -561,6 +558,14 @@ public final class ViewStore<ViewState, ViewAction>: ObservableObject {
   /// - Returns: A binding.
   public func binding(send action: ViewAction) -> Binding<ViewState> {
     self.binding(send: { _ in action })
+  }
+
+  private subscript<Value>(
+    get state: HashableWrapper<(ViewState) -> Value>,
+    send action: HashableWrapper<(Value) -> ViewAction>
+  ) -> Value {
+    get { state.rawValue(self.state) }
+    set { self.send(action.rawValue(newValue)) }
   }
 }
 
@@ -745,31 +750,8 @@ public struct StorePublisher<State>: Publisher {
   }
 }
 
-final private class ValueWrapper<V>: ObservableObject {
-  var value: V {
-    willSet { objectWillChange.send() }
-  }
-
-  init(_ value: V) {
-    self.value = value
-  }
-}
-
-@propertyWrapper private struct ObservedState<Value>: DynamicProperty {
-  @ObservedObject private var box: ValueWrapper<Value>
-
-  var wrappedValue: Value {
-    get { box.value }
-    nonmutating set { box.value = newValue }
-  }
-
-  var projectedValue: Binding<Value> {
-    .init(
-      get: { wrappedValue },
-      set: { wrappedValue = $0 }
-    )
-  }
-  init(wrappedValue value: Value) {
-    self._box = ObservedObject(wrappedValue: .init(value))
-  }
+private struct HashableWrapper<Value>: Hashable {
+  let rawValue: Value
+  static func == (lhs: Self, rhs: Self) -> Bool { false }
+  func hash(into hasher: inout Hasher) {}
 }
