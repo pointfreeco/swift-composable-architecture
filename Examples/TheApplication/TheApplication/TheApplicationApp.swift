3ac76f86697487d378a52ce13bf1b5da0f5ecfa7
Akash Soni <akashsoni8285@gmail.com>
2023-01-25 01:43:15 +0530

    Modularization 3 WIP

---
+++ Examples/TheApplication/TheApplication/TheApplicationApp.swift
@@ -0,0 +1,20 @@
+//
+//  TheApplicationApp.swift
+//  TheApplication
+//
+//  Created by Akash soni on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TheApplicationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
