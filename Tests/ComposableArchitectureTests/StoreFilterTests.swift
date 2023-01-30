a3cf0dfbf3ce77f1bb5803fc6d3b2b03ecc6d547
Stephen Celis <stephen@stephencelis.com>
2023-01-30 11:50:20 -0800

    fix

--- Tests/ComposableArchitectureTests/StoreFilterTests.swift
+++ Tests/ComposableArchitectureTests/StoreFilterTests.swift
@@ -1,26 +1,28 @@
-import Combine
-import XCTest
+#if DEBUG
+  import Combine
+  import XCTest
 
-@testable import ComposableArchitecture
+  @testable import ComposableArchitecture
 
-@MainActor
-final class StoreFilterTests: XCTestCase {
-  var cancellables: Set<AnyCancellable> = []
+  @MainActor
+  final class StoreFilterTests: XCTestCase {
+    var cancellables: Set<AnyCancellable> = []
 
-  func testFilter() {
-    let store = Store<Int?, Void>(initialState: nil, reducer: EmptyReducer())
-      .filter { state, _ in state != nil }
+    func testFilter() {
+      let store = Store<Int?, Void>(initialState: nil, reducer: EmptyReducer())
+        .filter { state, _ in state != nil }
 
-    let viewStore = ViewStore(store)
-    var count = 0
-    viewStore.publisher
-      .sink { _ in count += 1 }
-      .store(in: &self.cancellables)
+      let viewStore = ViewStore(store)
+      var count = 0
+      viewStore.publisher
+        .sink { _ in count += 1 }
+        .store(in: &self.cancellables)
 
-    XCTAssertEqual(count, 1)
-    viewStore.send(())
-    XCTAssertEqual(count, 1)
-    viewStore.send(())
-    XCTAssertEqual(count, 1)
+      XCTAssertEqual(count, 1)
+      viewStore.send(())
+      XCTAssertEqual(count, 1)
+      viewStore.send(())
+      XCTAssertEqual(count, 1)
+    }
   }
-}
+#endif
