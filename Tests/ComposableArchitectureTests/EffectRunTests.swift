2ac842a861e58312508dfa27987919da10f67006
Brandon Williams <mbrandonw@hey.com>
2023-02-13 13:16:30 -0800

    fix tests

--- Tests/ComposableArchitectureTests/EffectRunTests.swift
+++ Tests/ComposableArchitectureTests/EffectRunTests.swift
@@ -161,6 +161,7 @@ final class EffectRunTests: XCTestCase {
     let viewStore = ViewStore(store, observe: { $0 })
     await viewStore.send(.tap).finish()
     await queue.advance(by: .seconds(1))
+    try await Task.sleep(nanoseconds: NSEC_PER_MSEC * 300)
   }
 
   func testRunEscapeFailurePublisher() async throws {
@@ -204,5 +205,6 @@ final class EffectRunTests: XCTestCase {
     let viewStore = ViewStore(store, observe: { $0 })
     await viewStore.send(.tap).finish()
     await queue.advance(by: .seconds(1))
+    try await Task.sleep(nanoseconds: NSEC_PER_MSEC * 300)
   }
 }
