3258ec34aa40cc1a12dcd18e1b6b8fa18126c979
mbrandonw <mbrandonw@users.noreply.github.com>
2023-02-21 19:54:57 +0000

    Run swift-format

--- Examples/Integration/IntegrationUITests/ForEachBindingTests.swift
+++ Examples/Integration/IntegrationUITests/ForEachBindingTests.swift
@@ -18,11 +18,13 @@ final class ForEachBindingTests: XCTestCase {
     app.buttons["Remove last"].tap()
     XCTAssertFalse(app.textFields["A"].exists)
 
-    XCTExpectFailure("""
-      This ideally would not fail, but currently does. See this PR for more details:
+    XCTExpectFailure(
+      """
+        This ideally would not fail, but currently does. See this PR for more details:
 
-      https://github.com/pointfreeco/swift-composable-architecture/pull/1845
-    """) {
+        https://github.com/pointfreeco/swift-composable-architecture/pull/1845
+      """
+    ) {
       XCTAssertFalse(app.staticTexts["🛑"].exists)
     }
   }
