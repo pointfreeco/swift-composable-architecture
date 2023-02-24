e3852b8d42bedc4415c6ef486111f499e324f98c
Brandon Williams <mbrandonw@hey.com>
2023-02-24 12:48:07 -0800

    wip

--- Sources/ComposableArchitecture/Internal/EphemeralState.swift
+++ Sources/ComposableArchitecture/Internal/EphemeralState.swift
@@ -1,5 +1,10 @@
+/// Loosely represents features that are only briefly shown and the first time they are interacted
+/// with they go away. Such features do not manage any behavior on the inside.
+///
+/// Alerts and confirmation dialogs are examples of this kind of state.
 public protocol _EphemeralState {}
 
 extension AlertState: _EphemeralState {}
+
 @available(iOS 13, macOS 12, tvOS 13, watchOS 6, *)
 extension ConfirmationDialogState: _EphemeralState {}
