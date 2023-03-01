c20e654e1e4e4178da1de192f5fd552b4d941c68
Brandon Williams <mbrandonw@hey.com>
2023-03-01 08:42:30 -0800

    wip

--- Tests/ComposableArchitectureTests/StoreTests.swift
+++ Tests/ComposableArchitectureTests/StoreTests.swift
@@ -695,33 +695,3 @@ extension DependencyValues {
     set { self[Count.self] = newValue }
   }
 }
-
-
-public struct _Observe<Reducers: ReducerProtocol>: ReducerProtocol {
-  @usableFromInline
-  let reducers: (State, Action) -> Reducers
-
-  /// Initializes a reducer that builds a reducer from the current state and action.
-  ///
-  /// - Parameter build: A reducer builder that has access to the current state and action.
-  @inlinable
-  public init(
-    @ReducerBuilder<Reducers.State, Reducers.Action> _ build: @escaping (Reducers.State, Reducers.Action) -> Reducers
-  ) where Reducers.State == State, Reducers.Action == Action {
-    self.init(internal: build)
-  }
-
-  @usableFromInline
-  init(
-    internal reducers: @escaping (Reducers.State, Reducers.Action) -> Reducers
-  ) {
-    self.reducers = reducers
-  }
-
-  @inlinable
-  public func reduce(
-    into state: inout Reducers.State, action: Reducers.Action
-  ) -> EffectTask<Reducers.Action> {
-    self.reducers(state, action).reduce(into: &state, action: action)
-  }
-}
