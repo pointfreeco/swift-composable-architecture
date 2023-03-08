69a062a6a8e5ccfbaa0f975d8e6a3eecd8fbb871
Stephen Celis <stephen@stephencelis.com>
2023-03-08 10:28:23 -0800

    PresentationStore -> IfLetStore overloads

--- Sources/ComposableArchitecture/SwiftUI/IfLetStore.swift
+++ Sources/ComposableArchitecture/SwiftUI/IfLetStore.swift
@@ -99,6 +99,101 @@ public struct IfLetStore<State, Action, Content: View>: View {
     }
   }
 
+  /// Initializes an ``IfLetStore`` view that computes content depending on if a store of
+  /// ``PresentationState`` and ``PresentationAction`` is `nil` or non-`nil`.
+  ///
+  /// - Parameters:
+  ///   - store: A store of optional state.
+  ///   - ifContent: A function that is given a store of non-optional state and returns a view that
+  ///     is visible only when the optional state is non-`nil`.
+  ///   - elseContent: A view that is only visible when the optional state is `nil`.
+  public init<IfContent, ElseContent>(
+    _ store: Store<PresentationState<State>, PresentationAction<Action>>,
+    @ViewBuilder then ifContent: @escaping (Store<State, Action>) -> IfContent,
+    @ViewBuilder else elseContent: @escaping () -> ElseContent
+  ) where Content == _ConditionalContent<IfContent, ElseContent> {
+    self.init(
+      store.scope(state: { $0.wrappedValue }, action: PresentationAction.presented),
+      then: ifContent,
+      else: elseContent
+    )
+  }
+
+  /// Initializes an ``IfLetStore`` view that computes content depending on if a store of
+  /// ``PresentationState`` and ``PresentationAction`` is `nil` or non-`nil`.
+  ///
+  /// - Parameters:
+  ///   - store: A store of optional state.
+  ///   - ifContent: A function that is given a store of non-optional state and returns a view that
+  ///     is visible only when the optional state is non-`nil`.
+  public init<IfContent>(
+    _ store: Store<PresentationState<State>, PresentationAction<Action>>,
+    @ViewBuilder then ifContent: @escaping (Store<State, Action>) -> IfContent
+  ) where Content == IfContent? {
+    self.init(
+      store.scope(state: { $0.wrappedValue }, action: PresentationAction.presented),
+      then: ifContent
+    )
+  }
+
+  /// Initializes an ``IfLetStore`` view that computes content depending on if a store of
+  /// ``PresentationState`` and ``PresentationAction`` is `nil` or non-`nil` and state can further
+  /// be extracted from the destination state, _e.g._ it matches a particular case of an enum.
+  ///
+  /// - Parameters:
+  ///   - store: A store of optional state.
+  ///   - toState: A closure that attempts to extract state for the "if" branch from the destination
+  ///     state.
+  ///   - fromAction: A closure that embeds actions for the "if" branch in destination actions.
+  ///   - ifContent: A function that is given a store of non-optional state and returns a view that
+  ///     is visible only when the optional state is non-`nil` and state can be extracted from the
+  ///     destination state.
+  ///   - elseContent: A view that is only visible when state cannot be extracted from the
+  ///     destination.
+  public init<DestinationState, DestinationAction, IfContent, ElseContent>(
+    _ store: Store<PresentationState<DestinationState>, PresentationAction<DestinationAction>>,
+    state toState: @escaping (DestinationState) -> State?,
+    action fromAction: @escaping (Action) -> DestinationAction,
+    @ViewBuilder then ifContent: @escaping (Store<State, Action>) -> IfContent,
+    @ViewBuilder else elseContent: @escaping () -> ElseContent
+  ) where Content == _ConditionalContent<IfContent, ElseContent> {
+    self.init(
+      store.scope(
+        state: { $0.wrappedValue.flatMap(toState) },
+        action: { .presented(fromAction($0)) }
+      ),
+      then: ifContent,
+      else: elseContent
+    )
+  }
+
+  /// Initializes an ``IfLetStore`` view that computes content depending on if a store of
+  /// ``PresentationState`` and ``PresentationAction`` is `nil` or non-`nil` and state can further
+  /// be extracted from the destination state, _e.g._ it matches a particular case of an enum.
+  ///
+  /// - Parameters:
+  ///   - store: A store of optional state.
+  ///   - toState: A closure that attempts to extract state for the "if" branch from the destination
+  ///     state.
+  ///   - fromAction: A closure that embeds actions for the "if" branch in destination actions.
+  ///   - ifContent: A function that is given a store of non-optional state and returns a view that
+  ///     is visible only when the optional state is non-`nil` and state can be extracted from the
+  ///     destination state.
+  public init<DestinationState, DestinationAction, IfContent>(
+    _ store: Store<PresentationState<DestinationState>, PresentationAction<DestinationAction>>,
+    state toState: @escaping (DestinationState) -> State?,
+    action fromAction: @escaping (Action) -> DestinationAction,
+    @ViewBuilder then ifContent: @escaping (Store<State, Action>) -> IfContent
+  ) where Content == IfContent? {
+    self.init(
+      store.scope(
+        state: { $0.wrappedValue.flatMap(toState) },
+        action: { .presented(fromAction($0)) }
+      ),
+      then: ifContent
+    )
+  }
+
   public var body: some View {
     WithViewStore(
       self.store,
