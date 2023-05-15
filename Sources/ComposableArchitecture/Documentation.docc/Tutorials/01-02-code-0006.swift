4268d5288caad4205b140fa8d8264bfc70480147
Brandon Williams <mbrandonw@hey.com>
2023-05-15 11:23:54 -0400

    wip

--- Sources/ComposableArchitecture/Documentation.docc/Tutorials/01-02-code-0006.swift
+++ Sources/ComposableArchitecture/Documentation.docc/Tutorials/01-02-code-0006.swift
@@ -2,6 +2,7 @@ struct CounterPreview: PreviewProvider {
   static var previews: some View {
     CounterView(
       store: Store(initialState: CounterFeature.State()) {
+        // CounterFeature()
         EmptyReducer()
       }
     )
