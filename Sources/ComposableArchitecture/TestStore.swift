b177364979c757cd3329cd3f779add97881cd63a
Brandon Williams <mbrandonw@hey.com>
2023-05-24 19:21:45 -0400

    revert store.finish task cancellation

--- Sources/ComposableArchitecture/TestStore.swift
+++ Sources/ComposableArchitecture/TestStore.swift
@@ -831,6 +831,8 @@ public final class TestStore<State, Action, ScopedState, ScopedAction, Environme
     file: StaticString = #file,
     line: UInt = #line
   ) async {
+    Task.cancel(id: OnFirstAppearID())
+    
     let nanoseconds = nanoseconds ?? self.timeout
     let start = DispatchTime.now().uptimeNanoseconds
     await Task.megaYield()
@@ -851,9 +853,6 @@ public final class TestStore<State, Action, ScopedState, ScopedAction, Environme
           If you are not yet using a clock/scheduler, or can not use a clock/scheduler, \
           \(timeoutMessage).
           """
-        Task.cancel(id: OnFirstAppearID())
-        guard !self.reducer.inFlightEffects.isEmpty
-        else { break }
         XCTFailHelper(
           """
           Expected effects to finish, but there are still effects in-flight\
