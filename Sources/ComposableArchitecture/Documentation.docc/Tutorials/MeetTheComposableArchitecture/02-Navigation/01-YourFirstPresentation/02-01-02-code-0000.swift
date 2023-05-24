dfcca3692afceb44066b3f0b3dda17e8ab593a94
Brandon Williams <mbrandonw@hey.com>
2023-05-24 15:00:13 -0400

    wip

---
+++ Sources/ComposableArchitecture/Documentation.docc/Tutorials/MeetTheComposableArchitecture/02-Navigation/01-YourFirstPresentation/02-01-02-code-0000.swift
@@ -0,0 +1,17 @@
+struct ContactsFeature: ReducerProtocol {
+  struct State: Equatable {
+    var contacts: IdentifiedArrayOf<Contact> = []
+  }
+  enum Action {
+    case addButtonTapped
+  }
+  var body: some ReducerProtocolOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .addButtonTapped:
+        // TODO
+        return .none
+      }
+    }
+  }
+}
