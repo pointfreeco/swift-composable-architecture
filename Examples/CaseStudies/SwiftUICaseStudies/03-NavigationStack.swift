236263b820b4551b02272a95b1638e1348cb5b33
Jager-yoo <y73447jh@gmail.com>
2023-07-04 22:02:46 +0900

    Resolve CaseStudies crash

--- Examples/CaseStudies/SwiftUICaseStudies/03-NavigationStack.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/03-NavigationStack.swift
@@ -121,19 +121,19 @@ struct NavigationDemoView: View {
       switch $0 {
       case .screenA:
         CaseLet(
-          state: /NavigationDemo.Path.State.screenA,
+          /NavigationDemo.Path.State.screenA,
           action: NavigationDemo.Path.Action.screenA,
           then: ScreenAView.init(store:)
         )
       case .screenB:
         CaseLet(
-          state: /NavigationDemo.Path.State.screenB,
+          /NavigationDemo.Path.State.screenB,
           action: NavigationDemo.Path.Action.screenB,
           then: ScreenBView.init(store:)
         )
       case .screenC:
         CaseLet(
-          state: /NavigationDemo.Path.State.screenC,
+          /NavigationDemo.Path.State.screenC,
           action: NavigationDemo.Path.Action.screenC,
           then: ScreenCView.init(store:)
         )
