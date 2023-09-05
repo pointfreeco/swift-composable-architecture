cb05f1a9ba25105823666331bc9d125c88a8fb3b
tomokisun <github@tomokisun.com>
2023-09-05 23:45:07 +0900

    refactor: callAsFunction(animation:)

--- Sources/ComposableArchitecture/Dependencies/Dismiss.swift
+++ Sources/ComposableArchitecture/Dependencies/Dismiss.swift
@@ -89,24 +89,7 @@ public struct DismissEffect: Sendable {
     fileID: StaticString = #fileID,
     line: UInt = #line
   ) async {
-    guard let dismiss = self.dismiss
-    else {
-      runtimeWarn(
-        """
-        A reducer requested dismissal at "\(fileID):\(line)", but couldn't be dismissed. …
-
-        This is generally considered an application logic error, and can happen when a reducer \
-        assumes it runs in a presentation context. If a reducer can run at both the root level \
-        of an application, as well as in a presentation destination, use \
-        @Dependency(\\.isPresented) to determine if the reducer is being presented before calling \
-        @Dependency(\\.dismiss).
-        """
-      )
-      return
-    }
-    withAnimation(animation) {
-      dismiss()
-    }
+    await callAsFunction(transaction: Transaction(animation: animation))
   }
   
   @MainActor
