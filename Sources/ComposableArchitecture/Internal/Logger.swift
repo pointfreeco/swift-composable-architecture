e09d8591adbaeefdcf4a9ed8ab98df8316cdce5d
Brandon Williams <mbrandonw@hey.com>
2023-09-28 13:31:26 +0200

    wip

--- Sources/ComposableArchitecture/Internal/Logger.swift
+++ Sources/ComposableArchitecture/Internal/Logger.swift
@@ -2,13 +2,13 @@ import OSLog
 
 @_spi(Logging)
 public final class Logger {
-  @available(iOS 14.0, *)
-  var logger: os.Logger {
-    os.Logger(subsystem: "composable-architecture", category: "store-events")
-  }
-  @Published public var logs: [String] = []
   public static let shared = Logger()
+  @Published public var logs: [String] = []
   #if DEBUG
+    @available(iOS 14.0, *)
+    var logger: os.Logger {
+      os.Logger(subsystem: "composable-architecture", category: "store-events")
+    }
     public func log(level: OSLogType = .default, _ string: @autoclosure () -> String) {
       let string = string()
       if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
