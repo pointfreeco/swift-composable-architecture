ac52d3481a2e890b7839c5919895b2af5b084e65
Brandon Williams <mbrandonw@hey.com>
2023-09-28 14:42:16 +0200

    wip

--- Examples/Integration/IntegrationUITests/PresentationTests.swift
+++ Examples/Integration/IntegrationUITests/PresentationTests.swift
@@ -8,4 +8,111 @@ final class PresentationTests: BaseIntegrationTests {
     self.app.buttons["Presentation"].tap()
     self.clearLogs()
   }
+
+  func testOptional() {
+    self.app.buttons["Present sheet"].tap()
+    self.app.buttons["Increment"].tap()
+    self.app.buttons["Dismiss"].firstMatch.tap()
+    self.assertLogs([
+      .unordered(
+        """
+        Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+        Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+        StoreOf<Integration.BasicsView.Feature>.init
+        StoreOf<Integration.BasicsView.Feature>.init
+        BasicsView.body
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+        Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        StoreOf<Integration.BasicsView.Feature>.scope
+        BasicsView.body
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        StoreOf<Integration.BasicsView.Feature>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+        Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+        Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        StoreOf<Integration.PresentationView.Feature>.scope
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+        StoreOf<Integration.BasicsView.Feature>.deinit
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+        StoreOf<Integration.BasicsView.Feature>.deinit
+        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+        """)
+    ])
+  }
+
+  func testOptional_ObserveChildCount() {
+    self.app.buttons["Present sheet"].tap()
+    self.app.buttons["Observe child count"].tap()
+    self.app.buttons["Increment"].tap()
+    XCTAssertEqual(self.app.staticTexts["Count: 1"].exists, true)
+    self.app.buttons["Dismiss"].firstMatch.tap()
+    self.assertLogs([
+      .unordered("""
+      StoreOf<Integration.PresentationView.Feature>.scope
+      Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+      Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+      StoreOf<Integration.PresentationView.Feature>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+      StoreOf<Integration.BasicsView.Feature>.init
+      StoreOf<Integration.BasicsView.Feature>.init
+      BasicsView.body
+      StoreOf<Integration.PresentationView.Feature>.scope
+      PresentationView.body
+      StoreOf<Integration.PresentationView.Feature>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+      Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+      StoreOf<Integration.PresentationView.Feature>.scope
+      StoreOf<Integration.BasicsView.Feature>.scope
+      PresentationView.body
+      BasicsView.body
+      StoreOf<Integration.PresentationView.Feature>.scope
+      StoreOf<Integration.PresentationView.Feature>.scope
+      StoreOf<Integration.PresentationView.Feature>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      StoreOf<Integration.BasicsView.Feature>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+      Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+      Store<ComposableArchitecture.PresentationState<Integration.BasicsView.Feature.State>, ComposableArchitecture.PresentationAction<Integration.BasicsView.Feature.Action>>.scope
+      StoreOf<Integration.PresentationView.Feature>.scope
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+      PresentationView.body
+      StoreOf<Integration.BasicsView.Feature>.deinit
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+      StoreOf<Integration.BasicsView.Feature>.deinit
+      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+      """)
+    ])
+  }
 }
