cf6576fcf2c900805657a495533bdec5b5c07d56
Brandon Williams <mbrandonw@hey.com>
2023-10-07 14:06:41 -0400

    comment out all but one test

--- Examples/Integration/IntegrationUITests/EnumTests.swift
+++ Examples/Integration/IntegrationUITests/EnumTests.swift
@@ -1,143 +1,143 @@
-import TestCases
-import XCTest
-
-@MainActor
-final class EnumTests: BaseIntegrationTests {
-  override func setUp() {
-    super.setUp()
-    self.app.buttons["Enum"].tap()
-    self.clearLogs()
-  }
-
-  func testBasics() {
-    self.app.buttons["Toggle feature 1 on"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
-    self.assertLogs([
-      .unordered(
-        """
-        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-        StoreOf<Integration.EnumView.Feature>.scope
-        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-        StoreOf<Integration.EnumView.Feature>.scope
-        StoreOf<Integration.EnumView.Feature>.scope
-        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
-        StoreOf<Integration.BasicsView.Feature>.init
-        EnumView.body
-        StoreOf<Integration.BasicsView.Feature>.init
-        BasicsView.body
-        """)
-    ])
-    self.app.buttons["Increment"].tap()
-    XCTAssertEqual(self.app.staticTexts["1"].exists, true)
-    self.assertLogs([
-      .unordered(
-        """
-        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-        StoreOf<Integration.EnumView.Feature>.scope
-        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-        StoreOf<Integration.EnumView.Feature>.scope
-        StoreOf<Integration.EnumView.Feature>.scope
-        StoreOf<Integration.BasicsView.Feature>.scope
-        BasicsView.body
-        """)
-    ])
-  }
-
-  func testToggle1On_Toggle1Off() {
-    self.app.buttons["Toggle feature 1 on"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
-    self.clearLogs()
-    self.app.buttons["Toggle feature 1 off"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, false)
-    self.assertLogs([
-      .unordered("""
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      EnumView.body
-      StoreOf<Integration.BasicsView.Feature>.deinit
-      StoreOf<Integration.BasicsView.Feature>.deinit
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
-      """)
-    ])
-  }
-
-  func testToggle1On_Toggle2On() {
-    self.app.buttons["Toggle feature 1 on"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
-    self.clearLogs()
-    self.app.buttons["Toggle feature 2 on"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 2"].exists, true)
-    self.assertLogs([
-      .unordered("""
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
-      StoreOf<Integration.BasicsView.Feature>.init
-      EnumView.body
-      StoreOf<Integration.BasicsView.Feature>.deinit
-      StoreOf<Integration.BasicsView.Feature>.init
-      BasicsView.body
-      StoreOf<Integration.BasicsView.Feature>.deinit
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
-      """)
-    ])
-  }
-
-  func testDismiss() {
-    self.app.buttons["Toggle feature 1 on"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
-    self.clearLogs()
-    self.app.buttons["Dismiss"].tap()
-    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, false)
-    self.assertLogs([
-      .unordered("""
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
-      StoreOf<Integration.EnumView.Feature>.scope
-      EnumView.body
-      StoreOf<Integration.BasicsView.Feature>.deinit
-      StoreOf<Integration.BasicsView.Feature>.deinit
-      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
-      """)
-    ])
-  }
-}
+//import TestCases
+//import XCTest
+//
+//@MainActor
+//final class EnumTests: BaseIntegrationTests {
+//  override func setUp() {
+//    super.setUp()
+//    self.app.buttons["Enum"].tap()
+//    self.clearLogs()
+//  }
+//
+//  func testBasics() {
+//    self.app.buttons["Toggle feature 1 on"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
+//    self.assertLogs([
+//      .unordered(
+//        """
+//        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//        StoreOf<Integration.EnumView.Feature>.scope
+//        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//        StoreOf<Integration.EnumView.Feature>.scope
+//        StoreOf<Integration.EnumView.Feature>.scope
+//        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+//        StoreOf<Integration.BasicsView.Feature>.init
+//        EnumView.body
+//        StoreOf<Integration.BasicsView.Feature>.init
+//        BasicsView.body
+//        """)
+//    ])
+//    self.app.buttons["Increment"].tap()
+//    XCTAssertEqual(self.app.staticTexts["1"].exists, true)
+//    self.assertLogs([
+//      .unordered(
+//        """
+//        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//        Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//        StoreOf<Integration.EnumView.Feature>.scope
+//        Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//        StoreOf<Integration.EnumView.Feature>.scope
+//        StoreOf<Integration.EnumView.Feature>.scope
+//        StoreOf<Integration.BasicsView.Feature>.scope
+//        BasicsView.body
+//        """)
+//    ])
+//  }
+//
+//  func testToggle1On_Toggle1Off() {
+//    self.app.buttons["Toggle feature 1 on"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
+//    self.clearLogs()
+//    self.app.buttons["Toggle feature 1 off"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, false)
+//    self.assertLogs([
+//      .unordered("""
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      EnumView.body
+//      StoreOf<Integration.BasicsView.Feature>.deinit
+//      StoreOf<Integration.BasicsView.Feature>.deinit
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+//      """)
+//    ])
+//  }
+//
+//  func testToggle1On_Toggle2On() {
+//    self.app.buttons["Toggle feature 1 on"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
+//    self.clearLogs()
+//    self.app.buttons["Toggle feature 2 on"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 2"].exists, true)
+//    self.assertLogs([
+//      .unordered("""
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.init
+//      StoreOf<Integration.BasicsView.Feature>.init
+//      EnumView.body
+//      StoreOf<Integration.BasicsView.Feature>.deinit
+//      StoreOf<Integration.BasicsView.Feature>.init
+//      BasicsView.body
+//      StoreOf<Integration.BasicsView.Feature>.deinit
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+//      """)
+//    ])
+//  }
+//
+//  func testDismiss() {
+//    self.app.buttons["Toggle feature 1 on"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, true)
+//    self.clearLogs()
+//    self.app.buttons["Dismiss"].tap()
+//    XCTAssertEqual(self.app.staticTexts["Feature 1"].exists, false)
+//    self.assertLogs([
+//      .unordered("""
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      Store<ComposableArchitecture.PresentationState<Integration.EnumView.Feature.Destination.State>, ComposableArchitecture.PresentationAction<Integration.EnumView.Feature.Destination.Action>>.scope
+//      StoreOf<Integration.EnumView.Feature>.scope
+//      EnumView.body
+//      StoreOf<Integration.BasicsView.Feature>.deinit
+//      StoreOf<Integration.BasicsView.Feature>.deinit
+//      Store<Swift.Optional<Integration.BasicsView.Feature.State>, Integration.BasicsView.Feature.Action>.deinit
+//      """)
+//    ])
+//  }
+//}
--- Examples/Integration/IntegrationUITests/NavigationTests.swift
+++ Examples/Integration/IntegrationUITests/NavigationTests.swift
@@ -1,59 +1,59 @@
-import TestCases
-import XCTest
-
-@MainActor
-final class NavigationTests: BaseIntegrationTests {
-  override func setUp() {
-    super.setUp()
-    self.app.buttons["Navigation"].tap()
-    self.clearLogs()
-  }
-
-  func testBasics() {
-    self.app.buttons["Push feature"].tap()
-    self.assertLogs("""
-    Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.init
-    StoreOf<Integration.BasicsView.Feature>.init
-    StoreOf<Integration.BasicsView.Feature>.init
-    BasicsView.body
-    """)
-    self.app.buttons["Increment"].tap()
-    self.assertLogs("""
-    Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-    StoreOf<Integration.NavigationTestCaseView.Feature>.scope
-    StoreOf<Integration.BasicsView.Feature>.scope
-    BasicsView.body
-    """)
-  }
-
-
-  func testDeepStack() {
-    self.app.buttons["Push feature"].tap()
-    self.app.buttons["Push feature"].tap()
-    self.app.buttons["Push feature"].tap()
-    self.app.buttons["Push feature"].tap()
-    self.app.buttons["Push feature"].tap()
-    self.clearLogs()
-    self.app.buttons["Increment"].tap()
-    self.assertLogs([
-      .unordered("""
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
-      StoreOf<Integration.NavigationTestCaseView.Feature>.scope
-      StoreOf<Integration.BasicsView.Feature>.scope
-      BasicsView.body
-      """)
-    ])
-  }
-}
+//import TestCases
+//import XCTest
+//
+//@MainActor
+//final class NavigationTests: BaseIntegrationTests {
+//  override func setUp() {
+//    super.setUp()
+//    self.app.buttons["Navigation"].tap()
+//    self.clearLogs()
+//  }
+//
+//  func testBasics() {
+//    self.app.buttons["Push feature"].tap()
+//    self.assertLogs("""
+//    Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.init
+//    StoreOf<Integration.BasicsView.Feature>.init
+//    StoreOf<Integration.BasicsView.Feature>.init
+//    BasicsView.body
+//    """)
+//    self.app.buttons["Increment"].tap()
+//    self.assertLogs("""
+//    Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//    StoreOf<Integration.NavigationTestCaseView.Feature>.scope
+//    StoreOf<Integration.BasicsView.Feature>.scope
+//    BasicsView.body
+//    """)
+//  }
+//
+//
+//  func testDeepStack() {
+//    self.app.buttons["Push feature"].tap()
+//    self.app.buttons["Push feature"].tap()
+//    self.app.buttons["Push feature"].tap()
+//    self.app.buttons["Push feature"].tap()
+//    self.app.buttons["Push feature"].tap()
+//    self.clearLogs()
+//    self.app.buttons["Increment"].tap()
+//    self.assertLogs([
+//      .unordered("""
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      Store<ComposableArchitecture.StackState<Integration.BasicsView.Feature.State>, ComposableArchitecture.StackAction<Integration.BasicsView.Feature.State, Integration.BasicsView.Feature.Action>>.scope
+//      StoreOf<Integration.NavigationTestCaseView.Feature>.scope
+//      StoreOf<Integration.BasicsView.Feature>.scope
+//      BasicsView.body
+//      """)
+//    ])
+//  }
+//}
