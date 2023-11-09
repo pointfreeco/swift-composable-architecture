4f9df4dc42606c0078640d4cf1a7b8202a12a9f2
mbrandonw <mbrandonw@users.noreply.github.com>
2023-11-09 03:51:08 +0000

    Run swift-format

--- Sources/ComposableArchitecture/Internal/AreOrderedSetsDuplicates.swift
+++ Sources/ComposableArchitecture/Internal/AreOrderedSetsDuplicates.swift
@@ -5,7 +5,7 @@ import OrderedCollections
 func areOrderedSetsDuplicates<T>(_ lhs: OrderedSet<T>, _ rhs: OrderedSet<T>) -> Bool {
   guard lhs.count == rhs.count
   else { return false }
-  
+
   return withUnsafePointer(to: lhs) { lhsPointer in
     withUnsafePointer(to: rhs) { rhsPointer in
       memcmp(lhsPointer, rhsPointer, MemoryLayout<OrderedSet<T>>.size) == 0 || lhs == rhs
