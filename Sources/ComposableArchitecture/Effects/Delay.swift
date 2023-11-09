264280f5f683531bcf0aaf6c3cfd4d33a3eaf36b
Jan Provazník <proovaznik@gmail.com>
2023-11-09 10:47:15 +0100

    effect delay uses async and await

--- Sources/ComposableArchitecture/Effects/Delay.swift
+++ Sources/ComposableArchitecture/Effects/Delay.swift
@@ -17,6 +17,10 @@ extension Effect {
   ///     canceled before starting this new one.
   /// - Returns: An effect that publishes events  after a specified time.
   ///
+  @available(macOS, deprecated: 13.0, message: "use delay with a clock and tolereance parameters insted (`.delay(id:for:tolerance:clock:cancelInFlight)`).")
+  @available(iOS, deprecated: 16.0, message: "use delay with a clock and tolereance parameters insted (`.delay(id:for:tolerance:clock:cancelInFlight)`).")
+  @available(watchOS, deprecated: 9.0, message: "use delay with a clock and tolereance parameters insted (`.delay(id:for:tolerance:clock:cancelInFlight)`).")
+  @available(tvOS, deprecated: 16.0, message: "use delay with a clock and tolereance parameters insted (`.delay(id:for:tolerance:clock:cancelInFlight)`).")
   public func delay<ID: Hashable, S: Scheduler>(
     id: ID,
     for dueTime: S.SchedulerTimeType.Stride,
@@ -41,6 +45,51 @@ extension Effect {
     case .run:
       return .publisher { _EffectPublisher(self) }
         .delay(id: id, for: dueTime, scheduler: scheduler, options: options)
+    }
+  }
+
+  /// Delay an effect. using async.
+  ///
+  /// - Parameters:
+  ///   - id: The effect's identifier.
+  ///   - duration: The duration you want to debounce for.
+  ///   - tolerance: The tolarence you want accept.
+  ///   - clock: Th clock you want to use for delaying/
+  ///   - cancelInFlight: Determines if any in-flight effect with the same identifier should be
+  ///     canceled before starting this new one.
+  /// - Returns: An effect that publishes events  after a specified time.
+  ///
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  public func delay<ID: Hashable, C: Clock<Duration>>(
+    id: ID,
+    for duration: C.Instant.Duration,
+    tolerance: C.Instant.Duration? = nil,
+    clock: C,
+    cancelInFlight: Bool = false
+  ) -> Self {
+    switch self.operation {
+    case .none:
+      return .none
+
+    case let .publisher(publisher):
+      return Self(
+        operation: .run { send in
+          for await action in publisher.values {
+            _ = try? await clock.sleep(for: duration, tolerance: tolerance)
+            await send(action)
+          }
+        }
+      )
+      .cancellable(id: id, cancelInFlight: cancelInFlight)
+
+    case let .run(priority, operation):
+      return Self(
+        operation: .run(priority) { send in
+          _ = try? await clock.sleep(for: duration, tolerance: tolerance)
+          await operation(send)
+        }
+      )
+      .cancellable(id: id, cancelInFlight: cancelInFlight)
     }
   }
 }
