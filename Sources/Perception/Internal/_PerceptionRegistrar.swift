611402e0129589d7ec891518598d3d718b47cc41
Brandon Williams <mbrandonw@hey.com>
2023-11-09 17:35:07 -0800

    copyrights

--- Sources/Perception/Internal/_PerceptionRegistrar.swift
+++ Sources/Perception/Internal/_PerceptionRegistrar.swift
@@ -1,3 +1,14 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
 struct _PerceptionRegistrar: Sendable {
   internal class ValuePerceptionStorage {
     func emit<Element>(_ element: Element) -> Bool { return false }
