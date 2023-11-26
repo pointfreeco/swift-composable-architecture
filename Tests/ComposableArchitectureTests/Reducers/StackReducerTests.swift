0a19daf994e01df4310d3be4c60996f66f5f15b9
Stephen Celis <stephen@stephencelis.com>
2023-11-26 14:43:06 -0800

    wip

--- Tests/ComposableArchitectureTests/Reducers/StackReducerTests.swift
+++ Tests/ComposableArchitectureTests/Reducers/StackReducerTests.swift
@@ -798,7 +798,7 @@ final class StackReducerTests: BaseTCATestCase {
           • This action was sent to the store while its state contained no element at this ID. To \
           fix this make sure that actions for this reducer can only be sent from a view store when \
           its state contains an element at this id. In SwiftUI applications, use \
-          "NavigationStack.init(store:)".
+          "NavigationStack.init(path:)" with a binding to a store.
           """
       }
 
