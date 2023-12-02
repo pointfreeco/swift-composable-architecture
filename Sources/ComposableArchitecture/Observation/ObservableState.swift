c90b7a052bfb7e6ee05c676e080f0cca46b381f2
Stephen Celis <stephen@stephencelis.com>
2023-12-02 10:51:41 -0800

    Revert "wip"
    This reverts commit f221ed0e1adae5e7fed42afdf00aef02eb4abdb9.

--- Sources/ComposableArchitecture/Observation/ObservableState.swift
+++ Sources/ComposableArchitecture/Observation/ObservableState.swift
@@ -36,43 +36,6 @@ public struct ObservableStateID: Equatable, Hashable, Sendable {
   }
 }
 
-public func _$isIdentityEqual<T>(_ lhs: T, _ rhs: T) -> Bool {
-  func openCollection<C: Collection>(_ lhs: C, _ rhs: Any) -> Bool {
-    guard C.Element.self is ObservableState.Type else { return false }
-
-    func openIdentifiable<Element: Identifiable>(_: Element.Type) -> Bool? {
-      guard
-        let lhs = lhs as? IdentifiedArrayOf<Element>,
-        let rhs = rhs as? IdentifiedArrayOf<Element>
-      else { return nil }
-      return _$isIdentityEqual(lhs, rhs)
-    }
-
-    if
-      let identifiable = C.Element.self as? any Identifiable.Type,
-      let result = openIdentifiable(identifiable)
-    {
-      return result
-    } else if let rhs = rhs as? C {
-      return lhs.count == rhs.count && zip(lhs, rhs).allSatisfy(_$isIdentityEqual)
-    } else {
-      return false
-    }
-  }
-
-  if let lhs = lhs as? any ObservableState, let rhs = rhs as? any ObservableState {
-    return _$isIdentityEqual(lhs, rhs)
-  } else if let lhs = lhs as? any Collection {
-    return openCollection(lhs, rhs)
-  } else {
-    return false
-  }
-}
-
-public func _$isIdentityEqual<T: ObservableState>(_ lhs: T, _ rhs: T) -> Bool {
-  lhs._$id == rhs._$id
-}
-
 public func _$isIdentityEqual<ID: Hashable, T: ObservableState>(
   _ lhs: IdentifiedArray<ID, T>, _ rhs: IdentifiedArray<ID, T>
 ) -> Bool {
@@ -107,4 +70,37 @@ where C.Element: ObservableState {
 
 public func _$isIdentityEqual(_ lhs: String, _ rhs: String) -> Bool {
   false
+}
+
+public func _$isIdentityEqual<T>(_ lhs: T, _ rhs: T) -> Bool {
+  func openCollection<C: Collection>(_ lhs: C, _ rhs: Any) -> Bool {
+    guard C.Element.self is ObservableState.Type else { return false }
+
+    func openIdentifiable<Element: Identifiable>(_: Element.Type) -> Bool? {
+      guard
+        let lhs = lhs as? IdentifiedArrayOf<Element>,
+        let rhs = rhs as? IdentifiedArrayOf<Element>
+      else { return nil }
+      return areOrderedSetsDuplicates(lhs.ids, rhs.ids)
+    }
+
+    if
+      let identifiable = C.Element.self as? any Identifiable.Type,
+      let result = openIdentifiable(identifiable)
+    {
+      return result
+    } else if let rhs = rhs as? C {
+      return lhs.count == rhs.count && zip(lhs, rhs).allSatisfy(_$isIdentityEqual)
+    } else {
+      return false
+    }
+  }
+
+  if let lhs = lhs as? any ObservableState, let rhs = rhs as? any ObservableState {
+    return lhs._$id == rhs._$id
+  } else if let lhs = lhs as? any Collection {
+    return openCollection(lhs, rhs)
+  } else {
+    return false
+  }
 }
