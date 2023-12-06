b86978b230c5ec466979d7011c3e52b652832190
Brandon Williams <mbrandonw@hey.com>
2023-12-06 12:59:54 -0500

    wip

--- Tests/ComposableArchitectureTests/ScopeCacheTests.swift
+++ Tests/ComposableArchitectureTests/ScopeCacheTests.swift
@@ -15,13 +15,44 @@ final class ScopeCacheTests: BaseTCATestCase {
     _ = cancellable
   }
 
+
+  func testOptionalScope_StoreIfLet_UncachedStore_XYZ() {
+    let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
+      Feature()
+    }
+    XCTExpectFailure {
+      store
+        .scope(state: { $0 }, action: { $0 })
+        .scope(state: \.child, action: \.child)?
+        .send(.tap)
+    } issueMatcher: {
+      $0.compactDescription == """
+        Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure all \
+        store scoping operations in your application have been updated to take key paths and case \
+        key paths instead of transform functions, which have been deprecated.
+        """
+    }
+  }
+
+  func testOptionalScope_StoreIfLet_UncachedStore_XYZ111() {
+    let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
+      Feature()
+    }
+    store
+      .scope(state: \.self, action: \.self)
+      .scope(state: \.child, action: \.child)?
+      .send(.tap)
+  }
+
+
+
   func testOptionalScope_StoreIfLet_UncachedStore() {
     let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
       Feature()
     }
     XCTExpectFailure {
       let cancellable = store
-        .scope(state: { $0.child }, action: { .child($0) })
+        .scope(state: { $0 }, action: { $0 })
         .ifLet { store in
           store.scope(state: \.child, action: \.child)?.send(.tap)
         }
@@ -40,7 +71,11 @@ final class ScopeCacheTests: BaseTCATestCase {
       Feature()
     }
 
-    let rowsStore = Array(store.scope(state: \.rows, action: \.rows))
+    let rowsStore = Array(
+      store
+        .scope(state: \.self, action: \.self)
+        .scope(state: \.rows, action: \.rows)
+    )
     rowsStore[0].send(.tap)
   }
 
