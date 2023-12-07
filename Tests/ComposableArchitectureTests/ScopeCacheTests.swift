4d4b78ca67f7bbe3fdae9ed7d2a4e591085f2200
Brandon Williams <mbrandonw@hey.com>
2023-12-07 16:03:22 -0500

    wip

--- Tests/ComposableArchitectureTests/ScopeCacheTests.swift
+++ Tests/ComposableArchitectureTests/ScopeCacheTests.swift
@@ -4,95 +4,110 @@ import XCTest
 @MainActor
 final class ScopeCacheTests: BaseTCATestCase {
   func testOptionalScope_UncachedStore() {
-    let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
-      Feature()
-    }
-    XCTExpectFailure {
-      store
-        .scope(state: { $0 }, action: { $0 })
-        .scope(state: \.child, action: \.child)?
-        .send(.tap)
-    } issueMatcher: {
-      $0.compactDescription == """
-        Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure all \
-        store scoping operations in your application have been updated to take key paths and case \
-        key paths instead of transform functions, which have been deprecated.
-        """
-    }
+    #if DEBUG
+      let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
+        Feature()
+      }
+      XCTExpectFailure {
+        _ =
+          store
+          .scope(state: { $0 }, action: { $0 })
+          .scope(state: \.child, action: \.child)?
+          .send(.tap)
+      } issueMatcher: {
+        $0.compactDescription == """
+          Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure that all \
+          parent store scoping operations take key paths and case key paths instead of transform \
+          functions, which have been deprecated.
+          """
+      }
+    #endif
   }
 
   func testOptionalScope_CachedStore() {
-    let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
-      Feature()
-    }
-    store
-      .scope(state: \.self, action: \.self)
-      .scope(state: \.child, action: \.child)?
-      .send(.tap)
+    #if DEBUG
+      let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
+        Feature()
+      }
+      store
+        .scope(state: \.self, action: \.self)
+        .scope(state: \.child, action: \.child)?
+        .send(.tap)
+    #endif
   }
 
   func testOptionalScope_StoreIfLet() {
-    let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
-      Feature()
-    }
-    let cancellable = store
-      .scope(state: \.child, action: \.child)
-      .ifLet { store in
-        store.scope(state: \.child, action: \.child)?.send(.tap)
+    #if DEBUG
+      let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
+        Feature()
       }
-    _ = cancellable
-  }
-
-  func testOptionalScope_StoreIfLet_UncachedStore() {
-    let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
-      Feature()
-    }
-    XCTExpectFailure {
-      let cancellable = store
-        .scope(state: { $0 }, action: { $0 })
+      let cancellable =
+        store
+        .scope(state: \.child, action: \.child)
         .ifLet { store in
           store.scope(state: \.child, action: \.child)?.send(.tap)
         }
       _ = cancellable
-    } issueMatcher: {
-      $0.compactDescription == """
-        Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure all \
-        store scoping operations in your application have been updated to take key paths and case \
-        key paths instead of transform functions, which have been deprecated.
-        """
-    }
+    #endif
+  }
+
+  func testOptionalScope_StoreIfLet_UncachedStore() {
+    #if DEBUG
+      let store = StoreOf<Feature>(initialState: Feature.State(child: Feature.State())) {
+        Feature()
+      }
+      XCTExpectFailure {
+        let cancellable =
+          store
+          .scope(state: { $0 }, action: { $0 })
+          .ifLet { store in
+            store.scope(state: \.child, action: \.child)?.send(.tap)
+          }
+        _ = cancellable
+      } issueMatcher: {
+        $0.compactDescription == """
+          Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure that all \
+          parent store scoping operations take key paths and case key paths instead of transform \
+          functions, which have been deprecated.
+          """
+      }
+    #endif
   }
 
   func testIdentifiedArrayScope_CachedStore() {
-    let store = StoreOf<Feature>(initialState: Feature.State(rows: [Feature.State()])) {
-      Feature()
-    }
+    #if DEBUG
+      let store = StoreOf<Feature>(initialState: Feature.State(rows: [Feature.State()])) {
+        Feature()
+      }
 
-    let rowsStore = Array(
-      store
-        .scope(state: \.self, action: \.self)
-        .scope(state: \.rows, action: \.rows)
-    )
-    rowsStore[0].send(.tap)
+      let rowsStore = Array(
+        store
+          .scope(state: \.self, action: \.self)
+          .scope(state: \.rows, action: \.rows)
+      )
+      rowsStore[0].send(.tap)
+    #endif
   }
 
   func testIdentifiedArrayScope_UncachedStore() {
-    let store = StoreOf<Feature>(initialState: Feature.State(rows: [Feature.State()])) {
-      Feature()
-    }
-    XCTExpectFailure {
-      _ = Array(
-        store
-          .scope(state: { $0 }, action: { $0 })
-          .scope(state: \.rows, action: \.rows)
-      )
-    } issueMatcher: {
-      $0.compactDescription == """
-        Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure all \
-        store scoping operations in your application have been updated to take key paths and case \
-        key paths instead of transform functions, which have been deprecated.
-        """
-    }
+    #if DEBUG
+      let store = StoreOf<Feature>(initialState: Feature.State(rows: [Feature.State()])) {
+        Feature()
+      }
+      XCTExpectFailure {
+        _ = Array(
+          store
+            .scope(state: { $0 }, action: { $0 })
+            .scope(state: \.rows, action: \.rows)
+        )
+      } issueMatcher: {
+        $0.compactDescription == """
+          Scoping from uncached StoreOf<Feature> is not compatible with observation. Ensure that all \
+          parent store scoping operations take key paths and case key paths instead of transform \
+          functions, which have been deprecated.
+          """
+      }
+    #endif
   }
 }
 
