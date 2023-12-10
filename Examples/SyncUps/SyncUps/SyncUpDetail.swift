b29d5defe7163f9f5843cc9aa7b81c2fb308c66d
Stephen Celis <stephen@stephencelis.com>
2023-12-10 14:26:38 -0800

    fix

--- Examples/SyncUps/SyncUps/SyncUpDetail.swift
+++ Examples/SyncUps/SyncUps/SyncUpDetail.swift
@@ -7,6 +7,15 @@ struct SyncUpDetail {
   struct State: Equatable {
     @Presents var destination: Destination.State?
     var syncUp: SyncUp
+
+    // NB: This initializer is required in Xcode 15.0.1 (which CI uses at the time of writing
+    //     this). We can remove when Xcode 15.1 is released and CI uses it.
+    #if swift(<5.9.2)
+      init(destination: Destination.State? = nil, syncUp: SyncUp) {
+        self.destination = destination
+        self.syncUp = syncUp
+      }
+    #endif
   }
 
   enum Action: Sendable {
