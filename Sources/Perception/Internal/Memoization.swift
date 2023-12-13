bc29d1421902267d45957f5d2c73fc86c402fed6
Brandon Williams <mbrandonw@hey.com>
2023-12-13 09:51:38 -0600

    Small updates to perception caching.

--- Sources/Perception/Internal/Memoization.swift
+++ Sources/Perception/Internal/Memoization.swift
@@ -1,68 +1,43 @@
 import Foundation
 import OrderedCollections
 
-final class MemoizedCache<Key: Hashable, Value>: @unchecked Sendable {
-  private var dictionary = OrderedDictionary<Key, Value>()
-  private var lock = NSLock()
-  private let maxCapacity: Int
-  
-  // TODO possibly remove these stats later
-  private var totalEvictions = 0
-  private var totalCalls = 0
-  private var totalHits = 0
-  
+func memoize<Result>(
+  maxCapacity: Int = 500,
+  _ apply: @escaping () -> Result
+) -> () -> Result {
+  let cache = Cache<[NSNumber], Result>(maxCapacity: maxCapacity)
+  let callStack = Thread.callStackReturnAddresses
+  return {
+    guard let memoizedResult = cache[callStack]
+    else {
+      let result = apply()
+      defer { cache[callStack] = result }
+      return result
+    }
+    return memoizedResult
+  }
+}
+
+private final class Cache<Key: Hashable, Value>: @unchecked Sendable {
+  var dictionary = OrderedDictionary<Key, Value>()
+  var lock = NSLock()
+  let maxCapacity: Int
   init(maxCapacity: Int = 500) {
     self.maxCapacity = maxCapacity
   }
-  
-  func printStats() {
-    lock.sync {
-      let hitRate = (Float(totalHits) / Float(totalCalls)) * 100
-      let evictionRate = (Float(totalEvictions) / Float(totalCalls)) * 100
-      print("size = \(dictionary.count), totalCalls = \(totalCalls), hitRate = \(hitRate)%, evictionRate = \(evictionRate)%")
-    }
-  }
-  
   subscript(key: Key) -> Value? {
     get {
-      lock.sync {
-        totalCalls += 1
-        if let value = dictionary[key] {
-          totalHits += 1
-          return value
-        }
-        return nil
+      return self.lock.sync {
+        self.dictionary[key]
       }
     }
     set {
-      lock.sync {
-        dictionary[key] = newValue
-        if dictionary.count > maxCapacity {
-          // evict first (oldest) element
-          dictionary.removeFirst()
-          totalEvictions += 1
+      self.lock.sync {
+        self.dictionary[key] = newValue
+        if self.dictionary.count > self.maxCapacity {
+          self.dictionary.removeFirst()
         }
       }
     }
   }
-  
-}
-
-func memoize<Input: Hashable, Result>(
-  maxCapacity: Int = 500,
-  _ apply: @escaping (Input) -> Result
-) -> (Input) -> Result {
-  let cache = MemoizedCache<Input, Result>(maxCapacity: maxCapacity)
-  
-  return { input in
-//    defer { cache.printStats() }
-    if let memoizedResult = cache[input] {
-      return memoizedResult
-    }
-    
-    let result = apply(input)
-    cache[input] = result
-    return result
-  }
-  
 }
