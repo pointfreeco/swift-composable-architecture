1c5f55706d0c46e99d74f7dd8fc6d9bba046e7cd
Brandon Williams <mbrandonw@hey.com>
2023-12-18 13:48:11 -0600

    wip

--- Examples/Integration/IntegrationUITests/Legacy/ForEachBindingTests.swift
+++ Examples/Integration/IntegrationUITests/Legacy/ForEachBindingTests.swift
@@ -18,8 +18,8 @@ final class ForEachBindingTests: BaseIntegrationTests {
     XCTAssertFalse(app.textFields["A"].exists)
     XCTExpectFailure(
       """
-        This ideally would not fail, but currently does. See this PR for more details:
-        https://github.com/pointfreeco/swift-composable-architecture/pull/1845
+      This ideally would not fail, but currently does. See this PR for more details:
+      https://github.com/pointfreeco/swift-composable-architecture/pull/1845
       """
     ) {
       XCTAssertFalse(app.staticTexts["🛑"].exists)
