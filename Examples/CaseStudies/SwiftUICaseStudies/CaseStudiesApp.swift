ab75a077117ac3564607785a3f06b3e98effecdf
Brandon Williams <mbrandonw@hey.com>
2023-12-20 15:58:26 -0600

    wip

--- Examples/CaseStudies/SwiftUICaseStudies/CaseStudiesApp.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/CaseStudiesApp.swift
@@ -1,10 +1,9 @@
-@_spi(Logging) import ComposableArchitecture
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct CaseStudiesApp: App {
   var body: some Scene {
-    let _ = Logger.shared.isEnabled = true
     WindowGroup {
       RootView()
     }
