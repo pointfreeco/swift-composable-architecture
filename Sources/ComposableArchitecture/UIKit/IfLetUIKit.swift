ae0c05c492e628594c7d02bdcb2a8ea97f00b9a8
Stephen Celis <stephen@stephencelis.com>
2023-12-22 09:54:16 -0800

    fix

--- Sources/ComposableArchitecture/UIKit/IfLetUIKit.swift
+++ Sources/ComposableArchitecture/UIKit/IfLetUIKit.swift
@@ -57,7 +57,7 @@ extension Store {
           unwrap(
             self.scope(
               id: self.id(state: \.!, action: \.self),
-              state: ToState(\.[default: state]),
+              state: ToState(\.[default: SubscriptDefault(state)]),
               action: { $0 },
               isInvalid: { $0 == nil }
             )
