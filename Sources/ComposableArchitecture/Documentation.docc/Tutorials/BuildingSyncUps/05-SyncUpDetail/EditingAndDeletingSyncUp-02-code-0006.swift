dd858937ab1ce837adb6b83ed12dec18373f78fb
Brandon Williams <mbrandonw@hey.com>
2023-12-27 16:25:57 -0700

    more sync ups tutorial

---
+++ Sources/ComposableArchitecture/Documentation.docc/Tutorials/BuildingSyncUps/05-SyncUpDetail/EditingAndDeletingSyncUp-02-code-0006.swift
@@ -0,0 +1,66 @@
+import ComposableArchitecture
+
+@Reducer
+struct SyncUpDetail {
+  @ObservableState
+  struct State {
+    @Presents var alert: AlertState<Action.Alert>?
+    @Presents var editSyncUp: SyncUpForm.State?
+    var syncUp: SyncUp
+  }
+
+  enum Action {
+    case alert(PresentationAction<Alert>)
+    case cancelEditButtonTapped
+    case deleteButtonTapped
+    case doneEditingButtonTapped
+    case editButtonTapped
+    case editSyncUp(PresentationAction<SycnUpForm.Action>)
+    case startMeetingButtonTapped
+    case meetingTapped(id: Meeting.ID)
+    enum Alert {
+      case confirmButtonTapped
+    }
+  }
+
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .alert(.presented(.confirmButtonTapped)):
+        
+      case .alert(.dismiss):
+
+      case .cancelEditButtonTapped:
+        state.editSyncUp = nil
+        return .none
+
+      case .deleteButtonTapped:
+        return .none
+
+      case .doneEditingButtonTapped:
+        guard let editedSyncUp = state.editSyncUp?.syncUp
+        else { return .none }
+        state.syncUp = editedSyncUp
+        return .none
+
+      case .editButtonTapped:
+        state.editSyncUp = SyncUpForm.State(syncUp: state.syncUp)
+        return .none
+
+      case .startMeetingButtonTapped:
+        return .none
+
+      case let .meetingTapped(id: id):
+        return .none
+      }
+    }
+    .ifLet(\.$editSyncUp, action: \.editSyncUp) {
+      SyncUpForm()
+    }
+    .ifLet(\.$alert, action: \.alert) 
+  }
+}
+
+struct SyncUpDetailView {
+  // ...
+}
