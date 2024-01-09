85f22c83451c32f77dcbb0a4df4b487ee48834c7
Brandon Williams <mbrandonw@hey.com>
2024-01-09 13:36:25 -0800

    add alert test

--- Examples/CaseStudies/SwiftUICaseStudiesTests/01-GettingStarted-SharedStateTests.swift
+++ Examples/CaseStudies/SwiftUICaseStudiesTests/01-GettingStarted-SharedStateTests.swift
@@ -36,4 +36,16 @@ final class SharedStateTests: XCTestCase {
       $0.profile.stats = Stats()
     }
   }
+
+  func testAlert() async {
+    let store = TestStore(initialState: SharedState.State()) {
+      SharedState()
+    }
+
+    await store.send(.counter(.isPrimeButtonTapped)) {
+      $0.counter.alert = AlertState {
+        TextState("👎 The number 0 is not prime :(")
+      }
+    }
+  }
 }
