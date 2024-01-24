bf2326a0412e775b5cd1335c212cd3b8efe988dd
Stephen Celis <stephen@stephencelis.com>
2024-01-24 00:04:48 -0800

    wip

--- Sources/ComposableArchitecture/Internal/EphemeralState.swift
+++ Sources/ComposableArchitecture/Internal/EphemeralState.swift
@@ -4,27 +4,28 @@
 /// with they go away. Such features do not manage any behavior on the inside.
 ///
 /// Alerts and confirmation dialogs are examples of this kind of state.
-public protocol _EphemeralState {
-  static var actionType: Any.Type { get }
+public protocol _EphemeralState<Action>: CaseReducer, CaseReducerState {
+  associatedtype Reducer = Self
+  static var actionType: Action.Type { get }
 }
 
 #if swift(>=5.8)
   @_documentation(visibility:private)
   extension AlertState: _EphemeralState {
-    public static var actionType: Any.Type { Action.self }
+    public static var actionType: Action.Type { Action.self }
   }
   @_documentation(visibility:private)
   @available(iOS 13, macOS 12, tvOS 13, watchOS 6, *)
   extension ConfirmationDialogState: _EphemeralState {
-    public static var actionType: Any.Type { Action.self }
+    public static var actionType: Action.Type { Action.self }
   }
 #else
   extension AlertState: _EphemeralState {
-    public static var actionType: Any.Type { Action.self }
+    public static var actionType: Action.Type { Action.self }
   }
   @available(iOS 13, macOS 12, tvOS 13, watchOS 6, *)
   extension ConfirmationDialogState: _EphemeralState {
-    public static var actionType: Any.Type { Action.self }
+    public static var actionType: Action.Type { Action.self }
   }
 #endif
 
@@ -52,18 +53,15 @@ extension _EphemeralState {
   }
 }
 
-extension AlertState: Reducer {
-  public init() {
-    self.init(title: TextState(""))
-  }
-  public var body: some Reducer<Self, Action> {
-    EmptyReducer()
-  }
+public struct _EphemeralStateCases<State: _EphemeralState> {
+  let store: Store<State, State.Action>
 }
 
-@available(iOS 13, macOS 12, tvOS 13, watchOS 6, *)
-extension ConfirmationDialogState: Reducer {
-  public var body: some Reducer<Self, Action> {
+extension _EphemeralState {
+  public static func cases(_ store: Store<Self, Action>) -> _EphemeralStateCases<Self> {
+    _EphemeralStateCases(store: store)
+  }
+  public static var body: some ComposableArchitecture.Reducer<Self, Action> {
     EmptyReducer()
   }
 }
