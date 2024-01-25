2421cd70bc97326f3fdc5301caaf44cd0bb6396c
Brandon Williams <mbrandonw@hey.com>
2024-01-25 13:26:36 -0800

    fixed some todos

--- Sources/ComposableArchitectureMacros/ObservableStateMacro.swift
+++ Sources/ComposableArchitectureMacros/ObservableStateMacro.swift
@@ -426,28 +426,15 @@ extension ObservableStateMacro: ExtensionMacro {
       return []
     }
 
-    let decl: DeclSyntax = """
-      extension \(raw: type.trimmedDescription): \(raw: qualifiedConformanceName) {}
+    return [
+      (
       """
-    // TODO: Take maximum of current availability and 17/14/17/10/etc...
-    let obsDecl: DeclSyntax = """
-      @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-      extension \(raw: type.trimmedDescription): Observation.Observable {}
-      """
-    let ext = decl.cast(ExtensionDeclSyntax.self)
-    let obsExt = obsDecl.cast(ExtensionDeclSyntax.self)
-
-    if let availability = declaration.attributes.availability {
-      return [
-        ext.with(\.attributes, availability),
-        obsExt.with(\.attributes, availability),
-      ]
-    } else {
-      return [
-        ext,
-        obsExt,
-      ]
-    }
+      extension \(raw: type.trimmedDescription): \(raw: qualifiedConformanceName), \
+      Observation.Observable {}
+      """ as DeclSyntax
+      )
+      .cast(ExtensionDeclSyntax.self)
+    ]
   }
 }
 
