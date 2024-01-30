5610c2aa1b66ec495d1a29d6ae3b2a4a7f7bb8e8
Stephen Celis <stephen@stephencelis.com>
2024-01-29 17:44:11 -0800

    wip

--- Sources/ComposableArchitecture/Observation/Binding+Observation.swift
+++ Sources/ComposableArchitecture/Observation/Binding+Observation.swift
@@ -1,52 +1,13 @@
 #if canImport(Perception)
   import SwiftUI
 
-  @dynamicMemberLookup
-  public struct _StoreBinding<State, Action> {
-    fileprivate let wrappedValue: Store<State, Action>
-
-    public subscript<Member>(
-      dynamicMember keyPath: KeyPath<State, Member>
-    ) -> _StoreBinding<Member, Action> {
-      _StoreBinding<Member, Action>(
-        wrappedValue: self.wrappedValue.scope(state: keyPath, action: \.self)
-      )
-    }
-
-    /// Creates a binding to the value by sending new values through the given action.
-    ///
-    /// - Parameter action: An action for the binding to send values through.
-    /// - Returns: A binding.
-    public func sending(_ action: CaseKeyPath<Action, State>) -> Binding<State> {
-      Binding(
-        get: { self.wrappedValue.withState { $0 } },
-        set: { newValue, transaction in
-          BindingLocal.$isActive.withValue(true) {
-            _ = self.wrappedValue.send(action(newValue), transaction: transaction)
-          }
-        }
-      )
-    }
-  }
-
   extension Binding {
     @_disfavoredOverload
     public subscript<State: ObservableState, Action, Member>(
       dynamicMember keyPath: KeyPath<State, Member>
-    ) -> _StoreBinding<Member, Action>
+    ) -> _StoreBinding<State, Action, Member>
     where Value == Store<State, Action> {
-      _StoreBinding(wrappedValue: self.wrappedValue.scope(state: keyPath, action: \.self))
-    }
-  }
-
-  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
-  extension SwiftUI.Bindable {
-    @_disfavoredOverload
-    public subscript<State: ObservableState, Action, Member>(
-      dynamicMember keyPath: KeyPath<State, Member>
-    ) -> _StoreBinding<Member, Action>
-    where Value == Store<State, Action> {
-      _StoreBinding(wrappedValue: self.wrappedValue.scope(state: keyPath, action: \.self))
+      _StoreBinding(binding: self, keyPath: keyPath)
     }
   }
 
@@ -58,9 +19,9 @@
     @_disfavoredOverload
     public subscript<State: ObservableState, Action, Member>(
       dynamicMember keyPath: KeyPath<State, Member>
-    ) -> _StoreBinding<Member, Action>
+    ) -> _StoreBindable_Perception<State, Action, Member>
     where Value == Store<State, Action> {
-      _StoreBinding(wrappedValue: self.wrappedValue.scope(state: keyPath, action: \.self))
+      _StoreBindable_Perception(bindable: self, keyPath: keyPath)
     }
   }
 
@@ -160,6 +121,105 @@
       set {
         BindingLocal.$isActive.withValue(true) {
           self.send(.view(.binding(.set(\.self, newValue))))
+        }
+      }
+    }
+  }
+
+  @dynamicMemberLookup
+  public struct _StoreBinding<State: ObservableState, Action, Value> {
+    fileprivate let binding: Binding<Store<State, Action>>
+    fileprivate let keyPath: KeyPath<State, Value>
+
+    public subscript<Member>(
+      dynamicMember keyPath: KeyPath<Value, Member>
+    ) -> _StoreBinding<State, Action, Member> {
+      _StoreBinding<State, Action, Member>(
+        binding: self.binding,
+        keyPath: self.keyPath.appending(path: keyPath)
+      )
+    }
+
+    /// Creates a binding to the value by sending new values through the given action.
+    ///
+    /// - Parameter action: An action for the binding to send values through.
+    /// - Returns: A binding.
+    public func sending(_ action: CaseKeyPath<Action, Value>) -> Binding<Value> {
+      self.binding[state: self.keyPath, action: action]
+    }
+  }
+
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  @dynamicMemberLookup
+  public struct _StoreBindable_SwiftUI<State: ObservableState, Action, Value> {
+    fileprivate let bindable: SwiftUI.Bindable<Store<State, Action>>
+    fileprivate let keyPath: KeyPath<State, Value>
+
+    public subscript<Member>(
+      dynamicMember keyPath: KeyPath<Value, Member>
+    ) -> _StoreBindable_SwiftUI<State, Action, Member> {
+      _StoreBindable_SwiftUI<State, Action, Member>(
+        bindable: self.bindable,
+        keyPath: self.keyPath.appending(path: keyPath)
+      )
+    }
+
+    /// Creates a binding to the value by sending new values through the given action.
+    ///
+    /// - Parameter action: An action for the binding to send values through.
+    /// - Returns: A binding.
+    public func sending(_ action: CaseKeyPath<Action, Value>) -> Binding<Value> {
+      self.bindable[state: self.keyPath, action: action]
+    }
+  }
+
+  @available(iOS 17, macOS 14, tvOS 17, watchOS 10, *)
+  extension SwiftUI.Bindable {
+    @_disfavoredOverload
+    public subscript<State: ObservableState, Action, Member>(
+      dynamicMember keyPath: KeyPath<State, Member>
+    ) -> _StoreBindable_SwiftUI<State, Action, Member>
+    where Value == Store<State, Action> {
+      _StoreBindable_SwiftUI(bindable: self, keyPath: keyPath)
+    }
+  }
+
+  @available(iOS, introduced: 13, obsoleted: 17)
+  @available(macOS, introduced: 10.15, obsoleted: 14)
+  @available(tvOS, introduced: 13, obsoleted: 17)
+  @available(watchOS, introduced: 6, obsoleted: 10)
+  @dynamicMemberLookup
+  public struct _StoreBindable_Perception<State: ObservableState, Action, Value> {
+    fileprivate let bindable: Perception.Bindable<Store<State, Action>>
+    fileprivate let keyPath: KeyPath<State, Value>
+
+    public subscript<Member>(
+      dynamicMember keyPath: KeyPath<Value, Member>
+    ) -> _StoreBindable_Perception<State, Action, Member> {
+      _StoreBindable_Perception<State, Action, Member>(
+        bindable: self.bindable,
+        keyPath: self.keyPath.appending(path: keyPath)
+      )
+    }
+
+    /// Creates a binding to the value by sending new values through the given action.
+    ///
+    /// - Parameter action: An action for the binding to send values through.
+    /// - Returns: A binding.
+    public func sending(_ action: CaseKeyPath<Action, Value>) -> Binding<Value> {
+      self.bindable[state: self.keyPath, action: action]
+    }
+  }
+
+  extension Store where State: ObservableState {
+    fileprivate subscript<Value>(
+      state state: KeyPath<State, Value>,
+      action action: CaseKeyPath<Action, Value>
+    ) -> Value {
+      get { self.state[keyPath: state] }
+      set {
+        BindingLocal.$isActive.withValue(true) {
+          self.send(action(newValue))
         }
       }
     }
