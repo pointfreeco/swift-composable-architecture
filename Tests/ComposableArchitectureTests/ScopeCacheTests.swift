6b33c34d124253766b6194b7128cfc07401d3526
Brandon Williams <mbrandonw@hey.com>
2024-02-25 13:08:26 -0800

    fixes

--- Tests/ComposableArchitectureTests/ScopeCacheTests.swift
+++ Tests/ComposableArchitectureTests/ScopeCacheTests.swift
@@ -106,36 +106,38 @@
       rowsStore[0].send(.show)
     }
 
-    @available(*, deprecated)
-    func testIdentifiedArrayScope_UncachedStore() {
-      let store = StoreOf<Feature>(initialState: Feature.State(rows: [Feature.State()])) {
-        Feature()
+    #if DEBUG
+      @available(*, deprecated)
+      func testIdentifiedArrayScope_UncachedStore() {
+        let store = StoreOf<Feature>(initialState: Feature.State(rows: [Feature.State()])) {
+          Feature()
+        }
+        XCTExpectFailure {
+          _ = Array(
+            store
+              .scope(state: { $0 }, action: { $0 })
+              .scope(state: \.rows, action: \.rows)
+          )
+        } issueMatcher: {
+          $0.compactDescription == """
+            Scoping from uncached StoreOf<Feature> is not compatible with observation.
+
+            This can happen for one of two reasons:
+
+            • A parent view scopes on a store using transform functions, which has been \
+            deprecated, instead of with key paths and case paths. Read the migration guide for 1.5 \
+            to update these scopes: https://pointfreeco.github.io/swift-composable-architecture/\
+            main/documentation/composablearchitecture/migratingto1.5
+
+            • A parent feature is using deprecated navigation APIs, such as 'IfLetStore', \
+            'SwitchStore', 'ForEachStore', or any navigation view modifiers taking stores instead of \
+            bindings. Read the migration guide for 1.7 to update those APIs: \
+            https://pointfreeco.github.io/swift-composable-architecture/main/documentation/\
+            composablearchitecture/migratingto1.7
+            """
+        }
       }
-      XCTExpectFailure {
-        _ = Array(
-          store
-            .scope(state: { $0 }, action: { $0 })
-            .scope(state: \.rows, action: \.rows)
-        )
-      } issueMatcher: {
-        $0.compactDescription == """
-          Scoping from uncached StoreOf<Feature> is not compatible with observation.
-
-          This can happen for one of two reasons:
-
-          • A parent view scopes on a store using transform functions, which has been \
-          deprecated, instead of with key paths and case paths. Read the migration guide for 1.5 \
-          to update these scopes: https://pointfreeco.github.io/swift-composable-architecture/\
-          main/documentation/composablearchitecture/migratingto1.5
-
-          • A parent feature is using deprecated navigation APIs, such as 'IfLetStore', \
-          'SwitchStore', 'ForEachStore', or any navigation view modifiers taking stores instead of \
-          bindings. Read the migration guide for 1.7 to update those APIs: \
-          https://pointfreeco.github.io/swift-composable-architecture/main/documentation/\
-          composablearchitecture/migratingto1.7
-          """
-      }
-    }
+    #endif
   }
 
   @Reducer
