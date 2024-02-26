c610bf576f9c7d4363f7e3e16357ccba862a701b
Brandon Williams <mbrandonw@hey.com>
2024-02-26 12:11:50 -0800

    add tests

---
+++ Tests/ComposableArchitectureTests/EnumReducerMacroTests.swift
@@ -0,0 +1,31 @@
+#if swift(>=5.9)
+  import ComposableArchitecture
+
+  private enum TestEnumReducer_CompilerDirective {
+    @Reducer
+    struct ChildFeature {}
+    enum Options {}
+
+    @Reducer
+    enum Feature {
+      case child(ChildFeature)
+
+      #if os(macOS)
+        case mac(ChildFeature)
+        case macAlert(AlertState<Options>)
+      #elseif os(iOS)
+        case phone(ChildFeature)
+      #else
+        case other(ChildFeature)
+        case another
+      #endif
+
+      #if DEBUG
+        #if INNER
+          case inner(ChildFeature)
+          case innerDialog(ConfirmationDialogState<Options>)
+        #endif
+      #endif
+    }
+  }
+#endif
