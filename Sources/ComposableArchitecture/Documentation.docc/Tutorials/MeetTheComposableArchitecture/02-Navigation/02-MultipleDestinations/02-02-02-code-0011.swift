9dcbfd91bd71b651fabaa1972888e0f638e81d10
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2024-02-29 14:25:31 -0800

    Make enum reducer state equatable in tutorial. (#2873)

--- Sources/ComposableArchitecture/Documentation.docc/Tutorials/MeetTheComposableArchitecture/02-Navigation/02-MultipleDestinations/02-02-02-code-0011.swift
+++ Sources/ComposableArchitecture/Documentation.docc/Tutorials/MeetTheComposableArchitecture/02-Navigation/02-MultipleDestinations/02-02-02-code-0011.swift
@@ -1,40 +1,53 @@
-struct ContactsView: View {
-  @Bindable var store: StoreOf<ContactsFeature>
-  
-  var body: some View {
-    NavigationStack {
-      List {
-        ForEach(store.contacts) { contact in
-          HStack {
-            Text(contact.name)
-            Spacer()
-            Button {
-              store.send(.deleteButtonTapped(id: contact.id))
-            } label: {
-              Image(systemName: "trash")
-                .foregroundColor(.red)
+@Reducer
+struct ContactsFeature {
+  @ObservableState
+  struct State: Equatable {
+    var contacts: IdentifiedArrayOf<Contact> = []
+    @Presents var destination: Destination.State?
+  }
+  enum Action {
+    case addButtonTapped
+    case deleteButtonTapped(id: Contact.ID)
+    case destination(PresentationAction<Destination.Action>)
+    enum Alert: Equatable {
+      case confirmDeletion(id: Contact.ID)
+    }
+  }
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .addButtonTapped:
+        state.destination = .addContact(
+          AddContactFeature.State(
+            contact: Contact(id: UUID(), name: "")
+          )
+        )
+        return .none
+        
+      case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
+        state.contacts.append(contact)
+        return .none
+        
+      case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
+        state.contacts.remove(id: id)
+        return .none
+        
+      case .destination:
+        return .none
+        
+      case let .deleteButtonTapped(id: id):
+        state.destination = .alert(
+          AlertState {
+            TextState("Are you sure?")
+          } actions: {
+            ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+              TextState("Delete")
             }
           }
-        }
-      }
-      .navigationTitle("Contacts")
-      .toolbar {
-        ToolbarItem {
-          Button {
-            store.send(.addButtonTapped)
-          } label: {
-            Image(systemName: "plus")
-          }
-        }
+        )
+        return .none
       }
     }
-    .sheet(
-      item: $store.scope(state: \.destination?.addContact, action: \.destination.addContact)
-    ) { addContactStore in
-      NavigationStack {
-        AddContactView(store: addContactStore)
-      }
-    }
-    .alert($store.scope(state: \.alert, action: \.alert))
+    .ifLet(\.$destination, action: \.destination)
   }
 }
