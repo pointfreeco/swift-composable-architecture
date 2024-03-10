c258bcd7b6b4baefda2bcdc93c4d8a886f97485e
Stephen Celis <stephen@stephencelis.com>
2024-03-10 09:34:54 -0700

    Revert sharing in Todos for now

--- Examples/Todos/Todos/Todo.swift
+++ Examples/Todos/Todos/Todo.swift
@@ -1,49 +1,38 @@
-import Foundation
-
-struct Todo: Codable, Equatable, Identifiable {
-  var description = ""
-  let id: UUID
-  var isComplete = false
-}
-
-//import ComposableArchitecture
-//import SwiftUI
-//
-//@Reducer
-//struct Todo {
-//  @ObservableState
-//  struct State: Equatable, Identifiable {
-//    var description = ""
-//    let id: UUID
-//    var isComplete = false
-//  }
-//
-//  enum Action: BindableAction, Sendable {
-//    case binding(BindingAction<State>)
-//  }
-//
-//  var body: some Reducer<State, Action> {
-//    BindingReducer()
-//  }
-//}
-//
-
+import ComposableArchitecture
 import SwiftUI
 
+@Reducer
+struct Todo {
+  @ObservableState
+  struct State: Equatable, Identifiable {
+    var description = ""
+    let id: UUID
+    var isComplete = false
+  }
+
+  enum Action: BindableAction, Sendable {
+    case binding(BindingAction<State>)
+  }
+
+  var body: some Reducer<State, Action> {
+    BindingReducer()
+  }
+}
+
 struct TodoView: View {
-  @Binding var todo: Todo
+  @Bindable var store: StoreOf<Todo>
 
   var body: some View {
     HStack {
       Button {
-        todo.isComplete.toggle()
+        store.isComplete.toggle()
       } label: {
-        Image(systemName: todo.isComplete ? "checkmark.square" : "square")
+        Image(systemName: store.isComplete ? "checkmark.square" : "square")
       }
       .buttonStyle(.plain)
 
-      TextField("Untitled Todo", text: $todo.description)
+      TextField("Untitled Todo", text: $store.description)
     }
-    .foregroundColor(todo.isComplete ? .gray : nil)
+    .foregroundColor(store.isComplete ? .gray : nil)
   }
 }
