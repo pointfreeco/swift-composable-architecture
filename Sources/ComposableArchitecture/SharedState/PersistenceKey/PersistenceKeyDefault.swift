766c17566dc40801a45b30dccdd8e65e0106f157
Brandon Williams <mbrandonw@hey.com>
2024-04-16 10:13:56 -0700

    docs

--- Sources/ComposableArchitecture/SharedState/PersistenceKey/PersistenceKeyDefault.swift
+++ Sources/ComposableArchitecture/SharedState/PersistenceKey/PersistenceKeyDefault.swift
@@ -1,4 +1,27 @@
 /// A persistence key that provides a default value to an existing persistence key.
+///
+/// Use this persistence key when constructing type-safe keys (see
+/// <doc:SharingState#Type-safe-keys> for more info) to provide a deafult that is used instead of
+/// providing one at the call site of using [`@Shared`](<doc:Shared>).
+///
+/// For example, if an `isOn` value is backed by user defaults and it should default to `false` when
+/// there is no value in user defaults, then you can define a persistence key like so:
+///
+/// ```swift
+/// extension PersistenceReaderKey where Self == PersistenceKeyDefault<AppStorageKey<Bool>> {
+///   static var isOn: Self {
+///     PersistenceKeyDefault(.appStorage("isOn"), false)
+///   }
+/// }
+/// ```
+///
+/// And then use it like so:
+///
+/// ```swift
+/// struct State {
+///   @Shared(.isOn) var isOn
+/// }
+/// ```
 public struct PersistenceKeyDefault<Base: PersistenceReaderKey>: PersistenceReaderKey {
   let base: Base
   let defaultValue: Base.Value
