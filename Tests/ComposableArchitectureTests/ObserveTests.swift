aaa590cd6070540affe66f750911b6ac9c56e2e5
Brandon Williams <mbrandonw@hey.com>
2024-04-17 14:39:01 -0700

    add some tests

--- Tests/ComposableArchitectureTests/ObserveTests.swift
+++ Tests/ComposableArchitectureTests/ObserveTests.swift
@@ -14,9 +14,13 @@
       model.count += 1
       try await Task.sleep(nanoseconds: 1_000_000)
       XCTAssertEqual(counts, [0, 1])
+
+      model.otherCount += 1
+      try await Task.sleep(nanoseconds: 1_000_000)
+      XCTAssertEqual(counts, [0, 1])
+
       _ = observation
     }
-
     func testCancellation() async throws {
       let model = Model()
       var counts: [Int] = []
@@ -30,10 +34,48 @@
       XCTAssertEqual(counts, [0])
       _ = observation
     }
+
+    @MainActor
+    func testNestedObservation() async throws {
+      XCTExpectFailure {
+        $0.compactDescription == """
+          An "observe" was called from another "observe" closure, which can lead to \
+          over-observation and unintended side effects.
+
+          Avoid nested closures by moving child observation into their own lifecycle methods.
+          """
+      }
+
+      let model = Model()
+      var counts: [Int] = []
+      var innerObservation: Any!
+      let observation = observe { [weak self] in
+        guard let self else { return }
+        counts.append(model.count)
+        innerObservation = observe {
+          _ = model.otherCount
+        }
+      }
+      defer {
+        _ = observation
+        _ = innerObservation
+      }
+
+      XCTAssertEqual(counts, [0])
+
+      model.count += 1
+      try await Task.sleep(nanoseconds: 1_000_000)
+      XCTAssertEqual(counts, [0, 1])
+
+      model.otherCount += 1
+      try await Task.sleep(nanoseconds: 1_000_000)
+      XCTAssertEqual(counts, [0, 1, 1])
+    }
   }
 
   @Perceptible
   class Model {
     var count = 0
+    var otherCount = 0
   }
 #endif
