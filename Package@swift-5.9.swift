e6c0959044cbe2c934ac0ce9c3c4ead86350f44f
Stephen Celis <stephen@stephencelis.com>
2024-04-30 11:43:39 -0700

    Revert perception availability changes (#3036)

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -29,7 +29,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.0"),
-    .package(url: "https://github.com/pointfreeco/swift-perception", from: "1.1.6"),
+    .package(url: "https://github.com/pointfreeco/swift-perception", from: "1.1.7"),
     .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.1.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.1.0"),
   ],
