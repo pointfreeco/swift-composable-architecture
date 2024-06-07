a9f987d0028f47b0edf691411c478477e3e0422d
David Furman <DavidAsherFurman@gmail.com>
2024-06-06 19:35:53 -0700

    Revert "Add missing mock"
    This reverts commit 9672ecbd2061ca378254e8a922fd00389187621d.

--- Sources/ComposableArchitecture/Documentation.docc/Tutorials/BuildingSyncUps/07-SyncUpDetailNavigation/MeetingNavigation-01-code-0003.swift
+++ Sources/ComposableArchitecture/Documentation.docc/Tutorials/BuildingSyncUps/07-SyncUpDetailNavigation/MeetingNavigation-01-code-0003.swift
@@ -23,31 +23,6 @@ struct MeetingView: View {
   }
 }
 
-extension SyncUp {
-  static let mock = Self(
-    id: SyncUp.ID(),
-    attendees: [
-      Attendee(id: Attendee.ID(), name: "Blob"),
-      Attendee(id: Attendee.ID(), name: "Blob Jr"),
-      Attendee(id: Attendee.ID(), name: "Blob Sr"),
-    ],
-    duration: .seconds(60),
-    meetings: [
-      Meeting(
-        id: Meeting.ID(),
-        date: Date().addingTimeInterval(-60 * 60 * 24 * 7),
-        transcript: """
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
-          …
-          """
-      )
-    ],
-    theme: .orange,
-    title: "Design"
-  )
-}
-
 #Preview {
   MeetingView(meeting: SyncUp.mock.meetings[0], syncUp: .mock)
 }
