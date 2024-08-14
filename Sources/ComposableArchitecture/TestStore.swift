c8bf4494ec204eaaff7518f5d2f284a0ee48557a
Stephen Celis <stephen@stephencelis.com>
2024-08-13 17:56:25 -0700

    fix

--- Sources/ComposableArchitecture/TestStore.swift
+++ Sources/ComposableArchitecture/TestStore.swift
@@ -2906,11 +2906,13 @@ fileprivate func _withIssueContext<R>(
   column: UInt,
   @_inheritActorContext operation: () async throws -> R
 ) async rethrows -> R {
-  try await withIssueContext(
+  let result = try await withIssueContext(
     fileID: fileID,
     filePath: filePath,
     line: line,
     column: column,
     operation: operation
   )
+  await Task.megaYield()
+  return result
 }
