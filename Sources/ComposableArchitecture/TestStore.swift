b69ed9c2de1c3dedaa2dea147892842ec830fe9e
Stephen Celis <stephen@stephencelis.com>
2024-09-30 14:32:22 -0700

    TestStore+_Store

--- Sources/ComposableArchitecture/TestStore.swift
+++ Sources/ComposableArchitecture/TestStore.swift
@@ -502,7 +502,7 @@ public final class TestStore<State, Action> {
   private let column: UInt
   let reducer: TestReducer<State, Action>
   private let sharedChangeTracker: SharedChangeTracker
-  private let store: Store<State, TestReducer<State, Action>.TestAction>
+  private let store: _Store<State, TestReducer<State, Action>.TestAction>
 
   /// Returns `true` if the store's feature has been dismissed.
   public fileprivate(set) var isDismissed = false
@@ -545,7 +545,7 @@ public final class TestStore<State, Action> {
     self.line = line
     self.column = column
     self.reducer = reducer
-    self.store = Store(initialState: reducer.state) { reducer }
+    self.store = _Store(initialState: reducer.state) { reducer }
     self.timeout = 1 * NSEC_PER_SEC
     self.sharedChangeTracker = sharedChangeTracker
     self.useMainSerialExecutor = true
