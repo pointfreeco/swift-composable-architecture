1aca801ee2b2ae9e41ad28b2de21f8a89ac4473c
mbrandonw <mbrandonw@users.noreply.github.com>
2024-10-14 17:09:44 +0000

    Run swift-format

--- Sources/ComposableArchitecture/Internal/KeyPath+Sendable.swift
+++ Sources/ComposableArchitecture/Internal/KeyPath+Sendable.swift
@@ -2,7 +2,8 @@
   public typealias _SendableAnyKeyPath = any AnyKeyPath & Sendable
   public typealias _SendablePartialKeyPath<Root> = any PartialKeyPath<Root> & Sendable
   public typealias _SendableKeyPath<Root, Value> = any KeyPath<Root, Value> & Sendable
-  public typealias _SendableWritableKeyPath<Root, Value> = any WritableKeyPath<Root, Value> & Sendable
+  public typealias _SendableWritableKeyPath<Root, Value> = any WritableKeyPath<Root, Value>
+    & Sendable
   public typealias _SendableReferenceWritableKeyPath<Root, Value> = any ReferenceWritableKeyPath<
     Root, Value
   >
@@ -14,7 +15,9 @@
   public typealias _SendablePartialKeyPath<Root> = PartialKeyPath<Root>
   public typealias _SendableKeyPath<Root, Value> = KeyPath<Root, Value>
   public typealias _SendableWritableKeyPath<Root, Value> = WritableKeyPath<Root, Value>
-  public typealias _SendableReferenceWritableKeyPath<Root, Value> = ReferenceWritableKeyPath<Root, Value>
+  public typealias _SendableReferenceWritableKeyPath<Root, Value> = ReferenceWritableKeyPath<
+    Root, Value
+  >
   public typealias _SendablePartialCaseKeyPath<Root> = PartialCaseKeyPath<Root>
   public typealias _SendableCaseKeyPath<Root, Value> = CaseKeyPath<Root, Value>
 #endif
--- Tests/ComposableArchitectureTests/EffectTests.swift
+++ Tests/ComposableArchitectureTests/EffectTests.swift
@@ -50,7 +50,6 @@ final class EffectTests: BaseTCATestCase {
     await withMainSerialExecutor {
       let values = LockIsolated<[Int]>([])
 
-
       let effect = Effect<Int>.concatenate(
         .publisher { Just(1).delay(for: 1, scheduler: self.mainQueue) }
       )
