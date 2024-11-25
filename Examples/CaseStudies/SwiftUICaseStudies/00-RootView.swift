c97c44025b9796794b97e9cd89b4e9cf7f3b19f1
Brandon Williams <mbrandonw@hey.com>
2024-11-25 13:13:48 -0600

    remove old case study for shared notifications

--- Examples/CaseStudies/SwiftUICaseStudies/00-RootView.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/00-RootView.swift
@@ -83,15 +83,6 @@ struct RootView: View {
               SharedStateFileStorageView(store: store)
             }
           }
-          NavigationLink("Notifications") {
-            Demo(
-              store: Store(initialState: SharedStateNotifications.State()) {
-                SharedStateNotifications()
-              }
-            ) { store in
-              SharedStateNotificationsView(store: store)
-            }
-          }
           Button("Sign up flow") {
             isSignUpCaseStudyPresented = true
           }
