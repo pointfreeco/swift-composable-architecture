0bcbe0a076bc41b67c7ad8aacaa829098c95990a
Stephen Celis <stephen@stephencelis.com>
2024-11-30 14:06:23 -0600

    wip

--- Sources/ComposableArchitecture/Sharing/AppStorageKeyPathKey.swift
+++ Sources/ComposableArchitecture/Sharing/AppStorageKeyPathKey.swift
@@ -14,7 +14,7 @@ extension SharedReaderKey {
   ///
   /// - Parameter keyPath: A string key identifying a value to share in memory.
   /// - Returns: A persistence key.
-  @available(*, deprecated, "Use 'appStorage' with a supported data type, instead")
+  @available(*, deprecated, message: "Use 'appStorage' with a supported data type, instead")
   public static func appStorage<Value>(
     _ keyPath: _SendableReferenceWritableKeyPath<UserDefaults, Value>
   ) -> Self where Self == AppStorageKeyPathKey<Value> {
@@ -25,7 +25,7 @@ extension SharedReaderKey {
 /// A type defining a user defaults persistence strategy via key path.
 ///
 /// See ``PersistenceReaderKey/appStorage(_:)-69h4r`` to create values of this type.
-@available(*, deprecated, "Use an 'AppStorageKey', instead")
+@available(*, deprecated, message: "Use an 'AppStorageKey', instead")
 public struct AppStorageKeyPathKey<Value: Sendable>: Sendable {
   private let keyPath: _SendableReferenceWritableKeyPath<UserDefaults, Value>
   private let store: UncheckedSendable<UserDefaults>
@@ -37,6 +37,7 @@ public struct AppStorageKeyPathKey<Value: Sendable>: Sendable {
   }
 }
 
+@available(*, deprecated, message: "Use an 'AppStorageKey', instead")
 extension AppStorageKeyPathKey: SharedKey, Hashable {
   public func load(initialValue _: Value?) -> Value? {
     self.store.wrappedValue[keyPath: self.keyPath]
