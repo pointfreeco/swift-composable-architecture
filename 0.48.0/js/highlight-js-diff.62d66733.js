bd7a536b9019a81542c9fa4f8dd4c7982f641378
mbrandonw <135203+mbrandonw@users.noreply.github.com>
2024-12-22 17:16:22 +0000

    Deploying to gh-pages from @ pointfreeco/swift-composable-architecture@642af1d50ef73f93b182dbd9a8ddcf011483352c 🚀

---
+++ 0.48.0/js/highlight-js-diff.62d66733.js
@@ -0,0 +1,10 @@
+/*!
+ * This source file is part of the Swift.org open source project
+ * 
+ * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ * 
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["highlight-js-diff"],{"48b8":function(e,n){function a(e){const n=e.regex;return{name:"Diff",aliases:["patch"],contains:[{className:"meta",relevance:10,match:n.either(/^@@ +-\d+,\d+ +\+\d+,\d+ +@@/,/^\*\*\* +\d+,\d+ +\*\*\*\*$/,/^--- +\d+,\d+ +----$/)},{className:"comment",variants:[{begin:n.either(/Index: /,/^index/,/={3,}/,/^-{3}/,/^\*{3} /,/^\+{3}/,/^diff --git/),end:/$/},{match:/^\*{15}$/}]},{className:"addition",begin:/^\+/,end:/$/},{className:"deletion",begin:/^-/,end:/$/},{className:"addition",begin:/^!/,end:/$/}]}}e.exports=a}}]);
