9424b18a64ecc102d74fcfa717ffc9d59a23822d
Stephen Celis <stephen@stephencelis.com>
2025-10-13 09:51:31 -0700

    Fix 6.2 compilation failure

--- Sources/ComposableArchitecture/Internal/DispatchQueue.swift
+++ Sources/ComposableArchitecture/Internal/DispatchQueue.swift
@@ -1,24 +1,22 @@
-#if compiler(<6.2)
-  import Dispatch
+import Dispatch
 
-  func mainActorNow<R: Sendable>(execute block: @MainActor @Sendable () -> R) -> R {
-    if DispatchQueue.getSpecific(key: key) == value {
-      return MainActor._assumeIsolated {
+func mainActorNow<R: Sendable>(execute block: @MainActor @Sendable () -> R) -> R {
+  if DispatchQueue.getSpecific(key: key) == value {
+    return MainActor._assumeIsolated {
+      block()
+    }
+  } else {
+    return DispatchQueue.main.sync {
+      MainActor._assumeIsolated {
         block()
-      }
-    } else {
-      return DispatchQueue.main.sync {
-        MainActor._assumeIsolated {
-          block()
-        }
       }
     }
   }
+}
 
-  private let key: DispatchSpecificKey<UInt8> = {
-    let key = DispatchSpecificKey<UInt8>()
-    DispatchQueue.main.setSpecific(key: key, value: value)
-    return key
-  }()
-  private let value: UInt8 = 0
-#endif
+private let key: DispatchSpecificKey<UInt8> = {
+  let key = DispatchSpecificKey<UInt8>()
+  DispatchQueue.main.setSpecific(key: key, value: value)
+  return key
+}()
+private let value: UInt8 = 0
