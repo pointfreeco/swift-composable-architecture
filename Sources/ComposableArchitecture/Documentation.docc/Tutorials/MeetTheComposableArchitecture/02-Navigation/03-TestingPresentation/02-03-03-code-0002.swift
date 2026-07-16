e004e84ec917cc0ba2c57ad82f76730dc9cef06b
opficdev <162981733+opficdev@users.noreply.github.com>
2026-07-16 09:35:09 +0900

    Fix TestStore initialization in navigation tutorial

--- Sources/ComposableArchitecture/Documentation.docc/Tutorials/MeetTheComposableArchitecture/02-Navigation/03-TestingPresentation/02-03-03-code-0002.swift
+++ Sources/ComposableArchitecture/Documentation.docc/Tutorials/MeetTheComposableArchitecture/02-Navigation/03-TestingPresentation/02-03-03-code-0002.swift
@@ -8,8 +8,7 @@ import Testing
 struct ContactsFeatureTests {
   @Test
   func deleteContact() async {
-    let store = TestStore(initialState: ContactsFeature.State()) {
-      initialState: ContactsFeature.State(
+    let store = TestStore(initialState: ContactsFeature.State(
         contacts: [
           Contact(id: UUID(0), name: "Blob"),
           Contact(id: UUID(1), name: "Blob Jr."),
